7a7c1b5c4fb7b5f7158ee077ab0d3f1fd57920cd
Kaspar Etter <ke@shiftdevices.com>
2018-02-20 14:05:29 +0100

    btc: move FeeTarget and FeeTargetCode from the wallet to a separate file

--- coins/btc/wallet.go
+++ coins/btc/wallet.go
@@ -64,45 +64,6 @@ type HDKeyStoreInterface interface {
 	Sign(hashes [][]byte, keyPaths []string) ([]btcec.Signature, error)
 }
 
-// FeeTargetCode is the ID of a fee target. See the FeeTargetCode* constants.x
-type FeeTargetCode string
-
-// NewFeeTargetCode checks if the code is valid and returns a FeeTargetCode in that case.
-func NewFeeTargetCode(code string) (FeeTargetCode, error) {
-	switch code {
-	case string(FeeTargetCodeLow):
-	case string(FeeTargetCodeEconomy):
-	case string(FeeTargetCodeNormal):
-	case string(FeeTargetCodeHigh):
-	default:
-		return "", errp.Newf("unrecognized fee code %s", code)
-	}
-	return FeeTargetCode(code), nil
-}
-
-const (
-	// FeeTargetCodeLow is the low priority fee target.
-	FeeTargetCodeLow FeeTargetCode = "low"
-	// FeeTargetCodeEconomy is the economy priority fee target.
-	FeeTargetCodeEconomy = "economy"
-	// FeeTargetCodeNormal is the normal priority fee target.
-	FeeTargetCodeNormal = "normal"
-	// FeeTargetCodeHigh is the high priority fee target.
-	FeeTargetCodeHigh = "high"
-
-	defaultFeeTarget = FeeTargetCodeNormal
-)
-
-// FeeTarget contains the fee rate for a specific fee target.
-type FeeTarget struct {
-	// Blocks is the target number of blocks in which the tx should be confirmed.
-	Blocks int
-	// Code is the identifier for the UI.
-	Code FeeTargetCode
-	// FeeRatePerKb is the fee rate needed for this target. Can be nil until populated.
-	FeeRatePerKb *btcutil.Amount
-}
-
 // Wallet is a wallet whose addresses are derived from an xpub.
 type Wallet struct {
 	locker.Locker
