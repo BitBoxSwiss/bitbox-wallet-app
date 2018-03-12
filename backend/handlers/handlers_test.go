e5c2812352e50d82ec21b6ceb5ed0fb052a1296c
Kaspar Etter <ke@shiftdevices.com>
2018-03-12 17:36:01 +0100

    backend: add test that lists all routes
    Having a generated list of all routes makes it easier to understand the API and to develop the frontend.

---
+++ backend/handlers/handlers_test.go
@@ -0,0 +1,47 @@
+package handlers_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/shiftdevices/godbb/backend"
+	"github.com/shiftdevices/godbb/backend/handlers"
+)
+
+// List all routes with `go test backend/handlers/handlers_test.go -v`.
+func TestListRoutes(t *testing.T) {
+	handlers := handlers.NewHandlers(backend.NewBackend(), 8082)
+	err := handlers.Router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		pathTemplate, err := route.GetPathTemplate()
+		if err != nil {
+			return err
+		}
+		methods, err := route.GetMethods()
+		if err != nil {
+			return err
+		}
+		if len(methods) == 0 {
+			fmt.Println()
+		}
+		fmt.Print(pathTemplate)
+		if len(methods) > 0 {
+			fmt.Print(" (" + strings.Join(methods, ",") + ")")
+		}
+		/* The following methods are only available in a newer version of mux: */
+		// queriesTemplates, err := route.GetQueriesTemplates()
+		// if err == nil {
+		// 	   fmt.Println("Queries templates:", strings.Join(queriesTemplates, ","))
+		// }
+		// queriesRegexps, err := route.GetQueriesRegexp()
+		// if err == nil {
+		// 	   fmt.Println("Queries regexps:", strings.Join(queriesRegexps, ","))
+		// }
+		fmt.Println()
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
