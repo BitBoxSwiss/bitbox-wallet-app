15295f0e5fb80bb1b189c0f4f660c65a34737c5f
Kaspar Etter <ke@shiftdevices.com>
2018-03-20 19:16:46 +0100

    bitbox: generate the current communication interface mock

--- devices/bitbox/mocks/CommunicationInterface.go
+++ devices/bitbox/mocks/CommunicationInterface.go
@@ -13,6 +13,29 @@ func (_m *CommunicationInterface) Close() {
 	_m.Called()
 }
 
+// SendBootloader provides a mock function with given fields: _a0
+func (_m *CommunicationInterface) SendBootloader(_a0 []byte) ([]byte, error) {
+	ret := _m.Called(_a0)
+
+	var r0 []byte
+	if rf, ok := ret.Get(0).(func([]byte) []byte); ok {
+		r0 = rf(_a0)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]byte)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func([]byte) error); ok {
+		r1 = rf(_a0)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // SendEncrypt provides a mock function with given fields: _a0, _a1
 func (_m *CommunicationInterface) SendEncrypt(_a0 string, _a1 string) (map[string]interface{}, error) {
 	ret := _m.Called(_a0, _a1)
