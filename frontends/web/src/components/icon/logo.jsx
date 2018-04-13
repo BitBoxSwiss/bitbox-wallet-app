7a14de605b2ea3313e9fb3f60e67ff127ec2af4a
Enrique Erne <enrique.erne@shiftdevices.com>
2018-04-13 11:33:45 +0200

    frontend: fix mainnet icons
    adds segwit BTC and LTC mainnet icons

--- frontends/web/src/components/icon/logo.jsx
+++ frontends/web/src/components/icon/logo.jsx
@@ -6,8 +6,10 @@ import LTC from '../../assets/icons/litecoin.svg';
 const logoMap = {
     btc: BTC,
     tbtc: BTC,
+    'btc-p2wpkh-p2sh': BTC,
     'tbtc-p2wpkh-p2sh': BTC,
     ltc: LTC,
+    'ltc-p2wpkh-p2sh': LTC,
     'tltc-p2wpkh-p2sh': LTC
 };
 
