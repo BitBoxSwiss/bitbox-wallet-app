8a03afe952772c13948d266ba68b22fe857609d4
Tadashi Jeffrey Okazaki <originalblend337@gmail.com>
2018-04-30 18:14:52 +0900

    frontend: basic syntax fixes

--- frontends/web/src/routes/settings/settings.jsx
+++ frontends/web/src/routes/settings/settings.jsx
@@ -1,45 +1,41 @@
 import { Component } from 'preact';
 import { ButtonLink } from '../../components/forms';
 import { translate } from 'react-i18next';
-
 import { apiGet } from '../../utils/request';
-
 import Reset from './components/reset';
 import MobilePairing from './components/mobile-pairing';
 import UpgradeFirmware from './components/upgradefirmware';
 import LanguageSwitch from './components/language-switch';
 
 @translate()
+
 export default class Settings extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            version: null
-        };
-    }
+  state = {
+    version: null,
+  }
 
-    componentDidMount() {
-        apiGet('version').then(result => this.setState({ version: result }));
-        apiGet('devices/' + this.props.deviceID + '/info').then(({ sdcard }) => {
-            if (sdcard) {
-                alert('Keep the SD card stored securely unless you want to manage backups.');
-            }
-        });
-    }
+  componentDidMount() {
+    apiGet('version').then(result => this.setState({ version: result }));
+    apiGet('devices/' + this.props.deviceID + '/info').then(({ sdcard }) => {
+      if (sdcard) {
+        alert('Keep the SD card stored securely unless you want to manage backups.');
+      }
+    });
+  }
 
-    render({ t, deviceID }, { version }) {
-        return (
-            <div style="padding-left: 1rem;">
-                <h1>BitBox</h1>
-                {version ? <p>Version: {version}</p> : null}
-                <p><Reset deviceID={deviceID} /></p>
-                <p><MobilePairing deviceID={deviceID} /></p>
-                <p><UpgradeFirmware deviceID={deviceID} /></p>
-                <p><LanguageSwitch /></p>
-                <ButtonLink href={`/manage-backups/${deviceID}`}>
-                    { t('device.manageBackups') }
-                </ButtonLink>
-            </div>
-        );
-    }
+  render({ t, deviceID }, { version }) {
+    return (
+      <div style="padding-left: 1rem;">
+        <h1>BitBox</h1>
+        {version ? <p>Version: {version}</p> : null}
+        <p><Reset deviceID={deviceID} /></p>
+        <p><MobilePairing deviceID={deviceID} /></p>
+        <p><UpgradeFirmware deviceID={deviceID} /></p>
+        <p><LanguageSwitch /></p>
+        <ButtonLink href={`/manage-backups/${deviceID}`}>
+          { t('device.manageBackups') }
+        </ButtonLink>
+      </div>
+    );
+  }
 }
