5977502afc2ff4a76db858198a898b4313b92e36
Enrique Erne <enrique.erne@shiftdevices.com>
2018-05-07 12:01:51 +0200

    frontend: hide zero balance

--- frontends/web/src/components/balance/balance.jsx
+++ frontends/web/src/components/balance/balance.jsx
@@ -2,14 +2,21 @@ import { h } from 'preact';
 import style from './balance.css';
 
 export default function Balance({ name, amount, unit, children }) {
-  return (
-    <header className={style.balance}>
-      <h2 className={style.amount}>
-        {amount}
-        {' '}
-        <span className={style.unit}>{unit}</span>
-      </h2>
-      {children}
-    </header>
-  );
+    if (!amount || amount === '0') {
+        return (
+            <header className={style.balance}>
+                {children}
+            </header>
+        );
+    }
+    return (
+        <header className={style.balance}>
+            <h2 className={style.amount}>
+                {amount}
+                {' '}
+                <span className={style.unit}>{unit}</span>
+            </h2>
+            {children}
+        </header>
+    );
 }
