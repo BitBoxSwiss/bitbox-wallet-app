ead5016289d274654039917482c59fbf11ac748b
Enrique Erne <enrique.erne@shiftdevices.com>
2018-05-09 16:46:50 +0200

    frontend: adds light border around the qrcode

--- frontends/web/src/routes/account/receive/qrcode.jsx
+++ frontends/web/src/routes/account/receive/qrcode.jsx
@@ -1,10 +1,12 @@
 import { h } from 'preact';
 import { apiURL } from '../../../utils/request';
+import style from './qrcode.css';
 
 export default function QRCode({ data }) {
     return (
         <img
             width={256}
+            className={style.qrcode}
             src={apiURL('qr?data=' + encodeURIComponent(data))}
         />
     );
