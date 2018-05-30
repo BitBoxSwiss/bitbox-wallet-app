ff5f7d3b3900df57d54feb00d5e41424e5f7b28d
Tadashi Jeffrey Okazaki <originalblend337@gmail.com>
2018-05-30 14:19:00 +0900

    frontend: update language switcher to use dialog

--- frontends/web/src/components/language/language.jsx
+++ frontends/web/src/components/language/language.jsx
@@ -1,47 +1,77 @@
 import { Component } from 'preact';
 import { translate } from 'react-i18next';
+import { Button } from '../forms';
 import style from './language.css';
 
 @translate()
 export default class LanguageSwitcher extends Component {
-  constructor(props) {
-    super(props);
-    this.languages = [
-      { code: 'en', display: 'English' },
-      { code: 'de', display: 'Deutsch' },
-    ];
-    const inverse = {};
-    this.languages.forEach((obj, index) => {
-      inverse[obj.code] = index;
-    });
-    this.state = {
-      selectedIndex: inverse[props.i18n.language] || 0,
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.languages = [
+            { code: 'en', display: 'English' },
+            { code: 'de', display: 'Deutsch' },
+        ];
+        const inverse = {};
+        this.languages.forEach((obj, index) => {
+            inverse[obj.code] = index;
+        });
+        this.state = {
+            selectedIndex: inverse[props.i18n.language] || 0,
+            activeDialog: false,
+        };
+    }
 
-  changeLanguage = ({ target }) => {
-    const index = parseInt(target.selectedOptions[0].dataset.index);
-    this.setState({ selectedIndex: index });
-    this.context.i18n.changeLanguage(target.value);
-  }
+    changeLanguage = ({ target }) => {
+        const langCode = target.dataset.code;
+        const index = parseInt(target.dataset.index);
+        this.setState({ selectedIndex: index });
+        this.context.i18n.changeLanguage(langCode);
+    }
 
-  render({ t, i18n }, { selectedIndex }) {
-    return (
-      <div class={style.languageSelect}>
-        <div class={style.caret}>&#9660;</div>
-        <select
-          ref={selectBox => this.selectBox = selectBox}
-          onChange={this.changeLanguage}>
-          {
-            this.languages.map((lang, i) => {
-              const selected = selectedIndex === i;
-              return (
-                <option selected={selected} value={lang.code} data-index={i}>{lang.display}</option>
-              );
-            })
-          }
-        </select>
-      </div>
-    );
-  }
+    render({
+        t,
+        i18n,
+    }, {
+        selectedIndex,
+        activeDialog,
+    }) {
+        return (
+            <div class={style.languageSelect}>
+                <Button
+                    type="button"
+                    transparent
+                    onClick={() => this.setState({ activeDialog: true })}>
+                    {this.languages[selectedIndex].display}
+                </Button>
+                {
+                    activeDialog && (
+                        <div class={['overlay', activeDialog ? 'active' : ''].join(' ')}>
+                            <div class={['modal', 'small', activeDialog ? 'active' : ''].join(' ')}>
+                                <h3 class="modalHeader text-center">{t('language.title')}</h3>
+                                <div class="modalContent rows">
+                                    {
+                                        this.languages.map((language, i) => {
+                                            const selected = selectedIndex === i;
+                                            return (
+                                                <button
+                                                    class={[style.language, selected ? style.selected : ''].join(' ')}
+                                                    onClick={this.changeLanguage}
+                                                    data-index={i}
+                                                    data-code={language.code}>
+                                                    {language.display}
+                                                </button>
+                                            );
+                                        })
+                                    }
+                                    <div class="row extra flex flex-row flex-end">
+                                        <Button primary onClick={() => this.setState({ activeDialog: false })}>Done</Button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+        );
+    }
 }
