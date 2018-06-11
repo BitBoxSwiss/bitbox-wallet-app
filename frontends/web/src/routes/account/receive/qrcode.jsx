0f34b3b3664d438bb8956ebe0558fc2abef0c8d7
Kaspar Etter <ke@shiftdevices.com>
2018-06-11 15:48:21 +0200

    frontend: improve error handling of QR code

--- frontends/web/src/routes/account/receive/qrcode.jsx
+++ frontends/web/src/routes/account/receive/qrcode.jsx
@@ -3,7 +3,8 @@ import { apiGet } from '../../../utils/request';
 import style from './qrcode.css';
 
 export default function QRCode({ data }) {
-    apiGet('qr?data=' + encodeURIComponent(data)).then(src => this.img.setAttribute('src', src));
+    apiGet('qr?data=' + encodeURIComponent(data)).then(src => this.img.setAttribute('src', src))
+        .catch(() => this.img.setAttribute('hidden', true));
     return (
         <img
             ref={ref => this.img = ref}
