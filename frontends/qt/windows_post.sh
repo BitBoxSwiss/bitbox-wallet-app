9656202311059b2fadc84e645a46e9762d178673
Marko Bencun <mb@shiftdevices.com>
2018-06-20 15:55:10 +0200

    qt: forgot file

---
+++ frontends/qt/windows_post.sh
@@ -0,0 +1,7 @@
+sh ./genassets.sh
+cp build/assets.rcc build/windows/
+cp server/libserver.dll build/windows/
+windeployqt build/windows/BitBox.exe
+cp /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Community/VC/Redist/MSVC/14.14.26405/x64/Microsoft.VC141.CRT/msvcp140.dll build/windows/
+cp "/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Redist/MSVC/14.14.26405/x64/Microsoft.VC141.CRT/vccorlib140.dll" build/windows/
+cp "/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Redist/MSVC/14.14.26405/x64/Microsoft.VC141.CRT/vcruntime140.dll" build/windows/
