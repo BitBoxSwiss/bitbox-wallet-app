BitBoxSwiss/bitbox-wallet-app

commit 7b13a3e3da7b203e93a4c52d8e2fe726cd863ae3

frontend: sidebar device only darkish

frontends/web/src/components/sidebar/sidebar.css
@@ -71,6 +71,15 @@ a.sidebar-active .stacked img:last-child,
   -webkit-backface-visibility: hidden;
 }
 
+.single img {
+    opacity: .6;
+    transition: opacity 0.2s ease;
+}
+
+.sidebar a:hover .single img {
+    opacity: 1;
+}
+
 .sidebar_label {
   display: block;
   color: var(--color-secondary);

frontends/web/src/components/sidebar/sidebar.jsx
@@ -60,8 +60,7 @@ class Sidebar extends Component {
                         deviceIDs.map(deviceID => (
                             <div>
                                 <Link activeClassName="sidebar-active" class="settings" href={`/device/${deviceID}`} title={ t('sidebar.device') }>
-                                    <div class="stacked">
-                                        <img draggable="false" className="sidebar_settings" src={deviceSettings_GREY} alt={ t('sidebar.device') } />
+                                    <div class="single">
                                         <img draggable="false" className="sidebar_settings" src={deviceSettings} alt={ t('sidebar.device') } />
                                     </div>
                                     {/* <span className="sidebar_label">{ t('sidebar.settings') }</span> */}

commit b64a927d21404f11cd43c926acc7659d745b8484

frontend: improve wording of send screen and security warnings
Closes #355 and fixes sidebar issue introduced in
7b13a3e3da7b203e93a4c52d8e2fe726cd863ae3.

frontends/web/src/components/sidebar/sidebar.css
@@ -76,6 +76,7 @@ a.sidebar-active .stacked img:last-child,
     transition: opacity 0.2s ease;
 }
 
+a.sidebar-active .single img,
 .sidebar a:hover .single img {
     opacity: 1;
 }

frontends/web/src/components/sidebar/sidebar.jsx
@@ -8,7 +8,6 @@ import Logo from '../icon/logo';
 import settings from '../../assets/icons/settings-alt.svg';
 import settings_GREY from '../../assets/icons/settings-alt_disabled.svg';
 import deviceSettings from '../../assets/icons/wallet-dark.svg';
-import deviceSettings_GREY from '../../assets/icons/wallet-light.svg';
 import ejectIcon from '../../assets/icons/eject.svg';
 
 const labelMap = {
@@ -50,7 +49,7 @@ class Sidebar extends Component {
                 <div className="sidebar_drawer"></div>
                 <div className="sidebar_bottom">
                     {
-                        (debug && deviceIDs.length == 0) && (
+                        (debug && deviceIDs.length === 0) && (
                             <a href="#" onClick={eject}>
                                 <img draggable="false" className="sidebar_settings" src={ejectIcon} alt={t('sidebar.leave')} />
                             </a>

frontends/web/src/routes/account/send/send.jsx
@@ -336,7 +336,7 @@ export default class Send extends Component {
                                     {t('button.back')}
                                 </Button>
                                 <Button primary onClick={this.send} disabled={this.sendDisabled() || !valid}>
-                                    {t('button.send')}
+                                    {t('send.button')}
                                 </Button>
                             </div>
                         </div>
@@ -377,7 +377,7 @@ export default class Send extends Component {
                                         </div>
                                     </div>
                                     <p class={['label', style.confirmationLabel].join(' ')}>
-                                        {t('send.total.label')}
+                                        {t('send.confirm.total')}
                                     </p>
                                     <p class={[style.confirmationValue, style.standOut].join(' ')}>
                                         {proposedTotal && proposedTotal.amount + ' ' + proposedTotal.unit || 'N/A'}