163a0f2dcc43574b4f0be3a6042d500ed1e3898d
Enrique Erne <enrique.erne@shiftdevices.com>
2018-07-02 10:43:44 +0200

    frontend: use new uglify with es6 support
    related https://github.com/developit/preact-cli/issues/593

--- frontends/web/preact.config.js
+++ frontends/web/preact.config.js
@@ -1,3 +1,5 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 /**
  * Function that mutates original webpack config.
  * Supports asynchronous changes when promise is returned.
@@ -11,4 +13,39 @@ export default function(config, env, helpers) {
         console.log('disabling source maps for production build'); // eslint-disable-line no-console
         helpers.getPluginsByName(config, 'UglifyJsPlugin')[0].plugin.options.sourceMap = false;
     }
+
+    const { index } = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
+    config.plugins.splice(index, 1);
+
+    // org config https://github.com/developit/preact-cli/blob/3f670a44f0751a3a242dfbf466d235a4205990b2/src/lib/webpack/webpack-client-config.js#L107
+    config.plugins.push(new UglifyJsPlugin({
+        parallel: true,
+        sourceMap: !env.production,
+        uglifyOptions: {
+            mangle: true,
+            ecma: 7,
+            output: {
+                ecma: 7
+            },
+            compress: {
+                passes: 3,
+                pure_funcs: [
+                    'classCallCheck',
+                    '_classCallCheck',
+                    '_possibleConstructorReturn',
+                    'Object.freeze',
+                    'invariant',
+                    'warning'
+                ]
+            }
+        }
+    }));
+
+    // const { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
+    // rule.options.plugins.push('transform-regenerator');
+    // rule.options.plugins.push(['transform-runtime', {
+    //     helpers: false,
+    //     polyfill: false,
+    //     regenerator: true
+    // }]);
 }
