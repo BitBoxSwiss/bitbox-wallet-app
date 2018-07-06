ba5eb43121aaa065fa74d7a0fa4972c051378e6b
Enrique Erne <enrique.erne@shiftdevices.com>
2018-07-07 00:40:12 +0200

    frontend: inbrowser error in dev mode

--- frontends/web/preact.config.js
+++ frontends/web/preact.config.js
@@ -8,37 +8,40 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
  * @param {object} env - options passed to CLI.
  * @param {WebpackConfigHelpers} helpers - object with useful helpers when working with config.
  **/
-export default function(config, env, helpers) {
-    if (env.production) {
-        console.log('disabling source maps for production build'); // eslint-disable-line no-console
-        helpers.getPluginsByName(config, 'UglifyJsPlugin')[0].plugin.options.sourceMap = false;
+export default function (config, env, helpers) {
+
+    if (!env.production) {
+        config.devServer.overlay = true;
+        config.devServer.hot = false;
     }
 
-    const { index } = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
-    config.plugins.splice(index, 1);
+    if (env.production) {
+        const { index } = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
+        config.plugins.splice(index, 1);
 
-    // org config https://github.com/developit/preact-cli/blob/3f670a44f0751a3a242dfbf466d235a4205990b2/src/lib/webpack/webpack-client-config.js#L107
-    config.plugins.push(new UglifyJsPlugin({
-        parallel: true,
-        sourceMap: !env.production,
-        uglifyOptions: {
-            mangle: true,
-            ecma: 7,
-            output: {
-                ecma: 7
-            },
-            compress: {
-                passes: 3,
-                pure_funcs: [
-                    'classCallCheck',
-                    '_classCallCheck',
-                    '_possibleConstructorReturn',
-                    'Object.freeze',
-                    'invariant',
-                    'warning'
-                ]
+        // org config https://github.com/developit/preact-cli/blob/3f670a44f0751a3a242dfbf466d235a4205990b2/src/lib/webpack/webpack-client-config.js#L107
+        config.plugins.push(new UglifyJsPlugin({
+            parallel: true,
+            sourceMap: !env.production,
+            uglifyOptions: {
+                mangle: true,
+                ecma: 7,
+                output: {
+                    ecma: 7
+                },
+                compress: {
+                    passes: 3,
+                    pure_funcs: [
+                        'classCallCheck',
+                        '_classCallCheck',
+                        '_possibleConstructorReturn',
+                        'Object.freeze',
+                        'invariant',
+                        'warning'
+                    ]
+                }
             }
-        }
-    }));
+        }));
+    }
 
 }
