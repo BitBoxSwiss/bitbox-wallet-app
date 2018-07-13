c4aac061c14a6aefadd8b3c817c3568d537afa1f
Marko Bencun <mb@shiftdevices.com>
2018-07-13 13:54:20 +0200

    Revert "frontend: use new uglify with es6 support"
    This reverts commit 163a0f2dcc43574b4f0be3a6042d500ed1e3898d.

--- frontends/web/preact.config.js
+++ frontends/web/preact.config.js
@@ -1,5 +1,3 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
 /**
  * Function that mutates original webpack config.
  * Supports asynchronous changes when promise is returned.
@@ -14,35 +12,4 @@ export default function (config, env, helpers) {
         config.devServer.overlay = true;
         config.devServer.hot = false;
     }
-
-    if (env.production) {
-        const { index } = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
-        config.plugins.splice(index, 1);
-
-        // org config https://github.com/developit/preact-cli/blob/3f670a44f0751a3a242dfbf466d235a4205990b2/src/lib/webpack/webpack-client-config.js#L107
-        config.plugins.push(new UglifyJsPlugin({
-            parallel: true,
-            sourceMap: !env.production,
-            uglifyOptions: {
-                mangle: true,
-                ecma: 7,
-                output: {
-                    ecma: 7,
-                    beautify: false
-                },
-                compress: {
-                    passes: 3,
-                    pure_funcs: [
-                        'classCallCheck',
-                        '_classCallCheck',
-                        '_possibleConstructorReturn',
-                        'Object.freeze',
-                        'invariant',
-                        'warning'
-                    ]
-                }
-            }
-        }));
-    }
-
 }
