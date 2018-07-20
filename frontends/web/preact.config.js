609162198b6c2c085674ed54d35a4a19c06ba831
Kaspar Etter <ke@shiftdevices.com>
2018-07-20 14:27:18 +0200

    frontend: add TypeScript support

--- frontends/web/preact.config.js
+++ frontends/web/preact.config.js
@@ -1,3 +1,5 @@
+import preactCliTypeScript from 'preact-cli-plugin-typescript';
+
 /**
  * Function that mutates original webpack config.
  * Supports asynchronous changes when promise is returned.
@@ -5,11 +7,11 @@
  * @param {object} config - original webpack config.
  * @param {object} env - options passed to CLI.
  * @param {WebpackConfigHelpers} helpers - object with useful helpers when working with config.
- **/
+ */
 export default function (config, env, helpers) {
-
     if (!env.production) {
         config.devServer.overlay = true;
         config.devServer.hot = false;
     }
+    preactCliTypeScript(config);
 }
