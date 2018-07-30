8db56d3a30877cf504a758c967fd441a6492d7da
Marko Bencun <mb@shiftdevices.com>
2018-07-30 11:38:42 +0200

    LICENSE headers

--- backend/coins/btc/electrum/client/client.go
+++ backend/coins/btc/electrum/client/client.go
@@ -1,3 +1,17 @@
+// Copyright 2018 Shift Devices AG
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package client implements an Electrum JSON RPC client.
 // See https://github.com/kyuupichan/electrumx/blob/159db3f8e70b2b2cbb8e8cd01d1e9df3fe83828f/docs/PROTOCOL.rst
 package client
--- frontends/web/src/utils/polyfill.js
+++ frontends/web/src/utils/polyfill.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2018 Shift Devices AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 if (!String.prototype.startsWith) {
     String.prototype.startsWith = function(search, pos) {
         return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
