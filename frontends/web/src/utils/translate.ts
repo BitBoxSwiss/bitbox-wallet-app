0dc25de14e511f118387f7bf55c9cb3179cf35e8
Kaspar Etter <ke@shiftdevices.com>
2018-08-22 12:32:51 +0200

    frontend: add types for the translate function and interpolation values

---
+++ frontends/web/src/utils/translate.ts
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2018 Shift Devices AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Instead of 'https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/react-i18next'.
+
+/**
+ * This interface describes the interpolation values of 'i18next'.
+ */
+export interface InterpolationValues {
+    readonly [key: string]: string;
+}
+
+/**
+ * This type describes the translate method of 'react-i18next'.
+ */
+export type Translate = (i18nKey: string, values?: InterpolationValues) => string;
