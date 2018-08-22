aedfdf45a6d818f39f980f22d50e2d4438facc0a
Kaspar Etter <ke@shiftdevices.com>
2018-08-22 16:28:08 +0200

    frontend: use the 'typings-for-css-modules-loader' only for first rule
    Otherwise the global CSS in 'src/style' is also transformed.

--- frontends/web/preact.config.js
+++ frontends/web/preact.config.js
@@ -34,25 +34,39 @@ export default function (config, env, helpers) {
         helpers.getPluginsByName(config, 'UglifyJsPlugin')[0].plugin.options.sourceMap = false;
     }
 
-    // https://github.com/wub/preact-cli-plugin-typescript
+    // Support for TypeScript (https://github.com/wub/preact-cli-plugin-typescript):
     preactCliTypeScript(config);
 
-    // Taken from https://medium.com/@sapegin/css-modules-with-typescript-and-webpack-6b221ebe5f10.
-    const loader = {
+    // In order to import CSS Modules (https://github.com/css-modules/css-modules) from TypeScript,
+    // we need to replace the existing CSS Loader (https://github.com/webpack-contrib/css-loader)
+    // with https://github.com/Jimdo/typings-for-css-modules-loader, which generates '*.css.d.ts'
+    // files that export the class names as strings. With TypeScript, you can no longer reference
+    // CSS classes that do not exist. Use `import * as style from './styles.css';` to import them.
+
+    // Retrieve the rules in which the CSS Loader is used. The CSS Loader is used twice: The first
+    // time only for CSS (and other) files in 'src/components' and 'src/routes', the second time for
+    // CSS (and other) files everywhere else. This distinction is important because the CSS classes
+    // are only renamed to avoid name collisions in the former but not the latter rule. This is why
+    // the styles imported in 'src/index.js' can be used globally. Alternatively, the loaders could
+    // be suppressed there with `import '!style-loader!css-loader!./style';`. (If you want a single
+    // CSS rule to be available globally, you can set its scope with `:global(.className) {…}` (see
+    // https://github.com/css-modules/css-modules#exceptions).) We only want to replace the first:
+    const rule = helpers.getLoadersByName(config, 'css-loader')[0];
+
+    config.module.loaders[rule.ruleIndex].loader[rule.loaderIndex] = {
         loader: 'typings-for-css-modules-loader',
         options: {
-            modules: true,
+            // Instead of exporting CSS class names as properties of an interface (allowing dashes),
+            // export the names directly and transform dashes to valid variable names with camelCase
+            // (see https://github.com/Jimdo/typings-for-css-modules-loader#namedexport-option):
             camelCase: true,
             namedExport: true,
             // Existing options (see https://github.com/Jimdo/typings-for-css-modules-loader#usage):
+            modules: true,
             sourceMap: false,
             importLoaders: 1,
-            localIdentName: '[local]__[hash:base64:5]',
+            // See https://github.com/webpack/loader-utils#interpolatename for more options:
+            localIdentName: '[path][name]-[local]', // Was '[local]__[hash:base64:5]' before.
         }
     };
-
-    const rules = helpers.getLoadersByName(config, 'css-loader');
-    for (const rule of rules) {
-        config.module.loaders[rule.ruleIndex].loader[rule.loaderIndex] = loader;
-    }
 }
