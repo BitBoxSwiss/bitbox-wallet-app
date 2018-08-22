f73afd866c4e8f19557a9d341479c19b8dcf81e7
Kaspar Etter <ke@shiftdevices.com>
2018-08-22 11:53:52 +0200

    frontend: add 'typings-for-css-modules-loader' as a dev dependency
    This is required for importing CSS modules in TypeScript.

--- frontends/web/preact.config.js
+++ frontends/web/preact.config.js
@@ -29,8 +29,30 @@ export default function (config, env, helpers) {
         config.devServer.overlay = true;
         config.devServer.hot = false;
     }
+
     if (env.production) {
         helpers.getPluginsByName(config, 'UglifyJsPlugin')[0].plugin.options.sourceMap = false;
     }
+
+    // https://github.com/wub/preact-cli-plugin-typescript
     preactCliTypeScript(config);
+
+    // Taken from https://medium.com/@sapegin/css-modules-with-typescript-and-webpack-6b221ebe5f10.
+    const loader = {
+        loader: 'typings-for-css-modules-loader',
+        options: {
+            modules: true,
+            camelCase: true,
+            namedExport: true,
+            // Existing options (see https://github.com/Jimdo/typings-for-css-modules-loader#usage):
+            sourceMap: false,
+            importLoaders: 1,
+            localIdentName: '[local]__[hash:base64:5]',
+        }
+    };
+
+    const rules = helpers.getLoadersByName(config, 'css-loader');
+    for (const rule of rules) {
+        config.module.loaders[rule.ruleIndex].loader[rule.loaderIndex] = loader;
+    }
 }
