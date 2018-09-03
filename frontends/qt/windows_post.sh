33ea669b8fb30106d379a8a961ae2966fbeaf846
Marko Bencun <mb@shiftdevices.com>
2018-09-03 14:47:08 +0200

    fixes to the windows build script
    qmake needs to be called in a different dir, it overwrites the
    Makefile.
    
    The `go get` packages are not needed to build the app.

--- frontends/qt/windows_post.sh
+++ frontends/qt/windows_post.sh
@@ -19,4 +19,4 @@ windeployqt build/windows/BitBox.exe
 cp /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Community/VC/Redist/MSVC/14.14.26405/x64/Microsoft.VC141.CRT/msvcp140.dll build/windows/
 cp "/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Redist/MSVC/14.14.26405/x64/Microsoft.VC141.CRT/vccorlib140.dll" build/windows/
 cp "/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Redist/MSVC/14.14.26405/x64/Microsoft.VC141.CRT/vcruntime140.dll" build/windows/
-cp "/c/mingw/bin/libssp-0.dll" build/windows/
+cp $MINGW_BIN/libssp-0.dll build/windows/
