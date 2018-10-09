bc51e714021b2eae9219bf400d7d106e58562a94
Tadashi Jeffrey Okazaki <originalblend337@gmail.com>
2018-10-09 13:23:34 +0900

    frontend: tab / shift + tab cycles through within dialog content

--- frontends/web/src/components/dialog/dialog.jsx
+++ frontends/web/src/components/dialog/dialog.jsx
@@ -18,8 +18,12 @@ import { Component, h } from 'preact';
 import * as style from './dialog.css';
 
 export default class Dialog extends Component {
-    state = {
-        active: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false,
+            currentTab: 0,
+        };
     }
 
     componentDidMount() {
@@ -27,17 +31,60 @@ export default class Dialog extends Component {
     }
 
     componentWillUnmount() {
-        this.setState({ active: false });
+        document.removeEventListener('keydown', this.controlKeys);
+        this.setState({
+            active: false,
+            currentTab: 0,
+        });
+    }
+
+    focusWithin = () => {
+        this.focusableChildren = this.modalContent.querySelectorAll('a, button, input, textarea');
+        for (let c of this.focusableChildren) {
+            c.classList.add('tabbable');
+        }
+        document.addEventListener('keydown', this.controlKeys);
+    }
+
+    addTabIndex = currentTab => {
+        let next = currentTab + 1;
+        if (next >= this.focusableChildren.length) {
+            next = 0;
+        }
+        this.setState({ currentTab: next });
+    }
+
+    subtractTabIndex = currentTab => {
+        let previous = currentTab - 1;
+        if (previous < 0) {
+            previous = this.focusableChildren.length - 1;
+        }
+        this.setState({ currentTab: previous });
+    }
+
+    controlKeys = e => {
+        const { currentTab } = this.state;
+        const isTab = e.keyCode === 9;
+        if (isTab) { e.preventDefault(); }
+        if (isTab && e.shiftKey) {
+            this.focusableChildren[currentTab].focus();
+            this.subtractTabIndex(currentTab);
+        } else if (isTab) {
+            this.focusableChildren[currentTab].focus();
+            this.addTabIndex(currentTab);
+        }
     }
 
     activate = () => {
-        this.setState({ active: true });
+        this.setState({ active: true }, () => {
+            this.focusWithin();
+        });
     }
 
     render({
-        title,
+        title = null,
+        small = false,
         children,
-        small,
     },{
         active,
     }) {
@@ -45,8 +92,12 @@ export default class Dialog extends Component {
         return (
             <div class={[style.overlay, activeClass].join(' ')}>
                 <div class={[style.modal, activeClass, small ? style.small : ''].join(' ')}>
-                    <h3 class={style.modalHeader}>{title}</h3>
-                    <div class={style.modalContent}>
+                    {
+                        title && (
+                            <h3 class={style.modalHeader}>{title}</h3>
+                        )
+                    }
+                    <div class={[style.modalContent, title ? '' : 'first'].join(' ')} ref={el => this.modalContent = el}>
                         {children}
                     </div>
                 </div>
