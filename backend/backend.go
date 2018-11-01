BitBoxSwiss/bitbox-wallet-app

commit a7967cd0b261774e86831d35d5534716527cbb1e

backend/rates: fix race condition and redundant notifications
Rates were observed when coins were initialized, which is after
unlock. The first rates notification happens immediately when the
backend is initalized, so the frontend misses the first notification,
and waits a full minute for the first one arrives.

Also, for each coin, one notification was made, but there is a single
global observer/rates variable for all coins in the frontend, so it
does redundant work.

Now, the rates are removed from coins and are part of the backend, to
match how they are used in the frontend. Can move it back if we ever
do per-coin fetches, but that seems unlikely.

backend/backend.go
@@ -106,17 +106,20 @@ type Backend struct {
 // NewBackend creates a new backend with the given arguments.
 func NewBackend(arguments *arguments.Arguments) *Backend {
 	log := logging.Get().WithGroup("backend")
-	return &Backend{
+	backend := &Backend{
 		arguments: arguments,
 		config:    config.NewConfig(arguments.ConfigFilename()),
 		events:    make(chan interface{}, 1000),
 
-		devices:      map[string]device.Interface{},
-		keystores:    keystore.NewKeystores(),
-		coins:        map[string]coin.Coin{},
-		ratesUpdater: btc.NewRatesUpdater(),
-		log:          log,
+		devices:   map[string]device.Interface{},
+		keystores: keystore.NewKeystores(),
+		coins:     map[string]coin.Coin{},
+		log:       log,
 	}
+	ratesUpdater := btc.NewRatesUpdater()
+	ratesUpdater.Observe(func(event observable.Event) { backend.events <- event })
+	backend.ratesUpdater = ratesUpdater
+	return backend
 }
 
 func (backend *Backend) addAccount(
@@ -262,23 +265,23 @@ func (backend *Backend) Coin(code string) coin.Coin {
 	switch code {
 	case "rbtc":
 		servers := []*rpc.ServerInfo{{Server: "127.0.0.1:52001", TLS: false, PEMCert: ""}}
-		coin = btc.NewCoin("rbtc", "RBTC", &chaincfg.RegressionNetParams, dbFolder, servers, "", nil)
+		coin = btc.NewCoin("rbtc", "RBTC", &chaincfg.RegressionNetParams, dbFolder, servers, "")
 	case coinTBTC:
 		servers := backend.defaultElectrumXServers(code)
 		coin = btc.NewCoin(coinTBTC, "TBTC", &chaincfg.TestNet3Params, dbFolder, servers,
-			"https://testnet.blockchain.info/tx/", backend.ratesUpdater)
+			"https://testnet.blockchain.info/tx/")
 	case coinBTC:
 		servers := backend.defaultElectrumXServers(code)
 		coin = btc.NewCoin(coinBTC, "BTC", &chaincfg.MainNetParams, dbFolder, servers,
-			"https://blockchain.info/tx/", backend.ratesUpdater)
+			"https://blockchain.info/tx/")
 	case coinTLTC:
 		servers := backend.defaultElectrumXServers(code)
 		coin = btc.NewCoin(coinTLTC, "TLTC", &ltc.TestNet4Params, dbFolder, servers,
-			"http://explorer.litecointools.com/tx/", backend.ratesUpdater)
+			"http://explorer.litecointools.com/tx/")
 	case coinLTC:
 		servers := backend.defaultElectrumXServers(code)
 		coin = btc.NewCoin(coinLTC, "LTC", &ltc.MainNetParams, dbFolder, servers,
-			"https://insight.litecore.io/tx/", backend.ratesUpdater)
+			"https://insight.litecore.io/tx/")
 	case coinETH:
 		coin = eth.NewCoin(code, params.MainnetChainConfig, "https://etherscan.io/address/")
 	case coinTETH:
@@ -287,7 +290,6 @@ func (backend *Backend) Coin(code string) coin.Coin {
 		panic(errp.Newf("unknown coin code %s", code))
 	}
 	coin.Init()
-	coin.Observe(func(event observable.Event) { backend.events <- event })
 	backend.coins[code] = coin
 	return coin
 }

backend/coins/btc/coin.go
@@ -43,7 +43,6 @@ type Coin struct {
 	servers               []*rpc.ServerInfo
 	blockExplorerTxPrefix string
 
-	ratesUpdater coinpkg.RatesUpdater
 	observable.Implementation
 
 	blockchain blockchain.Interface
@@ -60,7 +59,6 @@ func NewCoin(
 	dbFolder string,
 	servers []*rpc.ServerInfo,
 	blockExplorerTxPrefix string,
-	ratesUpdater coinpkg.RatesUpdater,
 ) *Coin {
 	coin := &Coin{
 		code:                  code,
@@ -69,7 +67,6 @@ func NewCoin(
 		dbFolder:              dbFolder,
 		servers:               servers,
 		blockExplorerTxPrefix: blockExplorerTxPrefix,
-		ratesUpdater:          ratesUpdater,
 
 		log: logging.Get().WithGroup("coin").WithField("code", code),
 	}
@@ -106,10 +103,6 @@ func (coin *Coin) Init() {
 			})
 		}
 	})
-
-	if coin.ratesUpdater != nil {
-		coin.ratesUpdater.Observe(coin.Notify)
-	}
 }
 
 // Code implements coin.Coin.
@@ -133,11 +126,6 @@ func (coin *Coin) FormatAmount(amount coinpkg.Amount) string {
 		new(big.Rat).SetFrac(amount.BigInt(), big.NewInt(unitSatoshi)).FloatString(8),
 		"0.",
 	)
-}
-
-// RatesUpdater returns current exchange rates.
-func (coin *Coin) RatesUpdater() coinpkg.RatesUpdater {
-	return coin.ratesUpdater
 }
 
 // Blockchain connects to a blockchain backend.

backend/coins/btc/maketx/maketx_test.go
@@ -37,7 +37,7 @@ import (
 
 var noDust = btcutil.Amount(0)
 
-var tbtc = btc.NewCoin("tbtc", "TBTC", &chaincfg.TestNet3Params, ".", []*rpc.ServerInfo{}, "https://testnet.blockchain.info/tx/", nil)
+var tbtc = btc.NewCoin("tbtc", "TBTC", &chaincfg.TestNet3Params, ".", []*rpc.ServerInfo{}, "https://testnet.blockchain.info/tx/")
 
 // For reference, tx vsizes assuming two outputs (normal + change), for N inputs:
 // 1 inputs: 226

backend/coins/btc/rates.go
@@ -85,7 +85,7 @@ func (updater *RatesUpdater) update() {
 	updater.last = rates
 	updater.log.WithField("data", spew.Sprintf("%v", rates)).Debug("Exchange rates changed.")
 	updater.Notify(observable.Event{
-		Subject: "coins/rates",
+		Subject: "rates",
 		Action:  action.Replace,
 		Object:  rates,
 	})

frontends/web/src/components/rates/rates.tsx
@@ -129,11 +129,14 @@ function Conversion({
     } else {
         mainnetCoin = coin as MainnetCoin;
     }
-    const value = rates[mainnetCoin][active] * Number(amount.amount);
+    let formattedValue = '';
+    if (rates[mainnetCoin]) {
+        formattedValue = formatAsCurrency(rates[mainnetCoin][active] * Number(amount.amount));
+    }
     if (tableRow) {
         return (
             <tr className={unstyled ? '' : style.fiatRow}>
-                <td className={unstyled ? '' : style.availableFiatAmount}>{formatAsCurrency(value)}</td>
+                <td className={unstyled ? '' : style.availableFiatAmount}>{formattedValue}</td>
                 <td className={unstyled ? '' : style.availableFiatUnit} onClick={rotateFiat}>{active}</td>
             </tr>
         );
@@ -141,7 +144,7 @@ function Conversion({
     return (
         <span className={style.rates}>
             {children}
-            {formatAsCurrency(value)}
+            {formattedValue}
             {' '}
             <span className={style.unit} onClick={rotateFiat}>{active}</span>
         </span>

commit 15d6a472b73cb1a946d8b712fa438d89e3583efe

backend: relay coin events
Regression from a7967cd0b261774e86831d35d5534716527cbb1e.

backend/backend.go
@@ -288,6 +288,7 @@ func (backend *Backend) Coin(code string) coin.Coin {
 		panic(errp.Newf("unknown coin code %s", code))
 	}
 	backend.coins[code] = coin
+	coin.Observe(func(event observable.Event) { backend.events <- event })
 	return coin
 }
 