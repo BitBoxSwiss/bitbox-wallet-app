62722cd1304f67cb71fc09153fb7ce5654d71905
Marko Bencun <mb@shiftdevices.com>
2019-03-14 10:48:04 +0100

    Dockerfile: update node to 10.x

--- scripts/docker_install.sh
+++ scripts/docker_install.sh
@@ -18,7 +18,7 @@ apt-get update
 apt-get install -y --no-install-recommends curl ca-certificates
 
 # add repository for node/npm
-curl -sL https://deb.nodesource.com/setup_9.x | bash -
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
 
 apt-get install -y --no-install-recommends \
     clang \
