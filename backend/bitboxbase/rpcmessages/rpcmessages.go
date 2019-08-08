ace0f6a6270e56aafd96450a21c6c94bb0dce401
TheCharlatan <seb.kung@gmail.com>
2019-08-09 01:19:31 +0200

    Add RPCMessage file to centralize definition of rpc messages
    The rpcmessage.go file now specifies how to structure the incoming
    arguments of rpccalls, the outgoing reply and the notifications.

---
+++ backend/bitboxbase/rpcmessages/rpcmessages.go
@@ -0,0 +1,53 @@
+package rpcmessages
+
+/*
+Put notification constants here. Notifications for new rpc data should have the format 'OpUCanHas' + 'RPC Method Name'.
+*/
+const (
+	// OpRPCCall is prepended to every rpc response messages, to indicate that the message is rpc response and not a notification.
+	OpRPCCall = "r"
+	// OpUCanHasSampleInfo notifies when new SampleInfo data is available.
+	OpUCanHasSampleInfo = "d"
+)
+
+/*
+Put Incoming Args below this line. They should have the format of 'RPC Method Name' + 'Args'.
+*/
+
+// ResyncBitcoinArgs is an iota that holds the options for the ResyncBitcoin rpc call
+type ResyncBitcoinArgs int
+
+// The ResyncBitcoinArgs has two options. Other resync bitcon from scratch with an IBD, or delete the chainstate and reindex.
+const (
+	Resync ResyncBitcoinArgs = iota
+	Reindex
+)
+
+/*
+Put Response structs below this line. They should have the format of 'RPC Method Name' + 'Response'.
+*/
+
+// GetEnvResponse is the struct that gets sent by the rpc server during a GetSystemEnv call
+type GetEnvResponse struct {
+	Network        string
+	ElectrsRPCPort string
+}
+
+// ResyncBitcoinResponse is the struct that gets sent by the rpc server during a ResyncBitcoin call
+type ResyncBitcoinResponse struct {
+	Success bool
+}
+
+// SampleInfoResponse holds sample information from c-lightning and bitcoind. It is temporary for testing purposes
+type SampleInfoResponse struct {
+	Blocks         int64   `json:"blocks"`
+	Difficulty     float64 `json:"difficulty"`
+	LightningAlias string  `json:"lightningAlias"`
+}
+
+// VerificationProgressResponse is the struct that gets sent by the rpc server during a VerificationProgress rpc call
+type VerificationProgressResponse struct {
+	Blocks               int64   `json:"blocks"`
+	Headers              int64   `json:"headers"`
+	VerificationProgress float64 `json:"verificationProgress"`
+}
