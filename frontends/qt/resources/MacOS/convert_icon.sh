4fe09b27d1ab21508319f0aadd203019b262e220
Marko Bencun <marko@shiftcrypto.ch>
2019-08-20 13:21:33 +0200

    qt: update app logo
    And add conversion scripts for the future.

---
+++ frontends/qt/resources/MacOS/convert_icon.sh
@@ -0,0 +1,8 @@
+convert ../app_icon_source.png -resize 1024x icon_1024x1024x32.png
+convert ../app_icon_source.png -resize 512x icon_512x512x32.png
+convert ../app_icon_source.png -resize 256x icon_256x256x32.png
+convert ../app_icon_source.png -resize 32x icon_32x32x32.png
+convert ../app_icon_source.png -resize 16x icon_16x16x32.png
+png2icns icon.icns icon_1024x1024x32.png icon_512x512x32.png icon_256x256x32.png icon_32x32x32.png icon_16x16x32.png
+rm icon_1024x1024x32.png icon_512x512x32.png icon_256x256x32.png icon_32x32x32.png icon_16x16x32.png
+cp icon.icns Testnet.app/Contents/Resources/icon.icns
---
+++ frontends/qt/resources/win/convert.sh
@@ -0,0 +1,9 @@
+convert ../app_icon_source.png  -bordercolor white -border 0 \
+      \( -clone 0 -resize 16x16 \) \
+      \( -clone 0 -resize 20x20 \) \
+      \( -clone 0 -resize 24x24 \) \
+      \( -clone 0 -resize 32x32 \) \
+      \( -clone 0 -resize 48x48 \) \
+      \( -clone 0 -resize 64x64 \) \
+      \( -clone 0 -resize 256x256 \) \
+      -delete 0 icon.ico
