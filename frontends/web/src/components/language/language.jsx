be07ca8e2d319d5742b63c72c2a00e0c8b1d6534
Tadashi Jeffrey Okazaki <originalblend337@gmail.com>
2019-09-20 16:00:45 +0900

    frontend: fix indentation

--- frontends/web/src/components/language/language.jsx
+++ frontends/web/src/components/language/language.jsx
@@ -126,9 +126,9 @@ export default class LanguageSwitcher extends Component {
                             stroke-width="2"
                             stroke-linecap="round"
                             stroke-linejoin="round">
-                                <circle cx="12" cy="12" r="10"></circle>
-                                <line x1="2" y1="12" x2="22" y2="12"></line>
-                                <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
+                            <circle cx="12" cy="12" r="10"></circle>
+                            <line x1="2" y1="12" x2="22" y2="12"></line>
+                            <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
                         </svg>
                     }>
                     {languages[selectedIndex].code === 'en' ? 'Other languages' : 'English'}
