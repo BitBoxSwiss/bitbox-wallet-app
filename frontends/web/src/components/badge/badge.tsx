9f33f95d5a76228b9206a081845e78fc4ed92167
Tadashi Jeffrey Okazaki <originalblend337@gmail.com>
2019-09-24 16:08:27 +0900

    frontends: badges for accounts

---
+++ frontends/web/src/components/badge/badge.tsx
@@ -0,0 +1,21 @@
+import { Component, h, RenderableProps } from 'preact';
+import * as style from './badge.css';
+
+interface BadgeProps {
+    type: string;
+    className?: string;
+}
+
+class Badge extends Component<BadgeProps> {
+    public render(
+        { type, className, children }: RenderableProps<BadgeProps>,
+    ) {
+        return (
+            <span className={[style.container, style[type], className].join(' ')}>
+                {children}
+            </span>
+        );
+    }
+}
+
+export { Badge };
