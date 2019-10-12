3be29a007ff85c98685ea9e2a480556eba06b867
Marko Bencun <marko@shiftcrypto.ch>
2019-10-12 16:22:07 +0200

    vendor: upgrade btcsuite

--- vendor/github.com/btcsuite/btcd/server.go
+++ vendor/github.com/btcsuite/btcd/server.go
@@ -246,6 +246,14 @@ type server struct {
 	// messages for each filter type.
 	cfCheckptCaches    map[wire.FilterType][]cfHeaderKV
 	cfCheckptCachesMtx sync.RWMutex
+
+	// agentBlacklist is a list of blacklisted substrings by which to filter
+	// user agents.
+	agentBlacklist []string
+
+	// agentWhitelist is a list of whitelisted user agent substrings, no
+	// whitelisting will be applied if the list is empty or nil.
+	agentWhitelist []string
 }
 
 // serverPeer extends the peer to maintain state shared by the server and
@@ -1586,6 +1594,12 @@ func (s *server) handleAddPeerMsg(state *peerState, sp *serverPeer) bool {
 		return false
 	}
 
+	// Disconnect peers with unwanted user agents.
+	if sp.HasUndesiredUserAgent(s.agentBlacklist, s.agentWhitelist) {
+		sp.Disconnect()
+		return false
+	}
+
 	// Ignore new peers if we're shutting down.
 	if atomic.LoadInt32(&s.shutdown) != 0 {
 		srvrLog.Infof("New peer %s ignored - server is shutting down", sp)
@@ -1637,6 +1651,12 @@ func (s *server) handleAddPeerMsg(state *peerState, sp *serverPeer) bool {
 		}
 	}
 
+	// Update the address' last seen time if the peer has acknowledged
+	// our version and has sent us its version as well.
+	if sp.VerAckReceived() && sp.VersionKnown() && sp.NA() != nil {
+		s.addrManager.Connected(sp.NA())
+	}
+
 	return true
 }
 
@@ -1651,30 +1671,27 @@ func (s *server) handleDonePeerMsg(state *peerState, sp *serverPeer) {
 	} else {
 		list = state.outboundPeers
 	}
+
+	// Regardless of whether the peer was found in our list, we'll inform
+	// our connection manager about the disconnection. This can happen if we
+	// process a peer's `done` message before its `add`.
+	if !sp.Inbound() {
+		if sp.persistent {
+			s.connManager.Disconnect(sp.connReq.ID())
+		} else {
+			s.connManager.Remove(sp.connReq.ID())
+			go s.connManager.NewConnReq()
+		}
+	}
+
 	if _, ok := list[sp.ID()]; ok {
 		if !sp.Inbound() && sp.VersionKnown() {
 			state.outboundGroups[addrmgr.GroupKey(sp.NA())]--
-		}
-		if !sp.Inbound() && sp.connReq != nil {
-			s.connManager.Disconnect(sp.connReq.ID())
 		}
 		delete(list, sp.ID())
 		srvrLog.Debugf("Removed peer %s", sp)
 		return
 	}
-
-	if sp.connReq != nil {
-		s.connManager.Disconnect(sp.connReq.ID())
-	}
-
-	// Update the address' last seen time if the peer has acknowledged
-	// our version and has sent us its version as well.
-	if sp.VerAckReceived() && sp.VersionKnown() && sp.NA() != nil {
-		s.addrManager.Connected(sp.NA())
-	}
-
-	// If we get here it means that either we didn't know about the peer
-	// or we purposefully deleted it.
 }
 
 // handleBanPeerMsg deals with banning peers.  It is invoked from the
@@ -2011,14 +2028,19 @@ func (s *server) outboundPeerConnected(c *connmgr.ConnReq, conn net.Conn) {
 	p, err := peer.NewOutboundPeer(newPeerConfig(sp), c.Addr.String())
 	if err != nil {
 		srvrLog.Debugf("Cannot create outbound peer %s: %v", c.Addr, err)
-		s.connManager.Disconnect(c.ID())
+		if c.Permanent {
+			s.connManager.Disconnect(c.ID())
+		} else {
+			s.connManager.Remove(c.ID())
+			go s.connManager.NewConnReq()
+		}
+		return
 	}
 	sp.Peer = p
 	sp.connReq = c
 	sp.isWhitelisted = isWhitelisted(conn.RemoteAddr())
 	sp.AssociateConnection(conn)
 	go s.peerDoneHandler(sp)
-	s.addrManager.Attempt(sp.NA())
 }
 
 // peerDoneHandler handles peer disconnects by notifiying the server that it's
@@ -2538,7 +2560,10 @@ func setupRPCListeners() ([]net.Listener, error) {
 // newServer returns a new btcd server configured to listen on addr for the
 // bitcoin network type specified by chainParams.  Use start to begin accepting
 // connections from peers.
-func newServer(listenAddrs []string, db database.DB, chainParams *chaincfg.Params, interrupt <-chan struct{}) (*server, error) {
+func newServer(listenAddrs, agentBlacklist, agentWhitelist []string,
+	db database.DB, chainParams *chaincfg.Params,
+	interrupt <-chan struct{}) (*server, error) {
+
 	services := defaultServices
 	if cfg.NoPeerBloomFilters {
 		services &^= wire.SFNodeBloom
@@ -2562,6 +2587,13 @@ func newServer(listenAddrs []string, db database.DB, chainParams *chaincfg.Param
 		}
 	}
 
+	if len(agentBlacklist) > 0 {
+		srvrLog.Infof("User-agent blacklist %s", agentBlacklist)
+	}
+	if len(agentWhitelist) > 0 {
+		srvrLog.Infof("User-agent whitelist %s", agentWhitelist)
+	}
+
 	s := server{
 		chainParams:          chainParams,
 		addrManager:          amgr,
@@ -2581,6 +2613,8 @@ func newServer(listenAddrs []string, db database.DB, chainParams *chaincfg.Param
 		sigCache:             txscript.NewSigCache(cfg.SigCacheMaxSize),
 		hashCache:            txscript.NewHashCache(cfg.SigCacheMaxSize),
 		cfCheckptCaches:      make(map[wire.FilterType][]cfHeaderKV),
+		agentBlacklist:       agentBlacklist,
+		agentWhitelist:       agentWhitelist,
 	}
 
 	// Create the transaction and address indexes if needed.
@@ -2683,6 +2717,7 @@ func newServer(listenAddrs []string, db database.DB, chainParams *chaincfg.Param
 			MaxSigOpCostPerTx:    blockchain.MaxBlockSigOpsCost / 4,
 			MinRelayTxFee:        cfg.minRelayTxFee,
 			MaxTxVersion:         2,
+			RejectReplacement:    cfg.RejectReplacement,
 		},
 		ChainParams:    chainParams,
 		FetchUtxoView:  s.chain.FetchUtxoView,
@@ -2774,6 +2809,9 @@ func newServer(listenAddrs []string, db database.DB, chainParams *chaincfg.Param
 					activeNetParams.DefaultPort {
 					continue
 				}
+
+				// Mark an attempt for the valid address.
+				s.addrManager.Attempt(addr.NetAddress())
 
 				addrString := addrmgr.NetAddressKey(addr.NetAddress())
 				return addrStringToNetAddr(addrString)
@@ -3132,4 +3170,48 @@ func mergeCheckpoints(defaultCheckpoints, additional []chaincfg.Checkpoint) []ch
 	}
 	sort.Sort(checkpointSorter(checkpoints))
 	return checkpoints
+}
+
+// HasUndesiredUserAgent determines whether the server should continue to pursue
+// a connection with this peer based on its advertised user agent. It performs
+// the following steps:
+// 1) Reject the peer if it contains a blacklisted agent.
+// 2) If no whitelist is provided, accept all user agents.
+// 3) Accept the peer if it contains a whitelisted agent.
+// 4) Reject all other peers.
+func (sp *serverPeer) HasUndesiredUserAgent(blacklistedAgents,
+	whitelistedAgents []string) bool {
+
+	agent := sp.UserAgent()
+
+	// First, if peer's user agent contains any blacklisted substring, we
+	// will ignore the connection request.
+	for _, blacklistedAgent := range blacklistedAgents {
+		if strings.Contains(agent, blacklistedAgent) {
+			srvrLog.Debugf("Ignoring peer %s, user agent "+
+				"contains blacklisted user agent: %s", sp,
+				agent)
+			return true
+		}
+	}
+
+	// If no whitelist is provided, we will accept all user agents.
+	if len(whitelistedAgents) == 0 {
+		return false
+	}
+
+	// Peer's user agent passed blacklist. Now check to see if it contains
+	// one of our whitelisted user agents, if so accept.
+	for _, whitelistedAgent := range whitelistedAgents {
+		if strings.Contains(agent, whitelistedAgent) {
+			return false
+		}
+	}
+
+	// Otherwise, the peer's user agent was not included in our whitelist.
+	// Ignore just in case it could stall the initial block download.
+	srvrLog.Debugf("Ignoring peer %s, user agent: %s not found in "+
+		"whitelist", sp, agent)
+
+	return true
 }
--- vendor/github.com/btcsuite/btcutil/gcs/gcs_test.go
+++ vendor/github.com/btcsuite/btcutil/gcs/gcs_test.go
@@ -88,6 +88,85 @@ func TestGCSFilterBuild(t *testing.T) {
 	}
 }
 
+// TestGCSMatchZeroHash ensures that Match and MatchAny properly match an item
+// if it's hash after the reduction is zero. This is accomplished by brute
+// forcing a specific target whose hash is zero given a certain (P, M, key,
+// len(elements)) combination. In this case, P and M are the default, key was
+// chosen randomly, and len(elements) is 13. The target 4-byte value of 16060032
+// is the first such 32-bit value, thus we use the number 0-11 as the other
+// elements in the filter since we know they won't collide. We test both the
+// positive and negative cases, when the zero hash item is in the filter and
+// when it is excluded. In the negative case, the 32-bit value of 12 is added to
+// the filter instead of the target.
+func TestGCSMatchZeroHash(t *testing.T) {
+	t.Run("include zero", func(t *testing.T) {
+		testGCSMatchZeroHash(t, true)
+	})
+	t.Run("exclude zero", func(t *testing.T) {
+		testGCSMatchZeroHash(t, false)
+	})
+}
+
+func testGCSMatchZeroHash(t *testing.T, includeZeroHash bool) {
+	key := [gcs.KeySize]byte{
+		0x25, 0x28, 0x0d, 0x25, 0x26, 0xe1, 0xd3, 0xc7,
+		0xa5, 0x71, 0x85, 0x34, 0x92, 0xa5, 0x7e, 0x68,
+	}
+
+	// Construct the target data to match, whose hash is zero after applying
+	// the reduction with the parameters in the test.
+	target := make([]byte, 4)
+	binary.BigEndian.PutUint32(target, 16060032)
+
+	// Construct the set of 13 items including the target, using the 32-bit
+	// values of 0 through 11 as the first 12 items. We known none of these
+	// hash to zero since the brute force ended well beyond them.
+	elements := make([][]byte, 0, 13)
+	for i := 0; i < 12; i++ {
+		data := make([]byte, 4)
+		binary.BigEndian.PutUint32(data, uint32(i))
+		elements = append(elements, data)
+	}
+
+	// If the filter should include the zero hash element, add the target
+	// which we know hashes to zero. Otherwise add 32-bit value of 12 which
+	// we know does not hash to zero.
+	if includeZeroHash {
+		elements = append(elements, target)
+	} else {
+		data := make([]byte, 4)
+		binary.BigEndian.PutUint32(data, 12)
+		elements = append(elements, data)
+	}
+
+	filter, err := gcs.BuildGCSFilter(P, M, key, elements)
+	if err != nil {
+		t.Fatalf("unable to build filter: %v", err)
+	}
+
+	match, err := filter.Match(key, target)
+	if err != nil {
+		t.Fatalf("unable to match: %v", err)
+	}
+
+	// We should only get a match iff the target was included.
+	if match != includeZeroHash {
+		t.Fatalf("expected match from Match: %t, got %t",
+			includeZeroHash, match)
+	}
+
+	match, err = filter.MatchAny(key, [][]byte{target})
+	if err != nil {
+		t.Fatalf("unable to match any: %v", err)
+	}
+
+	// We should only get a match iff the target was included.
+	if match != includeZeroHash {
+		t.Fatalf("expected match from MatchAny: %t, got %t",
+			includeZeroHash, match)
+	}
+}
+
 // TestGCSFilterCopy deserializes and serializes a filter to create a copy.
 func TestGCSFilterCopy(t *testing.T) {
 	serialized2, err := filter.Bytes()
@@ -217,36 +296,71 @@ func TestGCSFilterMatch(t *testing.T) {
 	}
 }
 
-// TestGCSFilterMatchAny checks that both the built and copied filters match a
-// list correctly, logging any false positives without failing on them.
-func TestGCSFilterMatchAny(t *testing.T) {
-	match, err := filter.MatchAny(key, contents2)
-	if err != nil {
-		t.Fatalf("Filter match any failed: %s", err.Error())
+// AnyMatcher is the function signature of our matching algorithms.
+type AnyMatcher func(key [gcs.KeySize]byte, data [][]byte) (bool, error)
+
+// TestGCSFilterMatchAnySuite checks that all of our matching algorithms
+// properly match a list correctly when using built or copied filters, logging
+// any false positives without failing on them.
+func TestGCSFilterMatchAnySuite(t *testing.T) {
+	funcs := []struct {
+		name     string
+		matchAny func(*gcs.Filter) AnyMatcher
+	}{
+		{
+			"default",
+			func(f *gcs.Filter) AnyMatcher {
+				return f.MatchAny
+			},
+		},
+		{
+			"hash",
+			func(f *gcs.Filter) AnyMatcher {
+				return f.HashMatchAny
+			},
+		},
+		{
+			"zip",
+			func(f *gcs.Filter) AnyMatcher {
+				return f.ZipMatchAny
+			},
+		},
 	}
-	if match {
-		t.Logf("False positive match, should be 1 in 2**%d!", P)
-	}
-	match, err = filter2.MatchAny(key, contents2)
-	if err != nil {
-		t.Fatalf("Filter match any failed: %s", err.Error())
-	}
-	if match {
-		t.Logf("False positive match, should be 1 in 2**%d!", P)
-	}
-	contents2 = append(contents2, []byte("Nate"))
-	match, err = filter.MatchAny(key, contents2)
-	if err != nil {
-		t.Fatalf("Filter match any failed: %s", err.Error())
-	}
-	if !match {
-		t.Fatal("Filter didn't match any when it should have!")
-	}
-	match, err = filter2.MatchAny(key, contents2)
-	if err != nil {
-		t.Fatalf("Filter match any failed: %s", err.Error())
-	}
-	if !match {
-		t.Fatal("Filter didn't match any when it should have!")
+
+	for _, test := range funcs {
+		t.Run(test.name, func(t *testing.T) {
+			contentsCopy := make([][]byte, len(contents2))
+			copy(contentsCopy, contents2)
+
+			match, err := test.matchAny(filter)(key, contentsCopy)
+			if err != nil {
+				t.Fatalf("Filter match any failed: %s", err.Error())
+			}
+			if match {
+				t.Logf("False positive match, should be 1 in 2**%d!", P)
+			}
+			match, err = test.matchAny(filter2)(key, contentsCopy)
+			if err != nil {
+				t.Fatalf("Filter match any failed: %s", err.Error())
+			}
+			if match {
+				t.Logf("False positive match, should be 1 in 2**%d!", P)
+			}
+			contentsCopy = append(contentsCopy, []byte("Nate"))
+			match, err = test.matchAny(filter)(key, contentsCopy)
+			if err != nil {
+				t.Fatalf("Filter match any failed: %s", err.Error())
+			}
+			if !match {
+				t.Fatal("Filter didn't match any when it should have!")
+			}
+			match, err = test.matchAny(filter2)(key, contentsCopy)
+			if err != nil {
+				t.Fatalf("Filter match any failed: %s", err.Error())
+			}
+			if !match {
+				t.Fatal("Filter didn't match any when it should have!")
+			}
+		})
 	}
 }
