95dcf0c564e74fd3dedf3b0f05549384b323bca7
Marko Bencun <marko@shiftcrypto.ch>
2019-10-30 11:08:34 +0100

    backend/bitbox02: upgrade btc-only to v4.2.2

--- backend/devices/bitbox02bootloader/firmware.go
+++ backend/devices/bitbox02bootloader/firmware.go
@@ -34,7 +34,7 @@ var bundledFirmwares = map[bitbox02common.Edition]firmwareInfo{
 		filename: "assets/firmware.v%s.signed.bin",
 	},
 	bitbox02common.EditionBTCOnly: {
-		version:  semver.NewSemVer(4, 2, 1),
+		version:  semver.NewSemVer(4, 2, 2),
 		filename: "assets/firmware-btc.v%s.signed.bin",
 	},
 }
