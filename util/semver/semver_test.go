f9a722ac237203875f20549e2c650b06d7f46497
Marko Bencun <marko@shiftcrypto.ch>
2019-11-01 07:32:09 +0100

    util/semver: more unit tests

--- util/semver/semver_test.go
+++ util/semver/semver_test.go
@@ -15,26 +15,42 @@
 package semver_test
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/digitalbitbox/bitbox-wallet-app/util/semver"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewSemVer(t *testing.T) {
-	if version, err := semver.NewSemVerFromString("3.2.4"); assert.Nil(t, err) {
-		assert.Equal(t, semver.NewSemVer(3, 2, 4), version)
-	}
+	version, err := semver.NewSemVerFromString("3.2.4")
+	require.NoError(t, err)
+	assert.Equal(t, semver.NewSemVer(3, 2, 4), version)
 
-	var err error
+	version, err = semver.NewSemVerFromString("v3.2.4")
+	require.NoError(t, err)
+	assert.Equal(t, semver.NewSemVer(3, 2, 4), version)
+
+	_, err = semver.NewSemVerFromString("")
+	assert.Error(t, err)
+
+	_, err = semver.NewSemVerFromString("vv3.2.4")
+	assert.Error(t, err)
 
 	_, err = semver.NewSemVerFromString("3.2")
+	assert.Error(t, err)
+
+	_, err = semver.NewSemVerFromString("v3.2")
 	assert.Error(t, err)
 
 	_, err = semver.NewSemVerFromString("3.2.")
 	assert.Error(t, err)
 
 	_, err = semver.NewSemVerFromString("3.2.A")
+	assert.Error(t, err)
+
+	_, err = semver.NewSemVerFromString("3.A.4")
 	assert.Error(t, err)
 
 	_, err = semver.NewSemVerFromString("3.2.4-")
@@ -46,8 +62,23 @@ func TestAtLeast(t *testing.T) {
 
 	assert.True(t, semver.NewSemVer(3, 2, 4).AtLeast(fromVersion))
 	assert.False(t, semver.NewSemVer(3, 2, 3).AtLeast(fromVersion))
+	assert.False(t, semver.NewSemVer(2, 2, 5).AtLeast(fromVersion))
+	assert.False(t, semver.NewSemVer(3, 1, 5).AtLeast(fromVersion))
 }
 
 func TestString(t *testing.T) {
 	assert.Equal(t, "3.2.4", semver.NewSemVer(3, 2, 4).String())
+}
+
+func TestJSON(t *testing.T) {
+	jsonBytes, err := json.Marshal(semver.NewSemVer(3, 2, 4))
+	require.NoError(t, err)
+	require.Equal(t, string(jsonBytes), `"3.2.4"`)
+
+	var version semver.SemVer
+	require.Error(t, json.Unmarshal([]byte(`123`), &version))
+	require.Error(t, json.Unmarshal([]byte(`"3.A.4"`), &version))
+
+	require.NoError(t, json.Unmarshal([]byte(`"3.2.4"`), &version))
+	require.Equal(t, &version, semver.NewSemVer(3, 2, 4))
 }
