6b0b34d04cd27ec6e1936863ab4cff6fbf14cabf
alex <alex@cloudware.io>
2019-11-11 07:38:48 +0100

    android: add a script to automate app icon
    The script requires convert tool from ImageMagick and optipng.
    It isn't run during "make android" build because its output
    is nondeterministic, the dockerdev image is missing the required tools,
    and the icon changes very infrequently anyway.

---
+++ frontends/android/mkicon.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# This script requires convert tool from ImageMagick and optipng.
+# The output is nondeterministic.
+set -e
+
+thisdir=$(dirname ${0})
+source=${thisdir}/../qt/resources/app_icon_source.png
+res=${thisdir}/BitBox/app/src/main/res
+
+convert -resize 48x48 ${source} ${res}/mipmap-mdpi/ic_launcher.png
+convert -resize 72x72 ${source} ${res}/mipmap-hdpi/ic_launcher.png
+convert -resize 96x96 ${source} ${res}/mipmap-xhdpi/ic_launcher.png
+convert -resize 144x144 ${source} ${res}/mipmap-xxhdpi/ic_launcher.png
+convert -resize 192x192 ${source} ${res}/mipmap-xxxhdpi/ic_launcher.png
+
+find ${res} -name ic_launcher.png -exec optipng -quiet {} \;
