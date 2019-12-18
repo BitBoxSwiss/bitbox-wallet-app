272cb9bbe30b8657b396d76f192c0693106adb64
Marko Bencun <marko@shiftcrypto.ch>
2019-12-18 01:06:52 +0100

    frontend/app: deviceIDs shouldn't be a state variable
    It is completely derived.

--- frontends/web/src/app.tsx
+++ frontends/web/src/app.tsx
@@ -45,7 +45,6 @@ import { apiWebsocket } from './utils/websocket';
 interface State {
     accounts: AccountInterface[];
     accountsInitialized: boolean;
-    deviceIDs: string[];
     devices: Devices;
     detectedBases: DetectedBitBoxBases;
     bitboxBaseIDs: string[];
@@ -57,7 +56,6 @@ class App extends Component<Props, State> {
     public state = {
         accounts: [],
         accountsInitialized: false,
-        deviceIDs: [],
         devices: {},
         detectedBases: {},
         bitboxBaseIDs: [],
@@ -135,8 +133,7 @@ class App extends Component<Props, State> {
 
     private onDevicesRegisteredChanged = () => {
         apiGet('devices/registered').then(devices => {
-            const deviceIDs = Object.keys(devices);
-            this.setState({ devices, deviceIDs });
+            this.setState({ devices });
         });
     }
 
@@ -192,8 +189,9 @@ class App extends Component<Props, State> {
 
     public render(
         {}: RenderableProps<Props>,
-        { accounts, devices, deviceIDs, bitboxBaseIDs, accountsInitialized, detectedBases }: State,
+        { accounts, devices, bitboxBaseIDs, accountsInitialized, detectedBases }: State,
     ) {
+        const deviceIDs: string[] = Object.keys(devices);
         return (
             <div className={['app', i18nEditorActive ? 'i18nEditor' : ''].join(' ')}>
                 <TranslationHelper />
@@ -219,8 +217,7 @@ class App extends Component<Props, State> {
                         <Buy
                             path="/account/:code/buy"
                             devices={devices}
-                            accounts={accounts}
-                            deviceIDs={deviceIDs} />
+                            accounts={accounts} />
                         <Info
                             path="/account/:code/info"
                             accounts={accounts} />
