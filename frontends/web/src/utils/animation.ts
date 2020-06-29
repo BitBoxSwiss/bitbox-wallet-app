64b12ad55d3a89839d6cda4fc5d1b71c48959a02
thisconnect <hi@this-connect.me>
2020-06-29 16:57:25 +0200

    support enabling and disabling reduced motion

--- frontends/web/src/utils/animation.ts
+++ frontends/web/src/utils/animation.ts
@@ -1,8 +1,29 @@
+import { debug } from './env';
+
+const mq = window.matchMedia('(prefers-reduced-motion: reduce)');
+let reduceMotion = mq.matches;
+try {
+    // experimental https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList
+    // @ts-ignore
+    mq.addListener(e => reduceMotion = e.matches);
+} catch (e) {
+    if (debug) {
+        // tslint:disable-next-line: no-console
+        console.log(e);
+    }
+}
+
 export function animate(
     el: Element,
     effect: string,
     callback?: () => void,
 ) {
+    if (reduceMotion) {
+        if (callback) {
+            callback();
+        }
+        return;
+    }
     el.addEventListener('animationend', onAnimationEnd, false);
     function onAnimationEnd() {
         el.classList.remove('animated', 'faster', effect);
