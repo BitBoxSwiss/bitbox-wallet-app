BitBoxSwiss/bitbox-wallet-app

commit 401bf795715738d5f47e95f4b060ac2a91a09ba9

cmd/servewallet: avoid install and just run
The servewallet is used only during dev workflow and need not be
installed on PATH. It can be build and executed directly with "go run".

Especially relevant for docker-less setup.
For some hypothetical special cases, one can always
"go install ./cmd/servewallet" manually.

Makefile
@@ -39,13 +39,13 @@ osx-init:
 	brew install go@1.14
 	$(MAKE) envinit
 servewallet:
-	go install -mod=vendor ./cmd/servewallet/... && servewallet
+	go run -mod=vendor ./cmd/servewallet
 servewallet-mainnet:
-	go install -mod=vendor ./cmd/servewallet/... && servewallet -mainnet
+	go run -mod=vendor ./cmd/servewallet
 servewallet-regtest:
-	go install -mod=vendor ./cmd/servewallet/... && servewallet -regtest
+	go run -mod=vendor ./cmd/servewallet
 servewallet-prodservers:
-	go install -mod=vendor ./cmd/servewallet/... && servewallet -devservers=false
+	go run -mod=vendor ./cmd/servewallet
 buildweb:
 	node --version
 	rm -rf ${WEBROOT}/build

commit 6222f93bf0d82d0ec31fb9750b1cedf3cb576fa0

Makefile: fix servewallet commands
Regression from 401bf795715738d5f47e95f4b060ac2a91a09ba9 where the
args were dropped.

Makefile
@@ -41,11 +41,11 @@ osx-init:
 servewallet:
 	go run -mod=vendor ./cmd/servewallet
 servewallet-mainnet:
-	go run -mod=vendor ./cmd/servewallet
+	go run -mod=vendor ./cmd/servewallet -mainnet
 servewallet-regtest:
-	go run -mod=vendor ./cmd/servewallet
+	go run -mod=vendor ./cmd/servewallet -regtest
 servewallet-prodservers:
-	go run -mod=vendor ./cmd/servewallet
+	go run -mod=vendor ./cmd/servewallet -devservers=false
 buildweb:
 	node --version
 	rm -rf ${WEBROOT}/build