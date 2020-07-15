BitBoxSwiss/bitbox-wallet-app

commit 53466beec694c0bfdfdd80c5ba4ae10ddba6ff52

Improve Mobile Pairing Error Messages
When the relay server is not reachable, the frontend just displays a
timeout message, while the backend logs an unrelated error. This commit
attempts to improve this by displaying a separate message in the
frontend when messages are not relayed correctly and improves the
logging in the backend.

backend/devices/bitbox/pairing.go
@@ -46,13 +46,30 @@ func (device *Device) finishPairing(channel *relay.Channel) {
 	device.fireEvent(EventPairingSuccess, nil)
 }
 
+func (device *Device) handlePairingError(err error, message string) {
+	switch err.Error() {
+	case relay.PullFailedError:
+		device.log.Errorf("Failed to pull the mobile's %s.", message)
+		device.fireEvent(EventPairingPullMessageFailed, nil)
+	case relay.ResponseTimeoutError:
+		device.log.Errorf("Failed to wait for the mobile's %s.", message)
+		device.fireEvent(EventPairingTimedout, nil)
+	default:
+	}
+}
+
 // processPairing processes the pairing after the channel has been displayed as a QR code.
 func (device *Device) processPairing(channel *relay.Channel) {
-	if err := channel.WaitForScanningSuccess(time.Minute); err != nil {
-		device.log.WithError(err).Warning("Failed to wait for the scanning success.")
-		device.fireEvent(EventPairingTimedout, nil)
+	status, err := channel.WaitForScanningSuccess(10 * time.Second)
+	if err != nil {
+		device.handlePairingError(err, "scanning success message")
 		return
 	}
+	if status != "success" {
+		device.log.Error("Scanning unsuccessful")
+		device.fireEvent(EventPairingScanningFailed, nil)
+	}
+
 	deviceInfo, err := device.DeviceInfo()
 	if err != nil {
 		device.log.WithError(err).Error("Failed to check if device is locked or not")
@@ -67,8 +84,7 @@ func (device *Device) processPairing(channel *relay.Channel) {
 	device.fireEvent(EventPairingStarted, nil)
 	mobileECDHPKhash, err := channel.WaitForMobilePublicKeyHash(2 * time.Minute)
 	if err != nil {
-		device.log.WithError(err).Warning("Failed to wait for the mobile's public key hash.")
-		device.fireEvent(EventPairingTimedout, nil)
+		device.handlePairingError(err, "public key hash")
 		return
 	}
 	bitboxECDHPKhash, err := device.ecdhPKhash(mobileECDHPKhash)
@@ -86,8 +102,7 @@ func (device *Device) processPairing(channel *relay.Channel) {
 	}
 	mobileECDHPK, err := channel.WaitForMobilePublicKey(2 * time.Minute)
 	if err != nil {
-		device.log.WithError(err).Error("Failed to wait for the mobile's public key.")
-		device.fireEvent(EventPairingTimedout, nil)
+		device.handlePairingError(err, "public key")
 		return
 	}
 	bitboxECDHPK, err := device.ecdhPK(mobileECDHPK)
@@ -117,8 +132,7 @@ func (device *Device) processPairing(channel *relay.Channel) {
 		challenge, err = channel.WaitForCommand(2 * time.Minute)
 	}
 	if err != nil {
-		device.log.WithError(err).Error("Failed to get challenge request from mobile.")
-		device.fireEvent(EventPairingTimedout, nil)
+		device.handlePairingError(err, "challenge request")
 		return
 	}
 	device.log.Debug("Finished pairing")

backend/devices/bitbox/events.go
@@ -25,6 +25,12 @@ const (
 	// EventPairingTimedout is fired when the pairing timed out.
 	EventPairingTimedout event.Event = "pairingTimedout"
 
+	// EventPullFailed is fired when a message cannot be pulled through the pairing relay server.
+	EventPairingPullMessageFailed event.Event = "pairingPullMessageFailed"
+
+	// EventPairingScanningFailed is fired when the mobile does not respond with success after scanning the pairing code.
+	EventPairingScanningFailed event.Event = "pairingScanningFailed"
+
 	// EventPairingAborted is fired when the pairing aborted.
 	EventPairingAborted event.Event = "pairingAborted"
 

backend/devices/bitbox/relay/channel.go
@@ -56,6 +56,13 @@ type Channel struct {
 	log *logrus.Entry
 }
 
+// PullFailedError is used to create an error in case the pull from the relay server fails.
+// It is exported to allow the bitbox package to parse the error message with this content.
+const PullFailedError = "failed to pull data from relay server, the server might be down"
+
+// ResponseTimeoutError is used to create an error in case the pull from the relay server times out.
+const ResponseTimeoutError = "timeout: relay server did not respond"
+
 // NewChannel returns a new channel with the given channel ID, encryption and authentication key.
 func NewChannel(channelID string, encryptionKey []byte, authenticationKey []byte, socksProxy socksproxy.SocksProxy) *Channel {
 	return &Channel{
@@ -155,13 +162,13 @@ func (channel *Channel) waitForValue(duration time.Duration, name string) (strin
 			unlock()
 			message, err := PullOldestMessage(relayServer(), channel)
 			if err != nil {
-				return "", err
+				return "", errp.New(PullFailedError)
 			}
 			if message == nil {
 				if time.Now().Before(deadline) {
 					continue
 				}
-				return "", errp.New("Did not receive a response from the mobile in the given duration.")
+				return "", errp.New(ResponseTimeoutError)
 			}
 			value, found = channel.getValueFromMessage(message, name)
 			if !found {
@@ -177,15 +184,9 @@ func (channel *Channel) waitForValue(duration time.Duration, name string) (strin
 }
 
 // WaitForScanningSuccess waits for the given duration for the scanning success from the mobile.
-// Returns nil if the scanning success was retrieved from the relay server and an error otherwise.
-func (channel *Channel) WaitForScanningSuccess(duration time.Duration) error {
-	value, err := channel.waitForValue(duration, "id")
-	if err != nil {
-		return err
-	} else if value != "success" {
-		return errp.New("Scanning unsuccessful")
-	}
-	return nil
+// Returns an error if no public key hash has been received from the server in the given duration.
+func (channel *Channel) WaitForScanningSuccess(duration time.Duration) (string, error) {
+	return channel.waitForValue(duration, "id")
 }
 
 // WaitForMobilePublicKeyHash waits for the given duration for the public key hash from the mobile.

backend/devices/bitbox/relay/request.go
@@ -18,7 +18,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"io/ioutil"
+	"net/http"
 	"strings"
+
+	"github.com/digitalbitbox/bitbox-wallet-app/util/errp"
 )
 
 // request models a request to the relay server.
@@ -77,6 +80,9 @@ func (request *request) send() (*response, error) {
 	)
 	if err != nil {
 		return nil, err
+	}
+	if httpResponse.StatusCode != http.StatusOK {
+		return nil, errp.New("Proxy Server did not respond with OK http status code, it is probably offline")
 	}
 	defer func() { _ = httpResponse.Body.Close() }()
 	body, err := ioutil.ReadAll(httpResponse.Body)

frontends/web/src/routes/device/settings/components/mobile-pairing.tsx
@@ -84,6 +84,12 @@ class MobilePairing extends Component<Props, State> {
                     this.setState({ status: 'timeout' });
                 }
                 break;
+            case 'pairingPullMessageFailed':
+                this.setState({ status: 'pullFailed' });
+                break;
+            case 'pairingScanningFailed':
+                this.setState({ status: 'scanningFailed' });
+                break;
             case 'pairingAborted':
                 this.setState({ status: 'aborted' });
                 break;

commit 4becc5bcd908d111340d984df4c515e07a790e83

backend/bitbox01: restore longer timeout when starting the pairing
Regression from 53466beec694c0bfdfdd80c5ba4ae10ddba6ff52.

backend/devices/bitbox/pairing.go
@@ -60,7 +60,7 @@ func (device *Device) handlePairingError(err error, message string) {
 
 // processPairing processes the pairing after the channel has been displayed as a QR code.
 func (device *Device) processPairing(channel *relay.Channel) {
-	status, err := channel.WaitForScanningSuccess(10 * time.Second)
+	status, err := channel.WaitForScanningSuccess(2 * time.Minute)
 	if err != nil {
 		device.handlePairingError(err, "scanning success message")
 		return