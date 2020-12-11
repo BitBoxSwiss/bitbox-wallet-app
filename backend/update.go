71b566555b9189688e6bd8005936ced24b88f0cd
Marko Bencun <marko@shiftcrypto.ch>
2020-12-11 22:13:58 +0100

    backend: update to version 4.24.1

--- backend/update.go
+++ backend/update.go
@@ -27,7 +27,7 @@ const updateFileURL = "https://shiftcrypto.ch/updates/desktop.json"
 
 var (
 	// Version of the backend as displayed to the user.
-	Version = semver.NewSemVer(4, 24, 0)
+	Version = semver.NewSemVer(4, 24, 1)
 )
 
 // UpdateFile is retrieved from the server.
