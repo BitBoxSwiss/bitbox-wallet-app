c05e7f034a332b21b5b99e1151b570f63e1acd30
thisconnect <hi@this-connect.me>
2020-12-16 12:35:22 +0100

    moonpay ui improvements, add disclaimer and coin selector
    - moved buy view into routes
    - added a disclaimer page with do-not-show-anymore checkbox
    - added coin selector (still needs correct coins)
    - started formatting disclamer dummy text, linebreaks only for now
    - made MoonPay iframe use almost whole content (minus header)

---
+++ frontends/web/src/routes/buy/info.tsx
@@ -0,0 +1,146 @@
+/**
+ * Copyright 2020 Shift Crypto AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, h, RenderableProps } from 'preact';
+import { route } from 'preact-router';
+import { Entry } from '../../components/guide/entry';
+import { Guide } from '../../components/guide/guide';
+import { Header } from '../../components/layout';
+import { load } from '../../decorators/load';
+import { translate, TranslateProps } from '../../decorators/translate';
+import { Button, Checkbox, Select } from '../../components/forms';
+import { Devices } from '../device/deviceswitch';
+import { AccountInterface } from '../account/account';
+import { setConfig } from '../../utils/config';
+import * as style from './info.css';
+
+interface BuyInfoProps {
+    accounts: AccountInterface[];
+    code?: string;
+    devices: Devices;
+}
+
+interface LoadedBuyInfoProps {
+    config: any;
+}
+
+interface State {
+    status: 'choose' | 'agree'
+    selected?: string;
+}
+
+type Props = BuyInfoProps & LoadedBuyInfoProps & TranslateProps;
+
+class BuyInfo extends Component<Props, State> {
+    public readonly state: State = {
+        status: this.props.config.frontend.skipBuyDisclaimer ? 'choose' : 'agree',
+        selected: this.props.code,
+    }
+
+    private handleProceed = () => {
+        const { status, selected } = this.state;
+        if (selected && (status === 'choose' || this.props.config.frontend.skipBuyDisclaimer)) {
+            route(`/buy/moonpay/${selected}`);
+        } else {
+            this.setState({ status: 'choose' });
+        }
+    }
+
+    private handleSkipDisclaimer = (e) => {
+        setConfig({ frontend: { skipBuyDisclaimer: e.target.checked }});
+    }
+
+    public render(
+        { accounts,
+          t }: RenderableProps<Props>,
+        {
+            status,
+            selected,
+        }: State
+    ) {
+        return (
+            <div class="contentWithGuide">
+                <div class="container">
+                    <Header />
+                    <div class="innerContainer scrollableContainer">
+                        { status === 'agree' ? (
+                            <div class="content narrow isVerticallyCentered">
+                                <h1 class={style.title}>{t('buy.info.title')}</h1>
+                                <pre class={style.disclaimer}>
+                                    {t('buy.info.disclaimer')}
+                                </pre>
+                                <div class="text-center m-bottom-quarter">
+                                    <Checkbox
+                                        id="skip_disclaimer"
+                                        label={t('buy.info.skip')}
+                                        onChange={this.handleSkipDisclaimer} />
+                                </div>
+                                <div class="buttons text-center m-bottom-xlarge">
+                                    <Button
+                                        primary
+                                        onClick={this.handleProceed}>
+                                        {t('buy.info.continue')}
+                                    </Button>
+                                </div>
+                            </div>
+                        ) : (
+                            <div class="content narrow isVerticallyCentered">
+                                <h1 class={style.title}>{t('buy.title')}</h1>
+                                <Select
+                                    label={t('buy.info.selectLabel')}
+                                    placeholder={t('buy.info.selectPlaceholder')}
+                                    options={[{
+                                            text: t('buy.info.selectPlaceholder'),
+                                            disabled: true,
+                                            value: 'choose',
+                                        },
+                                        ...accounts.map(({ code, name}) => ({
+                                            text: `${name} (${code})`,
+                                            value: code,
+                                        }))]
+                                    }
+                                    onChange={e => this.setState({ selected: e.target.value})}
+                                    value={selected}
+                                    defaultValue={'choose'}
+                                    id="coinAndAccountCode"
+                                />
+                                <div class="buttons text-center m-bottom-xxlarge">
+                                    <Button
+                                        primary
+                                        onClick={this.handleProceed}
+                                        disabled={!selected}>
+                                        {t('buy.info.next')}
+                                    </Button>
+                                </div>
+
+                            </div>
+                        )}
+                    </div>
+                </div>
+                <Guide>
+                    <Entry key="guide.buy.support" entry={t('guide.buy.support')} shown={true} />
+                </Guide>
+            </div>
+        );
+    }
+}
+
+const loadHOC = load<LoadedBuyInfoProps, BuyInfoProps & TranslateProps>({
+    config: 'config'
+})(BuyInfo);
+
+const HOC = translate<BuyInfoProps>()(loadHOC);
+export { HOC as BuyInfo };
