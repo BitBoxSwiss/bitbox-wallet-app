efce9451d0fccfb9c4639cb465b2e5541d2693a6
thisconnect <this@shiftcrypto.ch>
2021-02-02 12:15:21 +0100

    frontend: refactor account into api/account
    Some components contain api interfaces, that got wildly imported
    
    all over and grew organically. This lead to incomplete and duplicate
    interfaces and is hard to maintain.
    
    Centralizing all api interfaces and promises into web/src/api helps
    reducing duplicates and separates api from ui components.
    Additionally promise return values from api all come typed.
    
    This is the first step and collects all account interfaces and api
    calls into api/account.ts. Next step is to refactor all components
    to import the interfaces and api calls from api/account.

---
+++ frontends/web/src/api/account.ts
@@ -0,0 +1,215 @@
+/**
+ * Copyright 2021 Shift Crypto AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { apiGet, apiPost } from '../utils/request';
+import { ChartData } from '../routes/account/summary/chart';
+
+export type AccountState = 'accountSynced' | 'accountDisabled' | 'fatalError' | 'offlineMode';
+
+export const getStatus = (code: string): Promise<AccountState[]> => {
+    return apiGet(`account/${code}/status`);
+};
+
+export type ScriptType = 'p2pkh' | 'p2wpkh-p2sh' | 'p2wpkh';
+
+export interface ISigningConfiguration {
+    address: string;
+    keypath: string;
+    scriptType: ScriptType;
+    threshold: number;
+    xpubs: string[];
+}
+
+export interface ISigningConfigurationList {
+    signingConfigurations: ISigningConfiguration[];
+}
+
+export const getInfo = (code: string): Promise<ISigningConfigurationList> => {
+    return apiGet(`account/${code}/info`);
+};
+
+export const init = (code: string): Promise<null> => {
+    return apiPost(`account/${code}/init`);
+};
+
+export const reinitialize = (): Promise<null> => {
+    return apiPost('accounts/reinitialize');
+};
+
+export interface ISummary {
+    chartDataMissing: boolean;
+    chartDataDaily: ChartData;
+    chartDataHourly: ChartData;
+    chartFiat: Fiat;
+    chartTotal: number | null;
+    chartIsUpToDate: boolean; // only valid if chartDataMissing is false
+}
+
+export const getSummary = (): Promise<ISummary> => {
+    return apiGet('account-summary');
+};
+
+export const exportSummary = (): Promise<string> => {
+    return apiPost('export-account-summary');
+};
+
+export type MainnetCoin = 'BTC' | 'LTC' | 'ETH';
+
+export type TestnetCoin = 'TBTC' | 'TLTC' | 'TETH' | 'RETH';
+
+export type Coin = MainnetCoin | TestnetCoin;
+
+export type Fiat = 'AUD' | 'BTC' | 'CAD' | 'CHF' | 'CNY' | 'EUR' | 'GBP' | 'ILS' | 'JPY' | 'KRW' | 'RUB' | 'SGD' | 'USD';
+
+export type Conversions = null | {
+    [key in Fiat]: string;
+}
+
+export interface IAmount {
+    amount: string;
+    conversions: Conversions;
+    unit: Coin;
+}
+
+export type CoinCode = 'btc' | 'tbtc' | 'ltc' | 'tltc' | 'eth' | 'teth' | 'reth';
+
+export interface IBalance {
+    available: IAmount;
+    hasIncoming: boolean;
+    incoming: IAmount;
+}
+
+export const getBalance = (code: string): Promise<IBalance> => {
+    return apiGet(`account/${code}/balance`);
+};
+
+export interface ITransaction {
+    addresses: string[];
+    amount: IAmount;
+    fee: IAmount;
+    feeRatePerKb: IAmount;
+    gas: number;
+    internalID: string;
+    note: string;
+    numConfirmations: number;
+    numConfirmationsComplete: number;
+    size: number;
+    status: 'complete' | 'pending' | 'failed';
+    time: string | null;
+    type: 'send' | 'receive' | 'self';
+    txID: string;
+    vsize: number;
+    weight: number;
+}
+
+export interface INoteTx {
+    internalTxID: string;
+    note: string;
+}
+
+export const postNotesTx = (code: string, {
+    internalTxID,
+    note,
+}: INoteTx): Promise<null> => {
+    return apiPost(`account/${code}/notes/tx`, { internalTxID, note });
+};
+
+export const getTransactionList = (code: string): Promise<ITransaction[]> => {
+    return apiGet(`account/${code}/transactions`);
+};
+
+export const exportAccount = (code: string): Promise<string> => {
+    return apiPost(`account/${code}/export`);
+};
+
+export const getCanVerifyXPub = (code: string): Promise<number[]> => {
+    return apiGet(`account/${code}/can-verify-extended-public-key`);
+};
+
+export interface IReceiveAddress {
+    addressID: string;
+    address: string;
+}
+
+export const verifyAddress = (code: string, addressID: string): Promise<boolean> => {
+    return apiPost(`account/${code}/verify-address`, addressID);
+};
+
+export type ReceiveAddressList = IReceiveAddress[][];
+
+export const getReceiveAddressList = (code: string): Promise<ReceiveAddressList> => {
+    return apiGet(`account/${code}/receive-addresses`);
+};
+
+export interface ISendTx {
+    aborted?: boolean;
+    success?: boolean;
+    errorMessage?: string;
+}
+
+export const sendTx = (code: string): Promise<ISendTx> => {
+    return apiPost(`account/${code}/sendtx`);
+};
+
+export type FeeTargetCode = 'custom' | 'low' | 'economy' | 'normal' | 'high';
+
+export interface IProposeTxData {
+    address?: string;
+    amount?: number;
+    // data?: string;
+    feePerByte: string;
+    feeTarget: FeeTargetCode;
+    selectedUTXOs: string[];
+    sendAll: 'yes' | 'no';
+}
+
+export interface IProposeTx {
+    aborted?: boolean;
+    success?: boolean;
+    errorMessage?: string;
+}
+
+export const proposeTx = (code: string, data: IProposeTxData): Promise<IProposeTx> => {
+    return apiPost(`account/${code}/tx-proposal`, data);
+};
+
+export const proposeTxNote = (code: string, note: string): Promise<null> => {
+    return apiPost(`account/${code}/propose-tx-note`, note);
+};
+
+export interface IUTXO {
+    address: string;
+    amount: IAmount;
+    outPoint: string;
+}
+
+export const getUTXOList = (code: string): Promise<IUTXO[]> => {
+    return apiGet(`account/${code}/utxos`);
+};
+
+export interface IFeeTarget {
+    code: FeeTargetCode;
+    feeRateInfo: string;
+}
+
+export interface IFeeTargetList {
+    feeTargets: IFeeTarget[],
+    defaultFeeTarget: FeeTargetCode
+}
+
+export const getFeeTargetList = (code: string): Promise<IFeeTargetList> => {
+    return apiGet(`account/${code}/fee-targets`);
+};
