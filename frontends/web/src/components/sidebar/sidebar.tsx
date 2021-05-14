0f1c298a2abe239447d6ca4c29fa1c4fdd7a297c
thisconnect <this@shiftcrypto.ch>
2021-05-14 08:46:01 +0200

    frontend: hide add account in sidebar header
    If there is no keystore hide add accounts and accounts header.

--- frontends/web/src/components/sidebar/sidebar.tsx
+++ frontends/web/src/components/sidebar/sidebar.tsx
@@ -172,24 +172,22 @@ class Sidebar extends Component<Props> {
                         </div>
                     </Link>
                     <div className="sidebarHeaderContainer">
-                        <span className="sidebarHeader">{t('sidebar.accounts')}</span>
-                        {
-                            debug && (
-                                <span className="sidebarHeaderAction">
-                                    <Link
-                                        href={`/add-account`}
-                                        title={t('sidebar.addAccount')}
-                                        activeClassName="sidebar-active"
-                                        onClick={this.handleSidebarItemClick}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                                            <circle cx="12" cy="12" r="10"></circle>
-                                            <line x1="12" y1="8" x2="12" y2="16"></line>
-                                            <line x1="8" y1="12" x2="16" y2="12"></line>
-                                        </svg>
-                                    </Link>
-                                </span>
-                            )
-                        }
+                        <span className="sidebarHeader" hidden={!keystores.length}>
+                            {t('sidebar.accounts')}
+                        </span>
+                        <span className="sidebarHeaderAction" hidden={!keystores.length}>
+                            <Link
+                                href={`/add-account`}
+                                title={t('sidebar.addAccount')}
+                                activeClassName="sidebar-active"
+                                onClick={this.handleSidebarItemClick}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                    <circle cx="12" cy="12" r="10"></circle>
+                                    <line x1="12" y1="8" x2="12" y2="16"></line>
+                                    <line x1="8" y1="12" x2="16" y2="12"></line>
+                                </svg>
+                            </Link>
+                        </span>
                     </div>
                     {/* <div className="activeGroup">
                         <div className="sidebarItem">
