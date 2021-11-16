dab6a0cb77ea3b0f20032c323f5d95575f79a960
Brian <briancraigok@gmail.com>
2021-11-16 10:13:16 -0300

    Frontend: redirect home to portfolio

--- frontends/web/src/components/sidebar/sidebar.tsx
+++ frontends/web/src/components/sidebar/sidebar.tsx
@@ -164,7 +164,7 @@ class Sidebar extends Component<Props> {
                 <nav className={['sidebar', activeSidebar ? 'forceShow' : '', shown ? 'withGuide' : ''].join(' ')}>
                     <Link
                         activeClassName=""
-                        href="/"
+                        href={accounts.length ? '/account-summary' : '/'}
                         onClick={this.handleSidebarItemClick}>
                         <div className="sidebarLogoContainer">
                             <AppLogoInverted className="sidebarLogo" />
