4aa8459cd6128eb6a280e6a3e62ad583ea678f32
Brian Craig <briancraigok@gmail.com>
2022-01-13 07:30:58 -0300

    frontend: rewrite Status as a FunctionComponent
    rewrite `Status` as a `FunctionComponent` adding
    the use of `getConfig` hook.

--- frontends/web/src/components/status/status.tsx
+++ frontends/web/src/components/status/status.tsx
@@ -15,15 +15,10 @@
  * limitations under the License.
  */
 
-import { Component} from 'react';
-import { apiGet, apiPost } from '../../utils/request';
+import { FunctionComponent } from 'react';
+import { useConfig } from '../../hooks/config';
 import style from './status.module.css';
-
-interface State {
-    show: boolean;
-}
-
-interface StatusProps {
+interface Props {
     hidden?: boolean;
     type?: 'success' | 'warning' | 'info';
     // used as keyName in the config if dismissing the status should be persisted, so it is not
@@ -33,91 +28,63 @@ interface StatusProps {
     className?: string;
 }
 
-type Props = StatusProps;
-
-export default class Status extends Component<Props, State> {
-    public readonly state: State = {
-        show: true,
-    };
-
-    public componentDidMount() {
-        this.checkConfig();
-    }
-
-    public componentDidUpdate(prevProps) {
-        if (this.props.dismissable !== prevProps.dismissable) {
-            this.checkConfig();
-        }
-    }
-
-    private checkConfig() {
-        if (this.props.dismissable) {
-            apiGet('config').then(({ frontend }) => {
-                if (!this.props.dismissable) {
-                    return;
-                }
-                this.setState({
-                    show: !frontend ? true : !frontend[this.props.dismissable],
-                });
-            });
-        }
-    }
-
-    private dismiss = () => {
-        apiGet('config').then(config => {
-            if (!this.props.dismissable) {
-                return;
-            }
-            const newConf = {
-                ...config,
-                frontend: {
-                    ...config.frontend,
-                    [this.props.dismissable]: true,
-                },
-            };
-            apiPost('config', newConf);
-        });
-        this.setState({
-            show: false,
-        });
-    }
-
-    public render() {
-        const {
-            children,
-            className,
-            dismissable,
-            hidden,
-            type = 'warning',
-        } = this.props;
-        const { show } = this.state;
-        if (hidden || !show) {
+const Status: FunctionComponent<Props> = ({
+    children,
+    className,
+    dismissable,
+    hidden,
+    type = 'warning',
+}) => {
+    const { config, setConfig } = useConfig();
+    
+    let dismissed = false;
+    if (dismissable) {
+        if (!config) {
+            // Wait until config is loaded to prevent rendering a dismissed Status
             return null;
         }
-        return (
-            <div className={[style.container, style[type], className ? className : ''].join(' ')}>
-                <div className={style.status}>
-                    {children}
-                    <button
-                        hidden={!dismissable}
-                        className={`${style.close} ${style[`close-${type}`]}`}
-                        onClick={this.dismiss}>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round">
-                            <line x1="18" y1="6" x2="6" y2="18"></line>
-                            <line x1="6" y1="6" x2="18" y2="18"></line>
-                        </svg>
-                    </button>
-                </div>
-            </div>
-        );
+        dismissed = !!config.frontend[dismissable]
     }
+
+    if (hidden || dismissed) {
+        return null;
+    }
+
+    const dismiss = () => {
+        setConfig((config) => ({
+            ...config,
+            frontend: {
+                ...config.frontend,
+                [dismissable!]: true,
+            },
+        }));
+    }
+
+    return (
+        <div className={[style.container, style[type], className ? className : ''].join(' ')}>
+            <div className={style.status}>
+                {children}
+                <button
+                    hidden={!dismissable}
+                    className={`${style.close} ${style[`close-${type}`]}`}
+                    onClick={dismiss}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round">
+                        <line x1="18" y1="6" x2="6" y2="18"></line>
+                        <line x1="6" y1="6" x2="18" y2="18"></line>
+                    </svg>
+                </button>
+            </div>
+        </div>
+    );
 }
+
+export default Status;
