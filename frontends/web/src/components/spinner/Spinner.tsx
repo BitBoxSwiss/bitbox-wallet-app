ba23fbe7a3f083c3c52356bfc3b1181303080ef9
Brian Craig <briancraigok@gmail.com>
2022-01-24 08:37:20 -0300

    frontend: refactor `spinner` component
     - Migrate `ClassComponent` to `FunctionComponent`
     - Migrate HOCs to hooks

--- frontends/web/src/components/spinner/Spinner.tsx
+++ frontends/web/src/components/spinner/Spinner.tsx
@@ -14,10 +14,10 @@
  * limitations under the License.
  */
 
-import { Component} from 'react';
+import { FunctionComponent, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import MenuIcon from '../../assets/icons/menu.svg';
 import { share } from '../../decorators/share';
-import { translate, TranslateProps } from '../../decorators/translate';
 import { SharedProps, store, toggle as toggleGuide } from '../guide/guide';
 import { toggleSidebar } from '../sidebar/sidebar';
 import style from './Spinner.module.css';
@@ -26,79 +26,68 @@ interface SpinnerProps {
     text?: string;
 }
 
-type Props = SpinnerProps & TranslateProps & SharedProps;
+type Props = SpinnerProps & SharedProps;
 
-class Spinner extends Component<Props> {
+const Spinner: FunctionComponent<Props> = ({ text, guideExists }) => {
+    const { t } = useTranslation();
 
-    public UNSAFE_componentWillMount() {
-        document.addEventListener('keydown', this.handleKeyDown);
-    }
-
-    public componentWillUnmount() {
-        document.removeEventListener('keydown', this.handleKeyDown);
-    }
-
-    private handleKeyDown = (e: KeyboardEvent) => {
+    const handleKeyDown = (e: KeyboardEvent) => {
         e.preventDefault();
-        // @ts-ignore (blur exists only on HTMLElements)
-        document.activeElement.blur();
+        (document.activeElement as HTMLInputElement).blur();
     }
 
-    public render() {
-        const {
-            t,
-            text,
-            guideExists,
-        } = this.props;
-        return (
-            <div className={style.spinnerContainer}>
-                <div className={style.togglersContainer}>
-                    <div className={style.togglerContainer}>
-                        <div className={style.toggler} onClick={toggleSidebar}>
-                            <img src={MenuIcon} />
-                        </div>
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+        return () => document.removeEventListener('keydown', handleKeyDown);
+    }, []);
+
+    return (
+        <div className={style.spinnerContainer}>
+            <div className={style.togglersContainer}>
+                <div className={style.togglerContainer}>
+                    <div className={style.toggler} onClick={toggleSidebar}>
+                        <img src={MenuIcon} />
                     </div>
-                    {
-                        guideExists && (
-                            <div className={style.guideToggler} onClick={toggleGuide}>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round">
-                                    <circle cx="12" cy="12" r="10"></circle>
-                                    <circle cx="12" cy="12" r="4"></circle>
-                                    <line x1="4.93" y1="4.93" x2="9.17" y2="9.17"></line>
-                                    <line x1="14.83" y1="14.83" x2="19.07" y2="19.07"></line>
-                                    <line x1="14.83" y1="9.17" x2="19.07" y2="4.93"></line>
-                                    <line x1="14.83" y1="9.17" x2="18.36" y2="5.64"></line>
-                                    <line x1="4.93" y1="19.07" x2="9.17" y2="14.83"></line>
-                                </svg>
-                                {t('guide.toggle.open')}
-                            </div>
-                        )
-                    }
                 </div>
                 {
-                    text && text.split('\n').map((line, i) => (
-                        <p key={`${line}-${i}`} className={style.spinnerText}>{line}</p>
-                    ))
+                    guideExists && (
+                        <div className={style.guideToggler} onClick={toggleGuide}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round">
+                                <circle cx="12" cy="12" r="10"></circle>
+                                <circle cx="12" cy="12" r="4"></circle>
+                                <line x1="4.93" y1="4.93" x2="9.17" y2="9.17"></line>
+                                <line x1="14.83" y1="14.83" x2="19.07" y2="19.07"></line>
+                                <line x1="14.83" y1="9.17" x2="19.07" y2="4.93"></line>
+                                <line x1="14.83" y1="9.17" x2="18.36" y2="5.64"></line>
+                                <line x1="4.93" y1="19.07" x2="9.17" y2="14.83"></line>
+                            </svg>
+                            {t('guide.toggle.open')}
+                        </div>
+                    )
                 }
-                <div className={style.spinner}>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className={style.overlay}></div>
             </div>
-        );
-    }
+            {
+                text && text.split('\n').map((line, i) => (
+                    <p key={`${line}-${i}`} className={style.spinnerText}>{line}</p>
+                ))
+            }
+            <div className={style.spinner}>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className={style.overlay}></div>
+        </div>
+    );
 }
 
-const SharedSpinner = share<SharedProps, SpinnerProps & TranslateProps>(store)(Spinner);
-const TranslatedSpinner = translate()(SharedSpinner);
-export { TranslatedSpinner as Spinner };
+const SharedSpinner = share<SharedProps, SpinnerProps>(store)(Spinner);
+export { SharedSpinner as Spinner };
