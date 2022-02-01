dde191cc485e3328d7664b717d4b21efc5c8403d
thisconnect <this@shiftcrypto.ch>
2022-02-01 16:25:06 +0100

    frontend: move to locale string function to utils
    That the format can be reused in other components.

---
+++ frontends/web/src/utils/date.ts
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2022 Shift Crypto AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export const convertDateToLocaleString = (
+    date: string,
+    language: string
+) => {
+    return new Date(date).toLocaleString(language, {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+    });
+};
