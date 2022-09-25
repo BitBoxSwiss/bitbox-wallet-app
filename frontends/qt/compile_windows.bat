e6d3f466fdcb96bedda31f0f8b7146f6b06b3ce0
mdcmeridius <undisclosed@some.provider.com>
2022-09-25 12:00:23 +0100

    compile_windows.bat: attempt to detect VS 2022 when VS 2019 isn't found

--- frontends/qt/compile_windows.bat
+++ frontends/qt/compile_windows.bat
@@ -3,8 +3,14 @@
 setlocal
 set varsbat="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 if exist %varsbat% (goto :CALL_VC_VARS_BAT)
-echo Trying to figure out Visual Studio location...
+echo Trying to figure out Visual Studio 2019 location...
 for /f "tokens=2* eol=," %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Classes\CLSID\{2E1517DA-87BF-4443-984A-D2BF18F5A908}\DefaultIcon" ^|findstr /rc:"REG_SZ *"') do set varsbat=%%~b
+If Defined varsbat (
+  set varsbat="%varsbat:common7\ide\devenv.exe,1200=VC\Auxiliary\Build\vcvars64.bat%"
+)
+if exist %varsbat% (goto :CALL_VC_VARS_BAT)
+echo Trying to figure out Visual Studio 2022 location...
+for /f "tokens=2* eol=," %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Classes\CLSID\{33ABD590-0400-4FEF-AF98-5F5A8A99CFC3}\DefaultIcon" ^|findstr /rc:"REG_SZ *"') do set varsbat=%%~b
 If Defined varsbat (
   set varsbat="%varsbat:common7\ide\devenv.exe,1200=VC\Auxiliary\Build\vcvars64.bat%"
 )
