ee744743f97754fda237a350bae90d761e456aca
thisconnect <this@shiftcrypto.ch>
2022-09-28 18:37:48 +0200

    frontend: move inline svg to image linking to svg
    Reduces the bundle size, JavaScript parse time and React dom diffing.

--- frontends/web/src/routes/settings/components/fiat/fiat.tsx
+++ frontends/web/src/routes/settings/components/fiat/fiat.tsx
@@ -16,6 +16,7 @@
 
 import React, { PropsWithChildren } from 'react';
 import { Fiat } from '../../../../api/account';
+import { Star, StarInactive } from '../../../../components/icon';
 import {
   currencies,
   selectFiat,
@@ -71,18 +72,7 @@ function Selection({
                       title={t(main ? 'fiat.default' : 'fiat.setDefault', { code: currency })}
                       data-code={currency}
                       onClick={setDefault}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round">
-                        <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
-                      </svg>
+                      { main ? <Star /> : <StarInactive /> }
                     </a>
                   )
                 }
