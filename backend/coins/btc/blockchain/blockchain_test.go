17e5f4f98f49e11f772891473295fb2c75524e41
Marko Bencun <marko@shiftcrypto.ch>
2022-12-07 18:03:14 +0100

    backend/btc: move unit test to right location
    It was testing a blockchain.go function, so it should be in that folder.

--- backend/coins/btc/blockchain/blockchain_test.go
+++ backend/coins/btc/blockchain/blockchain_test.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Shift Devices AG
+// Copyright 2022 Shift Crypto AG
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,37 +12,36 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package client_test
+package blockchain
 
 import (
 	"testing"
 
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/digitalbitbox/bitbox-wallet-app/backend/coins/btc/blockchain"
 	"github.com/stretchr/testify/require"
 )
 
 func TestStatus(t *testing.T) {
-	history := blockchain.TxHistory{}
+	history := TxHistory{}
 	require.Equal(t, "", history.Status())
 
-	tx1 := &blockchain.TxInfo{
+	tx1 := &TxInfo{
 		Height: 10,
-		TXHash: blockchain.TXHash(chainhash.HashH([]byte("tx1"))),
+		TXHash: TXHash(chainhash.HashH([]byte("tx1"))),
 		Fee:    nil,
 	}
-	tx2 := &blockchain.TxInfo{
+	tx2 := &TxInfo{
 		Height: 12,
-		TXHash: blockchain.TXHash(chainhash.HashH([]byte("tx2"))),
+		TXHash: TXHash(chainhash.HashH([]byte("tx2"))),
 		Fee:    nil,
 	}
 
-	history = []*blockchain.TxInfo{tx1}
+	history = []*TxInfo{tx1}
 	require.Equal(t,
 		"5ac1b066322843c5cb9160e8079dd759eddf6f1fd60645d6bf54942dcba00d09",
 		history.Status())
 
-	history = []*blockchain.TxInfo{tx1, tx2}
+	history = []*TxInfo{tx1, tx2}
 	require.Equal(t,
 		"9783fa8a2f1c89652022e0bb435f302ee8b856961dd979ee083435c65384f314",
 		history.Status())
