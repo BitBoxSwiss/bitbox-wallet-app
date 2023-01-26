BitBoxSwiss/bitbox-wallet-app

commit a229726d8cd5424d502aa9610bad5962088ce7bb

frontend: improve dialog on mobile
to maximise spacing and achieveing a more modern look on mobile,
we want to re-style the Dialog component to become a
modal bottom sheet.

frontends/web/src/routes/device/bitbox02/reset.tsx
@@ -82,35 +82,32 @@ class Reset extends Component<Props, State> {
           onClick={() => this.setState({ activeDialog: true })}>
           {t('reset.title')}
         </SettingsButton>
-        {
-          activeDialog && (
-            <Dialog
-              title={t('reset.title')}
-              onClose={this.abort}
-              disabledClose={isConfirming}
-              small>
-              <div className="columnsContainer half">
-                <div className="columns">
-                  <div className="column">
-                    <p>{t('reset.description')}</p>
-                    <div>
-                      <Checkbox
-                        id="reset_understand"
-                        label={t('reset.understandBB02')}
-                        checked={understand}
-                        onChange={this.handleUnderstandChange} />
-                    </div>
-                  </div>
+        <Dialog
+          open={activeDialog}
+          title={t('reset.title')}
+          onClose={this.abort}
+          disabledClose={isConfirming}
+          small>
+          <div className="columnsContainer half">
+            <div className="columns">
+              <div className="column">
+                <p>{t('reset.description')}</p>
+                <div>
+                  <Checkbox
+                    id="reset_understand"
+                    label={t('reset.understandBB02')}
+                    checked={understand}
+                    onChange={this.handleUnderstandChange} />
                 </div>
               </div>
-              <DialogButtons>
-                <Button danger disabled={!understand} onClick={this.reset}>
-                  {t('reset.title')}
-                </Button>
-              </DialogButtons>
-            </Dialog>
-          )
-        }
+            </div>
+          </div>
+          <DialogButtons>
+            <Button danger disabled={!understand} onClick={this.reset}>
+              {t('reset.title')}
+            </Button>
+          </DialogButtons>
+        </Dialog>
         {
           isConfirming && (
             <WaitDialog

frontends/web/src/components/confirm/Confirm.tsx
@@ -60,10 +60,8 @@ export const Confirm = () => {
   };
 
   const { message, active, customButtonText } = state;
-  if (!active) {
-    return null;
-  }
-  return <Dialog title={t('dialog.confirmTitle')} onClose={() => respond(false)}>
+
+  return <Dialog open={active} title={t('dialog.confirmTitle')} onClose={() => respond(false)}>
     <div className="columnsContainer half">
       <div className="columns">
         <div className="column">
@@ -79,6 +77,7 @@ export const Confirm = () => {
         </div>
       </div>
     </div>
+
     <DialogButtons>
       <Button primary onClick={() => respond(true)}>{customButtonText ? customButtonText : t('dialog.confirm')}</Button>
       <Button transparent onClick={() => respond(false)}>{t('dialog.cancel')}</Button>

frontends/web/src/components/dialog/dialog.module.css
@@ -8,13 +8,12 @@
     left: 0;
     height: 100%;
     width: 100%;
-    background-color: var(--bg-transparent-dark);
+    background: transparent;
     z-index: 4010;
-    opacity: 0;
 }
 
 .overlay.activeOverlay {
-    opacity: 1;
+    background-color: var(--bg-transparent-dark);
 }
 
 .modal {
@@ -26,14 +25,7 @@
     text-align: left;
     max-height: 100vh;
     overflow: auto;
-    opacity: 0;
 }
-
-.modal.activeModal {
-    opacity: 1;
-}
-
-.active {}
 
 .modal.small {
     max-width: 340px;
@@ -236,9 +228,34 @@
     width: 100%;
 }
 
+@media (min-width: 769px) {
+    .closingOverlay {
+        display: none;
+    }
+}
+
 @media (max-width: 768px) {
+
+    .header .title{
+        font-size: var(--header-default-font-size);
+    }
+
     .modal {
         padding: 0;
+    }
+
+    .modal, .modal.small, .modal.medium, .modal.large{
+        max-width: 100vw;
+        height: 100vh;
+        transform: translateY(100vh);
+        transition: transform 300ms;
+        border-top-right-radius: var(--space-half);
+        border-top-left-radius: var(--space-half);
+    }
+
+    .modal.open {
+      transition: transform 300ms;
+      transform: translateY(0);
     }
 
     .contentContainer.padded {

frontends/web/src/components/dialog/dialog.tsx
@@ -29,11 +29,13 @@ interface Props {
     onClose?: (e?: Event) => void;
     disabledClose?: boolean;
     children: React.ReactNode;
+    open: boolean;
 }
 
 interface State {
     active: boolean;
     currentTab: number;
+    renderDialog: boolean;
 }
 
 class Dialog extends Component<Props, State> {
@@ -41,14 +43,30 @@ class Dialog extends Component<Props, State> {
   private modal = createRef<HTMLDivElement>();
   private modalContent = createRef<HTMLDivElement>();
   private focusableChildren!: NodeListOf<HTMLElement>;
+  private timerId: any;
 
   public state: State = {
     active: false,
     currentTab: 0,
+    renderDialog: false,
   };
 
-  public componentDidMount() {
-    setTimeout(this.activate, 10);
+  public componentDidUpdate(prevProps: Readonly<Props>): void {
+    const { open } = this.props;
+    const { active } = this.state;
+
+    if (!active && open && !prevProps.open) {
+      this.setState({ renderDialog: true }, () => {
+        this.activate();
+      });
+      return;
+    }
+
+
+    if (active && !open && prevProps.open) {
+      this.deactivate();
+      return;
+    }
   }
 
   public componentWillUnmount() {
@@ -88,6 +106,19 @@ class Dialog extends Component<Props, State> {
     });
   };
 
+  private deactivateModal = () => {
+    if (!this.modal.current || !this.overlay.current) {
+      return;
+    }
+    this.overlay.current.classList.remove(style.closingOverlay);
+    this.setState({ active: false, currentTab: 0, renderDialog: false }, () => {
+      document.removeEventListener('keydown', this.handleKeyDown);
+      if (this.props.onClose) {
+        this.props.onClose();
+      }
+    });
+  };
+
   private getNextIndex(isNext: boolean) {
     const { currentTab } = this.state;
     const focusables = Array.from(this.focusableChildren);
@@ -119,23 +150,36 @@ class Dialog extends Component<Props, State> {
     if (!this.modal.current || !this.overlay.current) {
       return;
     }
-    this.modal.current.classList.remove(style.activeModal);
+
+    if (this.timerId) {
+      clearTimeout(this.timerId);
+    }
+
+    this.modal.current?.classList.remove(style.open);
     this.overlay.current.classList.remove(style.activeOverlay);
-    this.setState({ active: false, currentTab: 0 }, () => {
-      document.removeEventListener('keydown', this.handleKeyDown);
-      if (this.props.onClose) {
-        this.props.onClose();
-      }
-    });
+    this.overlay.current.classList.add(style.closingOverlay);
+    this.timerId = setTimeout(this.deactivateModal, 300);
   };
 
   private activate = () => {
+
     this.setState({ active: true }, () => {
       if (!this.modal.current || !this.overlay.current) {
         return;
       }
+
+      if (this.timerId) {
+        clearTimeout(this.timerId);
+      }
+
       this.overlay.current.classList.add(style.activeOverlay);
-      this.modal.current.classList.add(style.activeModal);
+
+      // Minor delay
+      this.timerId = setTimeout(() => {
+        this.modal.current?.classList.add(style.open);
+      }, 10);
+
+
       this.focusWithin();
       this.focusFirst();
     });
@@ -153,11 +197,17 @@ class Dialog extends Component<Props, State> {
       disabledClose,
       children,
     } = this.props;
+    const { renderDialog } = this.state;
     const isSmall = small ? style.small : '';
     const isMedium = medium ? style.medium : '';
     const isLarge = large ? style.large : '';
     const isSlim = slim ? style.slim : '';
     const isCentered = centered && !onClose ? style.centered : '';
+
+    if (!renderDialog) {
+      return null;
+    }
+
     return (
       <div className={style.overlay} ref={this.overlay}>
         <div

frontends/web/src/components/language/language.tsx
@@ -19,8 +19,8 @@ import { useState } from 'react';
 import { i18n as Ii18n } from 'i18next';
 import { useTranslation } from 'react-i18next';
 import { Dialog } from '../dialog/dialog';
-import style from './language.module.css';
 import { TActiveLanguageCodes, TLanguagesList } from './types';
+import style from './language.module.css';
 
 type TLanguageSwitchProps = {
     languages?: TLanguagesList;
@@ -113,44 +113,40 @@ const LanguageSwitch = ({ languages }: TLanguageSwitchProps) => {
         </svg>
         {allLanguages[selectedIndex].code === 'en' ? 'Other languages' : 'English'}
       </button>
-      {
-        activeDialog && (
-          <Dialog small slim title={t('language.title')} onClose={() => setActiveDialog(false)}>
-            {
-              allLanguages.map((language, i) => {
-                const selected = selectedIndex === i;
-                return (
-                  <button
-                    key={language.code}
-                    className={[style.language, selected ? style.selected : ''].join(' ')}
-                    onClick={() => changeLanguage(language.code, i)}
-                    data-testid={`language-selection-${language.code}`}
-                  >
-                    {language.display}
-                    {
-                      selected && (
-                        <svg
-                          className={style.checked}
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round">
-                          <polyline points="20 6 9 17 4 12" />
-                        </svg>
-                      )
-                    }
-                  </button>
-                );
-              })
-            }
-          </Dialog>
-        )
-      }
+      <Dialog small slim title={t('language.title')} onClose={() => setActiveDialog(false)} open={activeDialog}>
+        {
+          allLanguages.map((language, i) => {
+            const selected = selectedIndex === i;
+            return (
+              <button
+                key={language.code}
+                className={[style.language, selected ? style.selected : ''].join(' ')}
+                onClick={() => changeLanguage(language.code, i)}
+                data-testid={`language-selection-${language.code}`}
+              >
+                {language.display}
+                {
+                  selected && (
+                    <svg
+                      className={style.checked}
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round">
+                      <polyline points="20 6 9 17 4 12" />
+                    </svg>
+                  )
+                }
+              </button>
+            );
+          })
+        }
+      </Dialog>
     </div>
   );
 };

frontends/web/src/components/transactions/transaction.tsx
@@ -222,197 +222,197 @@ class Transaction extends Component<Props, State> {
             </div>
           </div>
         </div>
-        {
-          transactionDialog && transactionInfo && (
-            // Amount and Confirmations info are displayed using props data instead of transactionInfo
-            // because they are live updated
-            <Dialog title="Transaction Details" onClose={this.hideDetails} slim medium>
-              <form onSubmit={this.handleEdit} className={style.detailInput}>
-                <label htmlFor="note">{t('note.title')}</label>
-                <Input
-                  align="right"
-                  autoFocus={editMode}
-                  className={style.textOnlyInput}
-                  readOnly={!editMode}
-                  type="text"
-                  id="note"
-                  transparent
-                  placeholder={t('note.input.placeholder')}
-                  value={newNote}
-                  maxLength={256}
-                  onInput={this.handleNoteInput}
-                  ref={this.input}/>
-                <button
-                  className={style.editButton}
-                  onClick={this.handleEdit}
-                  title={t(`transaction.note.${editMode ? 'save' : 'edit'}`)}
-                  type="button"
-                  ref={this.editButton}>
-                  {editMode ? <Save /> : <Edit />}
-                </button>
-              </form>
-              <div className={style.detail}>
-                <label>{t('transaction.details.type')}</label>
-                <p>{arrow}</p>
-              </div>
-              <div className={style.detail}>
-                <label>{t('transaction.confirmation')}</label>
-                <p>{numConfirmations}</p>
-              </div>
-              <div className={style.detail}>
-                <label>{t('transaction.details.status')}</label>
-                <p className="flex flex-items-center">
-                  <ProgressRing
-                    className="m-right-quarter"
-                    width={14}
-                    value={progress}
-                    isComplete={numConfirmations >= numConfirmationsComplete}
-                  />
-                  <span className={style.status}>
-                    {statusText} {
-                      status === 'pending' && (
-                        <span>({numConfirmations}/{numConfirmationsComplete})</span>
-                      )
-                    }
-                  </span>
-                </p>
-              </div>
-              <div className={style.detail}>
-                <label>{t('transaction.details.date')}</label>
-                <p>{sDate}</p>
-              </div>
-              <div className={style.detail}>
-                <label>{t('transaction.details.fiat')}</label>
-                <p>
-                  <span className={`${style.fiat} ${typeClassName}`}>
-                    <FiatConversion amount={amount} sign={sign} noAction />
-                  </span>
-                </p>
-              </div>
-              <div className={style.detail}>
-                <label>{t('transaction.details.fiatAtTime')}</label>
-                <p>
-                  <span className={`${style.fiat} ${typeClassName}`}>
-                    { transactionInfo.amountAtTime ?
-                      <FiatConversion amount={transactionInfo.amountAtTime} sign={sign} noAction />
-                      :
-                      <FiatConversion noAction />
-                    }
-                  </span>
-                </p>
-              </div>
-              <div className={style.detail}>
-                <label>{t('transaction.details.amount')}</label>
-                <p className={typeClassName}>
-                  <span className={style.amount}>{sign}{amount.amount}</span>
-                  {' '}
-                  <span className={style.currencyUnit}>{transactionInfo.amount.unit}</span>
-                </p>
-              </div>
-              <div className={style.detail}>
-                <label>{t('transaction.fee')}</label>
-                {
-                  transactionInfo.fee && transactionInfo.fee.amount ? (
-                    <p title={feeRatePerKb.amount ? feeRatePerKb.amount + ' ' + feeRatePerKb.unit + '/Kb' : ''}>
-                      {transactionInfo.fee.amount}
-                      {' '}
-                      <span className={style.currencyUnit}>{transactionInfo.fee.unit}</span>
-                    </p>
-                  ) : (
-                    <p>---</p>
-                  )
-                }
-              </div>
-              <div className={[style.detail, style.addresses].join(' ')}>
-                <label>{t('transaction.details.address')}</label>
-                <div className={style.detailAddresses}>
-                  { transactionInfo.addresses.map((address) => (
-                    <CopyableInput
-                      key={address}
-                      alignRight
-                      borderLess
-                      flexibleHeight
-                      className={style.detailAddress}
-                      value={address} />
-                  )) }
-                </div>
-              </div>
+        {/*
+            Amount and Confirmations info are displayed using props data
+            instead of transactionInfo because they are live updated.
+          */}
+        <Dialog open={transactionDialog} title="Transaction Details" onClose={this.hideDetails} slim medium>
+          {transactionInfo && <>
+            <form onSubmit={this.handleEdit} className={style.detailInput}>
+              <label htmlFor="note">{t('note.title')}</label>
+              <Input
+                align="right"
+                autoFocus={editMode}
+                className={style.textOnlyInput}
+                readOnly={!editMode}
+                type="text"
+                id="note"
+                transparent
+                placeholder={t('note.input.placeholder')}
+                value={newNote}
+                maxLength={256}
+                onInput={this.handleNoteInput}
+                ref={this.input}/>
+              <button
+                className={style.editButton}
+                onClick={this.handleEdit}
+                title={t(`transaction.note.${editMode ? 'save' : 'edit'}`)}
+                type="button"
+                ref={this.editButton}>
+                {editMode ? <Save /> : <Edit />}
+              </button>
+            </form>
+            <div className={style.detail}>
+              <label>{t('transaction.details.type')}</label>
+              <p>{arrow}</p>
+            </div>
+            <div className={style.detail}>
+              <label>{t('transaction.confirmation')}</label>
+              <p>{numConfirmations}</p>
+            </div>
+            <div className={style.detail}>
+              <label>{t('transaction.details.status')}</label>
+              <p className="flex flex-items-center">
+                <ProgressRing
+                  className="m-right-quarter"
+                  width={14}
+                  value={progress}
+                  isComplete={numConfirmations >= numConfirmationsComplete}
+                />
+                <span className={style.status}>
+                  {statusText} {
+                    status === 'pending' && (
+                      <span>({numConfirmations}/{numConfirmationsComplete})</span>
+                    )
+                  }
+                </span>
+              </p>
+            </div>
+            <div className={style.detail}>
+              <label>{t('transaction.details.date')}</label>
+              <p>{sDate}</p>
+            </div>
+            <div className={style.detail}>
+              <label>{t('transaction.details.fiat')}</label>
+              <p>
+                <span className={`${style.fiat} ${typeClassName}`}>
+                  <FiatConversion amount={amount} sign={sign} noAction />
+                </span>
+              </p>
+            </div>
+            <div className={style.detail}>
+              <label>{t('transaction.details.fiatAtTime')}</label>
+              <p>
+                <span className={`${style.fiat} ${typeClassName}`}>
+                  { transactionInfo.amountAtTime ?
+                    <FiatConversion amount={transactionInfo.amountAtTime} sign={sign} noAction />
+                    :
+                    <FiatConversion noAction />
+                  }
+                </span>
+              </p>
+            </div>
+            <div className={style.detail}>
+              <label>{t('transaction.details.amount')}</label>
+              <p className={typeClassName}>
+                <span className={style.amount}>{sign}{amount.amount}</span>
+                {' '}
+                <span className={style.currencyUnit}>{transactionInfo.amount.unit}</span>
+              </p>
+            </div>
+            <div className={style.detail}>
+              <label>{t('transaction.fee')}</label>
               {
-                transactionInfo.gas ? (
-                  <div className={style.detail}>
-                    <label>{t('transaction.gas')}</label>
-                    <p>{transactionInfo.gas}</p>
-                  </div>
-                ) : null
+                transactionInfo.fee && transactionInfo.fee.amount ? (
+                  <p title={feeRatePerKb.amount ? feeRatePerKb.amount + ' ' + feeRatePerKb.unit + '/Kb' : ''}>
+                    {transactionInfo.fee.amount}
+                    {' '}
+                    <span className={style.currencyUnit}>{transactionInfo.fee.unit}</span>
+                  </p>
+                ) : (
+                  <p>---</p>
+                )
               }
-              {
-                transactionInfo.nonce !== null ? (
-                  <div className={style.detail}>
-                    <label>Nonce</label>
-                    <p>{transactionInfo.nonce}</p>
-                  </div>
-                ) : null
-              }
-              {
-                transactionInfo.weight ? (
-                  <div className={style.detail}>
-                    <label>{t('transaction.weight')}</label>
-                    <p>
-                      {transactionInfo.weight}
-                      {' '}
-                      <span className={style.currencyUnit}>WU</span>
-                    </p>
-                  </div>
-                ) : null
-              }
-              {
-                transactionInfo.vsize ? (
-                  <div className={style.detail}>
-                    <label>{t('transaction.vsize')}</label>
-                    <p>
-                      {transactionInfo.vsize}
-                      {' '}
-                      <span className={style.currencyUnit}>b</span>
-                    </p>
-                  </div>
-                ) : null
-              }
-              {
-                transactionInfo.size ? (
-                  <div className={style.detail}>
-                    <label>{t('transaction.size')}</label>
-                    <p>
-                      {transactionInfo.size}
-                      {' '}
-                      <span className={style.currencyUnit}>b</span>
-                    </p>
-                  </div>
-                ) : null
-              }
-              <div className={[style.detail, style.addresses].join(' ')}>
-                <label>{t('transaction.explorer')}</label>
-                <div className={style.detailAddresses}>
+            </div>
+            <div className={[style.detail, style.addresses].join(' ')}>
+              <label>{t('transaction.details.address')}</label>
+              <div className={style.detailAddresses}>
+                { transactionInfo.addresses.map((address) => (
                   <CopyableInput
+                    key={address}
                     alignRight
                     borderLess
                     flexibleHeight
                     className={style.detailAddress}
-                    value={transactionInfo.txID} />
+                    value={address} />
+                )) }
+              </div>
+            </div>
+            {
+              transactionInfo.gas ? (
+                <div className={style.detail}>
+                  <label>{t('transaction.gas')}</label>
+                  <p>{transactionInfo.gas}</p>
                 </div>
+              ) : null
+            }
+            {
+              transactionInfo.nonce !== null ? (
+                <div className={style.detail}>
+                  <label>Nonce</label>
+                  <p>{transactionInfo.nonce}</p>
+                </div>
+              ) : null
+            }
+            {
+              transactionInfo.weight ? (
+                <div className={style.detail}>
+                  <label>{t('transaction.weight')}</label>
+                  <p>
+                    {transactionInfo.weight}
+                    {' '}
+                    <span className={style.currencyUnit}>WU</span>
+                  </p>
+                </div>
+              ) : null
+            }
+            {
+              transactionInfo.vsize ? (
+                <div className={style.detail}>
+                  <label>{t('transaction.vsize')}</label>
+                  <p>
+                    {transactionInfo.vsize}
+                    {' '}
+                    <span className={style.currencyUnit}>b</span>
+                  </p>
+                </div>
+              ) : null
+            }
+            {
+              transactionInfo.size ? (
+                <div className={style.detail}>
+                  <label>{t('transaction.size')}</label>
+                  <p>
+                    {transactionInfo.size}
+                    {' '}
+                    <span className={style.currencyUnit}>b</span>
+                  </p>
+                </div>
+              ) : null
+            }
+            <div className={[style.detail, style.addresses].join(' ')}>
+              <label>{t('transaction.explorer')}</label>
+              <div className={style.detailAddresses}>
+                <CopyableInput
+                  alignRight
+                  borderLess
+                  flexibleHeight
+                  className={style.detailAddress}
+                  value={transactionInfo.txID} />
               </div>
-              <div className={[style.detail, 'flex-center'].join(' ')}>
-                <p>
-                  <A
-                    className={style.externalLink}
-                    href={explorerURL + transactionInfo.txID}
-                    title={t('transaction.explorerTitle') + '\n' + explorerURL + transactionInfo.txID}>
-                    {t('transaction.explorerTitle')}
-                  </A>
-                </p>
-              </div>
-            </Dialog>
-          )
-        }
+            </div>
+            <div className={[style.detail, 'flex-center'].join(' ')}>
+              <p>
+                <A
+                  className={style.externalLink}
+                  href={explorerURL + transactionInfo.txID}
+                  title={t('transaction.explorerTitle') + '\n' + explorerURL + transactionInfo.txID}>
+                  {t('transaction.explorerTitle')}
+                </A>
+              </p>
+            </div>
+          </> }
+        </Dialog>
       </div>
     );
   }

commit a9d7498effd6f36229f84e43de96b707f904616d

frontend: fix regression in dialog
The dialog behavior changed slightly it shows depending on the open
prop controlled by the state of the parent component. This
introduced a small regression, that the onClose callback is called
later after the dialog open prop is set to false, passed to the
dialog again and then it hides during render.

This is not a problem if onClose was only used to reset the dialog,
but if onClose does anything else then it will overwrite the state
at a later point and cause bugs.

In reset.tsx when clickin on reset there should be a blocking.
confirmation is overwritten by onClose and does not show anymore.

In send-qr there is a function to toggle open and close the scan
dialog. The back button toggles close, but the onClose callback
toggles open again, so when clicking back the dialog shows again.

regressio introduced in a229726d8cd5424d502aa9610bad5962088ce7bb

components with onClose that should be tested:
- Confirm dialog
- Language switch (should be good as only used to close dialog)
- Transaction details
- QR scan dialog in send (fix in this commit)
- coincontrol (should be good)
- BB01 create (maybe fine)
- BB01 check (maybe fine)
- BB01 restore (should have bug, there are other dialogs
  controlled by the same state)
- BB01 changepin (should have bug with isConfirming)
- BB01 randomnumber (looks ok)
- BB01 reset (should have the bug)
- BB01 mobile-pairing (looks ok)
- BB02 manage backups / check backup
- BB02 reset, blocking confirm missing
- BB02 set device name (looks ok)
- manage account (looks ok)
- settings proxy (looks ok)

frontends/web/src/routes/device/bitbox02/reset.tsx
@@ -85,7 +85,7 @@ class Reset extends Component<Props, State> {
         <Dialog
           open={activeDialog}
           title={t('reset.title')}
-          onClose={this.abort}
+          onClose={() => this.setState({ activeDialog: false, understand: false })}
           disabledClose={isConfirming}
           small>
           <div className="columnsContainer half">