1bf9747881c47a1248949a618fc17a8e21853b79
shonsirsha <seangeekpro@gmail.com>
2023-04-24 17:42:40 +0200

    frontend: use the `useSettingsTab` hook in `/new-settings/appearance`

--- frontends/web/src/routes/new-settings/appearance.tsx
+++ frontends/web/src/routes/new-settings/appearance.tsx
@@ -1,24 +1,25 @@
-import { View, ViewContent } from '../../components/view/view';
-import { Header, Main } from '../../components/layout';
 import { DarkmodeToggleSetting } from './components/appearance/darkmodeToggleSetting';
 import { DefaultCurrencyDropdownSetting } from './components/appearance/defaultCurrencyDropdownSetting';
 import { DisplaySatsToggleSetting } from './components/appearance/displaySatsToggleSetting';
 import { LanguageDropdownSetting } from './components/appearance/languageDropdownSetting';
 import { ActiveCurrenciesDropdownSetting } from './components/appearance/activeCurrenciesDropdownSetting';
+import useSettingsTab from './components/hooks/useSettingsTab';
+
+const ApperanceWithoutTabs = () => {
+  return (
+    <>
+      <DefaultCurrencyDropdownSetting />
+      <ActiveCurrenciesDropdownSetting />
+      <LanguageDropdownSetting />
+      <DarkmodeToggleSetting />
+      <DisplaySatsToggleSetting />
+    </>
+  );
+};
 
 export const Appearance = () => {
+  const WithTabs = useSettingsTab(ApperanceWithoutTabs);
   return (
-    <Main>
-      <Header title={<h2>Settings</h2>} />
-      <View fullscreen={false}>
-        <ViewContent>
-          <DefaultCurrencyDropdownSetting />
-          <ActiveCurrenciesDropdownSetting />
-          <LanguageDropdownSetting />
-          <DarkmodeToggleSetting />
-          <DisplaySatsToggleSetting />
-        </ViewContent>
-      </View>
-    </Main>
+    <WithTabs />
   );
 };
