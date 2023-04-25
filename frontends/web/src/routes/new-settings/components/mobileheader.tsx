06389f6cbfa577004b8619e06ec0847743aa4f6e
shonsirsha <seangeekpro@gmail.com>
2023-04-25 12:54:40 +0200

    frontend: further development for back button of `MobileHeader`
    1. Added the "chevron left" / back icon according to theme (light / dark)
    
    2. Used native `button` instead of `<Link>`

--- frontends/web/src/routes/new-settings/components/mobileheader.tsx
+++ frontends/web/src/routes/new-settings/components/mobileheader.tsx
@@ -1,6 +1,9 @@
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import styles from './mobileheader.module.css';
 import { TTab } from './hooks/useSettingsTab';
+import { useTranslation } from 'react-i18next';
+import { ChevronLeftDark, ChevronLeftLight } from '../../../components/icon';
+import { useDarkmode } from '../../../hooks/darkmode';
 
 type TProps = {
   settingsTabsDetail: TTab[];
@@ -8,10 +11,19 @@ type TProps = {
 
 export const MobileHeader = ({ settingsTabsDetail }: TProps) => {
   const { pathname } = useLocation();
+  const { t } = useTranslation();
+  const { isDarkMode } = useDarkmode();
+
   const activeTabIndex = settingsTabsDetail.findIndex(tab => tab.url === pathname);
+
+  const backIcon = isDarkMode ? <ChevronLeftLight /> : <ChevronLeftDark />;
+
+  const handleClick = () => {
+    //goes to the 'general settings' page
+  };
   return (
     <div className={styles.container}>
-      <Link to={'#'} className={styles.backButton}>Back</Link>
+      <button onClick={handleClick} className={styles.backButton}>{backIcon} {t('button.back')}</button>
       <h1 className={styles.headerText}>{activeTabIndex >= 0 ? settingsTabsDetail[activeTabIndex].tabName : ''}</h1>
     </div>
   );
