132a6778f9dd5629547d7ed78145ba80f11b8e75
shonsirsha <seangeekpro@gmail.com>
2023-05-01 17:16:15 +0200

    frontend: created `singledropdown`
    to be used for dropdown setting items

--- frontends/web/src/routes/new-settings/components/appearance/defaultCurrencyDropdownSetting.tsx
+++ frontends/web/src/routes/new-settings/components/appearance/defaultCurrencyDropdownSetting.tsx
@@ -1,32 +1,6 @@
+import { setActiveFiat, store } from '../../../../components/rates/rates';
+import { SingleDropdown } from '../singledropdown/singledropdown';
 import { SettingsItem } from '../settingsItem/settingsItem';
-import Select from 'react-select';
-import { store, setActiveFiat } from '../../../../components/rates/rates';
-import { Fiat } from '../../../../api/account';
-import styles from './defaultCurrencySetting.module.css';
-
-type SelectOption = {
-  label: Fiat;
-  value: Fiat;
-}
-
-type TSelectProps = {
-  options: SelectOption[];
-  handleChange: (fiat: Fiat) => void;
-}
-
-const ReactSelect = ({ options, handleChange }: TSelectProps) => <Select
-  className={styles.select}
-  classNamePrefix="react-select"
-  defaultValue={{ label: store.state.active, value: store.state.active }}
-  isSearchable={true}
-  onChange={(selected) => {
-    if (selected) {
-      handleChange(selected.value as Fiat);
-    }
-  }
-  }
-  options={options}
-/>;
 
 export const DefaultCurrencyDropdownSetting = () => {
   const formattedCurrencies = store.state.selected.map((currency) => ({ label: currency, value: currency }));
@@ -35,7 +9,13 @@ export const DefaultCurrencyDropdownSetting = () => {
     <SettingsItem
       settingName="Default Currency"
       secondaryText="Select your default currency."
-      extraComponent={<ReactSelect options={formattedCurrencies} handleChange={setActiveFiat}/>}
+      extraComponent={
+        <SingleDropdown
+          options={formattedCurrencies}
+          handleChange={setActiveFiat}
+          defaultValue={{ label: store.state.active, value: store.state.active }}
+        />
+      }
     />
   );
 };
