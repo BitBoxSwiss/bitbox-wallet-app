48eebe98e81fa1c026c45e3b9ed1b1f602aae435
thisconnect <this@shiftcrypto.ch>
2023-09-20 15:15:08 +0200

    frontend: display warning if camera has to access to scan
    Choosing scan QRCode icon in send view should prompt (at least on
    macOS) that the application wants to access the camera.
    
    Added a hook to check for camera permissions, so that the app can
    display a information text to allow camera accesss when the user
    tries to scan.

--- frontends/web/src/routes/account/send/components/dialogs/scan-qr-dialog.tsx
+++ frontends/web/src/routes/account/send/components/dialogs/scan-qr-dialog.tsx
@@ -32,14 +32,16 @@ export const ScanQRDialog = ({ parseQRResult, activeScanQR, toggleScanQR, onChan
   return (
     <Dialog
       large
+      onClose={toggleScanQR}
       open={activeScanQR}
-      title={t('send.scanQR')}
-      onClose={toggleScanQR}>
-      <ScanQRVideo
-        onResult={result => {
-          parseQRResult(result);
-          onChangeActiveScanQR(false);
-        }} />
+      title={t('send.scanQR')}>
+      <div style={{ minHeight: '300px' }}>
+        <ScanQRVideo
+          onResult={result => {
+            parseQRResult(result);
+            onChangeActiveScanQR(false);
+          }} />
+      </div>
       <DialogButtons>
         <Button
           secondary
