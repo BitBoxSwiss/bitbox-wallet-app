c020de6cfb0889d3e5a9ea468455c67ed3216bfb
Tomas Vrba <vrba.tomas@pm.me>
2023-09-21 13:57:32 +0200

    backend: add unit tests for eth address>account code matching

--- backend/accounts.go
+++ backend/accounts.go
@@ -23,7 +23,6 @@ import (
 
 	"github.com/btcsuite/btcd/btcutil/hdkeychain"
 	"github.com/digitalbitbox/bitbox-wallet-app/backend/accounts"
-	"github.com/digitalbitbox/bitbox-wallet-app/backend/accounts/errors"
 	accountsTypes "github.com/digitalbitbox/bitbox-wallet-app/backend/accounts/types"
 	"github.com/digitalbitbox/bitbox-wallet-app/backend/coins/btc"
 	coinpkg "github.com/digitalbitbox/bitbox-wallet-app/backend/coins/coin"
@@ -34,7 +33,6 @@ import (
 	"github.com/digitalbitbox/bitbox-wallet-app/util/errp"
 	"github.com/digitalbitbox/bitbox-wallet-app/util/observable"
 	"github.com/digitalbitbox/bitbox-wallet-app/util/observable/action"
-	ethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/params"
 )
 
@@ -1106,19 +1104,16 @@ func (backend *Backend) checkAccountUsed(account accounts.Interface) {
 // Used for handling Wallet Connect requests from anywhere in the app
 // Implemented only for pure ETH accounts (not ERC20s), as all Wallet Connect interactions are handled through the root ETH accounts.
 func (backend *Backend) LookupEthAccountCode(address string) (accountsTypes.Code, string, error) {
-	if !eth.IsValidEthAddress(address) {
-		return "", "", errp.WithStack(errors.ErrInvalidAddress)
-	}
 	for _, account := range backend.Accounts() {
 		ethAccount, ok := account.(*eth.Account)
 		if !ok {
 			continue
 		}
-		accountAddress, err := ethAccount.Address()
+		matches, err := ethAccount.MatchesAddress(address)
 		if err != nil {
-			return "", "", errp.WithStack(err)
+			return "", "", err
 		}
-		if ethcommon.HexToAddress(address).Hex() == accountAddress.Hex() && !eth.IsERC20(ethAccount) {
+		if matches && !eth.IsERC20(ethAccount) {
 			return ethAccount.Config().Config.Code, ethAccount.Config().Config.Name, nil
 		}
 	}
