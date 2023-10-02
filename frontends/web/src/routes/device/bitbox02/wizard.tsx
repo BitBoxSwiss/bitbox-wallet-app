8f121823f8844b8a6576b8fd13fa4f2d132857b7
Marko Bencun <marko@shiftcrypto.ch>
2023-10-02 17:10:46 +0200

    frontend: split bitbox02 into settings and wizard
    Navigating to 'Settings -> Manage device' shows the device
    settings. The BitBox02 component also included all of the
    "wizard" (fullscreen) functionality like the unlock animation,
    pairing, creating/restoring, etc.
    
    We split the wizard into a new file/component. We mount that in
    App.tsx globally, so it can be displayed at any time, not just when
    the user is in the `/settings/device-settings/<deviceID>` route. This
    is important in watch-only, where the user can connect the BitBox02
    when prompted for it (e.g. when trying to verify an address or sign a
    transaction) without being removed from whatever worfklow the user is
    in.
    
    I copied bitbox02.tsx to wizard.tsx and removed everything
    non-essential from the original bitbox02.tsx. In wizard.tsx, almost
    everything is the same as in the original, except that the settings
    are not rendered and we don't automatically route anywhere after
    unlock.
    
    The waitDialog state was removed as it was unused.
    
    --- Routing ---
    
    With watch-only, inserting and unplugging a BitBox02 does not
    influence routing anymore. Connecting can happen anywhere (e.g. when
    trying to sign).
    
    Instead, we route to the portfolio page when the app goes from no
    accounts to some accounts. This happens either at launch when
    watch-only account are loaded, or when accounts are loaded for the
    first time when unlocking a keystore.
    
    For the bitbox01 and the bitbox02 bootloader, we keep routing to the
    settings, the bb01 unlock screen and bb02 bootloader screen are not
    full screen and are not mounted globally.
    This means that
    - if a bb01 user needs to insert their device when signing a
      transaction, they will be removed from that screen. We don't put in
      extra effort into the bb01
    - if a bb02 user needs to insert but ends up having to upgrade the fw,
      they will also be removed from their current workflow. This should
      be a very rare, ideally never.

---
+++ frontends/web/src/routes/device/bitbox02/wizard.tsx
@@ -0,0 +1,221 @@
+/**
+ * Copyright 2023 Shift Crypto AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component } from 'react';
+import { getStatus, getVersion, VersionInfo, verifyAttestation, TStatus } from '../../../api/bitbox02';
+import { attestationCheckDone, statusChanged } from '../../../api/devicessync';
+import { UnsubscribeList, unsubscribe } from '../../../utils/subscriptions';
+import { route } from '../../../utils/route';
+import { AppUpgradeRequired } from '../../../components/appupgraderequired';
+import { FirmwareUpgradeRequired } from '../upgrade-firmware-required';
+import { Main } from '../../../components/layout';
+import { Unlock } from './unlock';
+import { Pairing } from './setup/pairing';
+import { Wait } from './setup/wait';
+import { SetupOptions, TWalletCreateOptions, TWalletSetupChoices } from './setup/choose';
+import { CreateWallet } from './setup/wallet-create';
+import { RestoreFromSDCard, RestoreFromMnemonic } from './setup/wallet-restore';
+import { CreateWalletSuccess, RestoreFromMnemonicSuccess, RestoreFromSDCardSuccess } from './setup/success';
+
+type Props = {
+  deviceID: string;
+}
+
+type State = {
+    versionInfo?: VersionInfo;
+    attestation: boolean | null;
+    status: '' | TStatus;
+    appStatus: '' | TWalletSetupChoices;
+    createOptions?: TWalletCreateOptions;
+    // if true, we just pair and unlock, so we can hide some steps.
+    unlockOnly: boolean;
+    showWizard: boolean;
+    waitDialog?: {
+        title: string;
+        text?: string;
+    };
+}
+
+export class Wizard extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      attestation: null,
+      status: '',
+      appStatus: '',
+      unlockOnly: true,
+      showWizard: false,
+      waitDialog: undefined,
+    };
+  }
+
+  private unsubscribeList: UnsubscribeList = [];
+
+  public componentDidMount() {
+    const { deviceID } = this.props;
+    getVersion(deviceID).then(versionInfo => {
+      this.setState({ versionInfo });
+    });
+    this.updateAttestationCheck();
+    this.onStatusChanged();
+    this.unsubscribeList = [
+      statusChanged(deviceID, this.onStatusChanged),
+      attestationCheckDone(deviceID, this.updateAttestationCheck),
+    ];
+  }
+
+  private updateAttestationCheck = () => {
+    verifyAttestation(this.props.deviceID).then(attestation => {
+      this.setState({ attestation });
+    });
+  };
+
+  private handleGetStarted = () => {
+    this.setState({ status: '' });
+    route('/account-summary', true);
+  };
+
+  private onStatusChanged = () => {
+    const { showWizard, unlockOnly } = this.state;
+    getStatus(this.props.deviceID).then(status => {
+      if (!showWizard && ['connected', 'unpaired', 'pairingFailed', 'uninitialized', 'seeded'].includes(status)) {
+        this.setState({ showWizard: true });
+      }
+      if (unlockOnly && ['uninitialized', 'seeded'].includes(status)) {
+        this.setState({ unlockOnly: false });
+      }
+      this.setState({ status });
+    });
+  };
+
+  private handleAbort = () => {
+    this.setState({
+      appStatus: '',
+      createOptions: undefined,
+    });
+  };
+
+  public componentWillUnmount() {
+    unsubscribe(this.unsubscribeList);
+  }
+
+  public render() {
+    const { deviceID } = this.props;
+    const {
+      attestation,
+      createOptions,
+      versionInfo,
+      status,
+      appStatus,
+      unlockOnly,
+      showWizard,
+      waitDialog,
+    } = this.state;
+
+    if (status === '') {
+      return null;
+    }
+    if (!versionInfo) {
+      return null;
+    }
+    if (status === 'require_firmware_upgrade') {
+      return (
+        <FirmwareUpgradeRequired
+          deviceID={deviceID}
+          versionInfo={versionInfo} />
+      );
+    }
+    if (status === 'require_app_upgrade') {
+      return <AppUpgradeRequired/>;
+    }
+    if (!showWizard) {
+      return null;
+    }
+    if (waitDialog) {
+      return (
+        <Wait
+          key="wait-view"
+          title={waitDialog.title}
+          text={waitDialog.text} />
+      );
+    }
+
+    return (
+      <Main>
+        { (status === 'connected') ? (
+          <Unlock
+            key="unlock"
+            attestation={attestation} />
+        ) : null }
+
+        { (status === 'unpaired' || status === 'pairingFailed') && (
+          <Pairing
+            key="pairing"
+            deviceID={deviceID}
+            attestation={attestation}
+            pairingFailed={status === 'pairingFailed'} />
+        )}
+
+        { (!unlockOnly && appStatus === '') && (
+          <SetupOptions
+            key="choose-setup"
+            versionInfo={versionInfo}
+            onSelectSetup={(
+              type: TWalletSetupChoices,
+              createOptions?: TWalletCreateOptions,
+            ) => this.setState({ appStatus: type, createOptions })} />
+        )}
+
+        { (!unlockOnly && appStatus === 'create-wallet') && (
+          <CreateWallet
+            backupType={(createOptions?.withMnemonic ? 'mnemonic' : 'sdcard')}
+            backupSeedLength={createOptions?.with12Words ? 16 : 32}
+            deviceID={deviceID}
+            isSeeded={status === 'seeded'}
+            onAbort={this.handleAbort} />
+        )}
+
+        {/* keeping the backups mounted even restoreBackupStatus === 'restore' is not true so it catches potential errors */}
+        { (!unlockOnly && appStatus === 'restore-sdcard' && status !== 'initialized') && (
+          <RestoreFromSDCard
+            key="restore-sdcard"
+            deviceID={deviceID}
+            onAbort={this.handleAbort} />
+        )}
+
+        { (!unlockOnly && appStatus === 'restore-mnemonic' && status !== 'initialized') && (
+          <RestoreFromMnemonic
+            key="restore-mnemonic"
+            deviceID={deviceID}
+            onAbort={this.handleAbort} />
+        )}
+
+        { (appStatus === 'create-wallet' && status === 'initialized') && (
+          <CreateWalletSuccess
+            key="success"
+            backupType={(createOptions?.withMnemonic ? 'mnemonic' : 'sdcard')}
+            onContinue={this.handleGetStarted} />
+        )}
+        { (appStatus === 'restore-sdcard' && status === 'initialized') && (
+          <RestoreFromSDCardSuccess key="backup-success" onContinue={this.handleGetStarted} />
+        )}
+        { (appStatus === 'restore-mnemonic' && status === 'initialized') && (
+          <RestoreFromMnemonicSuccess key="backup-mnemonic-success" onContinue={this.handleGetStarted} />
+        )}
+      </Main>
+    );
+  }
+}
