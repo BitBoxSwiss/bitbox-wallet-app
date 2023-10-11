BitBoxSwiss/bitbox-wallet-app

commit 48eebe98e81fa1c026c45e3b9ed1b1f602aae435

frontend: display warning if camera has to access to scan
Choosing scan QRCode icon in send view should prompt (at least on
macOS) that the application wants to access the camera.

Added a hook to check for camera permissions, so that the app can
display a information text to allow camera accesss when the user
tries to scan.

frontends/web/src/hooks/permissions.ts
@@ -1,0 +1,55 @@
+/**
+ * Copyright 2023 Shift Crypto AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useMountedRef } from './mount';
+
+type TExperimentalDeviceName = 'camera' | 'microphone' | 'speaker';
+
+export const useDevicePermission = (deviceName: TExperimentalDeviceName) => {
+  const mounted = useMountedRef();
+  const permissionObject = useRef<PermissionStatus>();
+  const [permissionState, setPermissionState] = useState<PermissionState>();
+
+  const handlePermissionChange = useCallback(() => {
+    if (mounted.current && permissionObject.current) {
+      setPermissionState(permissionObject.current.state);
+    }
+  }, [mounted]);
+
+  useEffect(() => {
+    navigator.permissions
+      // TypeScript broke this somehow in 4.4.2
+      // https://github.com/microsoft/TypeScript/issues/33923
+      // Type '"camera"' is not assignable to type 'PermissionName'.ts(2322)
+      .query({ name: deviceName } as unknown as PermissionDescriptor)
+      .then((permissionStatus) => {
+        permissionObject.current = permissionStatus;
+        handlePermissionChange();
+      });
+  });
+
+  useEffect(() => {
+    if (permissionObject.current) {
+      permissionObject.current.addEventListener('change', handlePermissionChange);
+      return () => {
+        permissionObject.current?.removeEventListener('change', handlePermissionChange);
+      };
+    }
+  }, [handlePermissionChange, permissionObject]);
+
+  return permissionState; // 'granted', 'denied' or 'prompt'
+};

frontends/web/src/routes/account/send/components/dialogs/scan-qr-dialog.tsx
@@ -32,14 +32,16 @@ export const ScanQRDialog = ({ parseQRResult, activeScanQR, toggleScanQR, onChan
   return (
     <Dialog
       large
+      onClose={toggleScanQR}
       open={activeScanQR}
-      title={t('send.scanQR')}
-      onClose={toggleScanQR}>
-      <ScanQRVideo
-        onResult={result => {
-          parseQRResult(result);
-          onChangeActiveScanQR(false);
-        }} />
+      title={t('send.scanQR')}>
+      <div style={{ minHeight: '300px' }}>
+        <ScanQRVideo
+          onResult={result => {
+            parseQRResult(result);
+            onChangeActiveScanQR(false);
+          }} />
+      </div>
       <DialogButtons>
         <Button
           secondary

frontends/web/src/routes/account/send/components/inputs/scan-qr-video.tsx
@@ -15,7 +15,9 @@
  */
 
 import { useRef } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useQRScanner } from '../../../../../hooks/qrcodescanner';
+import { useDevicePermission } from '../../../../../hooks/permissions';
 import { SpinnerAnimation } from '../../../../../components/spinner/SpinnerAnimation';
 import style from '../../send.module.css';
 
@@ -26,12 +28,18 @@ type TProps = {
 export const ScanQRVideo = ({
   onResult,
 }: TProps) => {
+  const { t } = useTranslation();
   const videoRef = useRef<HTMLVideoElement>(null);
+  const permission = useDevicePermission('camera');
 
   useQRScanner(videoRef, {
     onResult: result => onResult(result.data),
     onError: console.error
   });
+
+  const permissionWarning = permission !== 'granted'
+    ? <div>{t('permission.camera')}</div>
+    : null;
 
   return (
     <>
@@ -40,6 +48,7 @@ export const ScanQRVideo = ({
        gets loaded.*/}
       <div className={style.spinnerAnimationContainer}>
         <SpinnerAnimation />
+        {permissionWarning}
       </div>
       <video
         className={style.qrVideo}

frontends/web/src/routes/account/send/send.module.css
@@ -121,6 +121,8 @@
 .spinnerAnimationContainer {
     align-items: center;
     display: flex;
+    flex-direction: column;
+    gap: 4rem;
     height: 0;
     justify-content: center;
     position: relative;

commit 7cedc0f9788dd30f0ec91508de7b7f97ca3a628a

frontend: fix camera on linux
Regression introduced in 48eebe98e81fa1c026c45e3b9ed1b1f602aae435

That commit added a please-grant-camera-permission warning, in the
same component that contains the video element. It uses a new hook
to check for camera devices and conditionally renders the warning.

Conditionally showing the warning based on the hook re-renders the
component and seems to be the cause for the video to error with:

    DOMException: The play() request was interrupted by a new load request.

This commit is an approach to lift the warning up to the parent
component. It seems to work on macOS, but the warning does not go
away.
Could be that the event listener on permissionstate object is not
working.

frontends/web/src/hooks/permissions.ts
@@ -41,7 +41,7 @@ export const useDevicePermission = (deviceName: TExperimentalDeviceName) => {
         permissionObject.current = permissionStatus;
         handlePermissionChange();
       });
-  });
+  }, [handlePermissionChange, deviceName]);
 
   useEffect(() => {
     if (permissionObject.current) {

frontends/web/src/routes/account/send/components/dialogs/scan-qr-dialog.tsx
@@ -14,9 +14,10 @@
  * limitations under the License.
  */
 
+import { useTranslation } from 'react-i18next';
+import { useDevicePermission } from '../../../../../hooks/permissions';
 import { Dialog, DialogButtons } from '../../../../../components/dialog/dialog';
 import { ScanQRVideo } from '../inputs/scan-qr-video';
-import { useTranslation } from 'react-i18next';
 import { Button } from '../../../../../components/forms';
 
 type TProps = {
@@ -25,6 +26,19 @@ type TProps = {
     onChangeActiveScanQR: (active: boolean) => void;
     parseQRResult: (result: string) => void;
 }
+
+const PermissionWarning = () => {
+  const { t } = useTranslation();
+  const permission = useDevicePermission('camera');
+
+  const permissionWarning = permission !== 'granted'
+    ? t('permission.camera')
+    : null;
+
+  return (
+    <div>{permissionWarning}</div>
+  );
+};
 
 export const ScanQRDialog = ({ parseQRResult, activeScanQR, toggleScanQR, onChangeActiveScanQR }: TProps) => {
   const { t } = useTranslation();
@@ -36,6 +50,7 @@ export const ScanQRDialog = ({ parseQRResult, activeScanQR, toggleScanQR, onChan
       open={activeScanQR}
       title={t('send.scanQR')}>
       <div style={{ minHeight: '300px' }}>
+        <PermissionWarning />
         <ScanQRVideo
           onResult={result => {
             parseQRResult(result);

frontends/web/src/routes/account/send/components/inputs/scan-qr-video.tsx
@@ -15,9 +15,7 @@
  */
 
 import { useRef } from 'react';
-import { useTranslation } from 'react-i18next';
 import { useQRScanner } from '../../../../../hooks/qrcodescanner';
-import { useDevicePermission } from '../../../../../hooks/permissions';
 import { SpinnerAnimation } from '../../../../../components/spinner/SpinnerAnimation';
 import style from '../../send.module.css';
 
@@ -28,18 +26,12 @@ type TProps = {
 export const ScanQRVideo = ({
   onResult,
 }: TProps) => {
-  const { t } = useTranslation();
   const videoRef = useRef<HTMLVideoElement>(null);
-  const permission = useDevicePermission('camera');
 
   useQRScanner(videoRef, {
     onResult: result => onResult(result.data),
     onError: console.error
   });
-
-  const permissionWarning = permission !== 'granted'
-    ? <div>{t('permission.camera')}</div>
-    : null;
 
   return (
     <>
@@ -48,7 +40,6 @@ export const ScanQRVideo = ({
        gets loaded.*/}
       <div className={style.spinnerAnimationContainer}>
         <SpinnerAnimation />
-        {permissionWarning}
       </div>
       <video
         className={style.qrVideo}