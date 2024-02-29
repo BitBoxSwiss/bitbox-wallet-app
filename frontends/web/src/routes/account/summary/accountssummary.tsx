BitBoxSwiss/bitbox-wallet-app

commit 40976c936c7e388bd4a361a04b674d94ae1b3c83

backend/lightning: add lightning balance endpoint
Adds a new backend endpoint that allows to fetch the node balance and
the relative fiat conversions.

frontends/web/src/routes/account/summary/accountssummary.tsx
@@ -23,7 +23,6 @@ import { statusChanged, syncdone } from '../../../api/accountsync';
 import { unsubscribe } from '../../../utils/subscriptions';
 import { useMountedRef } from '../../../hooks/mount';
 import { useSDCard } from '../../../hooks/sdcard';
-import { useLightning } from '../../../hooks/lightning';
 import { Status } from '../../../components/status/status';
 import { GuideWrapper, GuidedContent, Header, Main } from '../../../components/layout';
 import { View } from '../../../components/view/view';
@@ -35,7 +34,6 @@ import { Guide } from '../../../components/guide/guide';
 import { HideAmountsButton } from '../../../components/hideamountsbutton/hideamountsbutton';
 import { AppContext } from '../../../contexts/AppContext';
 import { getAccountsByKeystore, isAmbiguiousName } from '../utils';
-import { NodeState, getNodeInfo, subscribeNodeState } from '../../../api/lightning';
 
 type TProps = {
   accounts: accountApi.IAccount[];
@@ -51,7 +49,6 @@ export function AccountsSummary({ accounts, devices }: TProps) {
   const summaryReqTimerID = useRef<number>();
   const mounted = useMountedRef();
   const { hideAmounts } = useContext(AppContext);
-  const { lightningConfig } = useLightning();
 
   const accountsByKeystore = getAccountsByKeystore(accounts);
 
@@ -59,7 +56,6 @@ export function AccountsSummary({ accounts, devices }: TProps) {
   const [balancePerCoin, setBalancePerCoin] = useState<accountApi.TAccountsBalance>();
   const [accountsTotalBalance, setAccountsTotalBalance] = useState<accountApi.TAccountsTotalBalance>();
   const [balances, setBalances] = useState<Balances>();
-  const [nodeState, setNodeState] = useState<NodeState>();
 
   const hasCard = useSDCard(devices);
 
@@ -171,22 +167,6 @@ export function AccountsSummary({ accounts, devices }: TProps) {
     getAccountsBalance();
   }, [onStatusChanged, getAccountsBalance, accounts]);
 
-  // fetch the lightning node state
-  const onLightningNodeStateChange = useCallback(async () => {
-    try {
-      const nodeState = await getNodeInfo();
-      setNodeState(nodeState);
-    } catch (err) {}
-  }, []);
-
-  // subscribe to any node state changes
-  useEffect(() => {
-    onLightningNodeStateChange();
-
-    const subscriptions = [subscribeNodeState(onLightningNodeStateChange)];
-    return () => unsubscribe(subscriptions);
-  }, [onLightningNodeStateChange]);
-
   return (
     <GuideWrapper>
       <GuidedContent>
@@ -210,14 +190,11 @@ export function AccountsSummary({ accounts, devices }: TProps) {
               (accountsByKeystore.map(({ keystore, accounts }) =>
                 <SummaryBalance
                   keystoreDisambiguatorName={isAmbiguiousName(keystore.name, accountsByKeystore) ? keystore.rootFingerprint : undefined}
-                  connected={keystore.connected}
-                  keystoreName={keystore.name}
-                  key={keystore.rootFingerprint}
+                  accountsKeystore={keystore}
                   accounts={accounts}
                   totalBalancePerCoin={ balancePerCoin ? balancePerCoin[keystore.rootFingerprint] : undefined}
                   totalBalance={ accountsTotalBalance ? accountsTotalBalance[keystore.rootFingerprint] : undefined}
                   balances={balances}
-                  lightningNodeState={(lightningConfig.accounts.length && lightningConfig.accounts[0].rootFingerprint === keystore.rootFingerprint) ? nodeState : undefined}
                 />
               ))}
           </View>

backend/backend.go
@@ -285,8 +285,17 @@ func NewBackend(arguments *arguments.Arguments, environment Environment) (*Backe
 
 	backend.banners = banners.NewBanners()
 	backend.banners.Observe(backend.Notify)
+	btcCoin, err := backend.Coin(coin.CodeBTC)
+	if err != nil {
+		return nil, err
+	}
 
-	backend.lightning = lightning.NewLightning(backend.config, backend.arguments.CacheDirectoryPath(), backend.Keystore, backend.httpClient)
+	backend.lightning = lightning.NewLightning(backend.config,
+		backend.arguments.CacheDirectoryPath(),
+		backend.Keystore, backend.httpClient,
+		backend.ratesUpdater,
+		btcCoin)
+
 	backend.lightning.Observe(backend.Notify)
 
 	return backend, nil

backend/handlers/handlers.go
@@ -257,6 +257,7 @@ func NewHandlers(
 	getAPIRouterNoError(apiRouter)("/lightning/activate-node", handlers.postLightningActivateNode).Methods("POST")
 	getAPIRouterNoError(apiRouter)("/lightning/deactivate-node", handlers.postLightningDeactivateNode).Methods("POST")
 	getAPIRouterNoError(apiRouter)("/lightning/node-info", handlers.getLightningNodeInfo).Methods("GET")
+	getAPIRouterNoError(apiRouter)("/lightning/balance", handlers.getLightningBalance).Methods("GET")
 	getAPIRouterNoError(apiRouter)("/lightning/list-payments", handlers.getLightningListPayments).Methods("GET")
 	getAPIRouterNoError(apiRouter)("/lightning/open-channel-fee", handlers.getLightningOpenChannelFee).Methods("GET")
 	getAPIRouterNoError(apiRouter)("/lightning/parse-input", handlers.getLightningParseInput).Methods("GET")
@@ -1435,6 +1436,10 @@ func (handlers *Handlers) postLightningDeactivateNode(r *http.Request) interface
 
 func (handlers *Handlers) getLightningNodeInfo(r *http.Request) interface{} {
 	return handlers.backend.Lightning().GetNodeInfo(r)
+}
+
+func (handlers *Handlers) getLightningBalance(r *http.Request) interface{} {
+	return handlers.backend.Lightning().GetBalance(r)
 }
 
 func (handlers *Handlers) getLightningListPayments(r *http.Request) interface{} {

backend/lightning/handlers.go
@@ -20,6 +20,8 @@ import (
 	"net/http"
 
 	"github.com/breez/breez-sdk-go/breez_sdk"
+	"github.com/digitalbitbox/bitbox-wallet-app/backend/accounts"
+	"github.com/digitalbitbox/bitbox-wallet-app/backend/coins/coin"
 )
 
 // PostLightningActivateNode handles the POST request to activate the lightning node.
@@ -42,7 +44,7 @@ func (lightning *Lightning) PostLightningDeactivateNode(r *http.Request) interfa
 	return responseDto{Success: true}
 }
 
-// GetNodeInfo handles the GET request retrieve the node info.
+// GetNodeInfo handles the GET request to retrieve the node info.
 func (lightning *Lightning) GetNodeInfo(_ *http.Request) interface{} {
 	if lightning.sdkService == nil {
 		return responseDto{Success: false, ErrorMessage: "BreezServices not initialized"}
@@ -54,6 +56,32 @@ func (lightning *Lightning) GetNodeInfo(_ *http.Request) interface{} {
 	}
 
 	return responseDto{Success: true, Data: toNodeStateDto(nodeState)}
+}
+
+// GetBalance handles the GET request to retrieve the node balance and its fiat conversions.
+func (lightning *Lightning) GetBalance(_ *http.Request) interface{} {
+	balance, err := lightning.Balance()
+	if err != nil {
+		return responseDto{Success: false, ErrorMessage: err.Error()}
+	}
+
+	formatAsSats := lightning.backendConfig.AppConfig().Backend.BtcUnit == coin.BtcUnitSats
+	btcCoin := lightning.btcCoin
+
+	formattedAvailableAmount := coin.FormattedAmount{
+		Amount:      btcCoin.FormatAmount(balance.Available(), false),
+		Unit:        btcCoin.GetFormatUnit(false),
+		Conversions: coin.Conversions(balance.Available(), btcCoin, false, lightning.ratesUpdater, formatAsSats),
+	}
+
+	return responseDto{
+		Success: true,
+		Data: accounts.FormattedAccountBalance{
+			HasAvailable: balance.Available().BigInt().Sign() > 0,
+			Available:    formattedAvailableAmount,
+			HasIncoming:  false,
+			Incoming:     coin.FormattedAmount{},
+		}}
 }
 
 // GetListPayments handles the GET request to list payments.

backend/lightning/lightning.go
@@ -23,9 +23,12 @@ import (
 	"strings"
 
 	"github.com/breez/breez-sdk-go/breez_sdk"
+	"github.com/digitalbitbox/bitbox-wallet-app/backend/accounts"
 	"github.com/digitalbitbox/bitbox-wallet-app/backend/accounts/types"
+	"github.com/digitalbitbox/bitbox-wallet-app/backend/coins/coin"
 	"github.com/digitalbitbox/bitbox-wallet-app/backend/config"
 	"github.com/digitalbitbox/bitbox-wallet-app/backend/keystore"
+	"github.com/digitalbitbox/bitbox-wallet-app/backend/rates"
 	"github.com/digitalbitbox/bitbox-wallet-app/backend/util"
 	"github.com/digitalbitbox/bitbox-wallet-app/util/errp"
 	"github.com/digitalbitbox/bitbox-wallet-app/util/logging"
@@ -45,31 +48,40 @@ const (
 type Lightning struct {
 	observable.Implementation
 
-	config             *config.Config
+	backendConfig      *config.Config
 	cacheDirectoryPath string
 	getKeystore        func() keystore.Keystore
 	synced             bool
 
-	log        *logrus.Entry
-	sdkService *breez_sdk.BlockingBreezServices
-	httpClient *http.Client
+	log          *logrus.Entry
+	sdkService   *breez_sdk.BlockingBreezServices
+	httpClient   *http.Client
+	ratesUpdater *rates.RateUpdater
+	btcCoin      coin.Coin
 }
 
 // NewLightning creates a new instance of the Lightning struct.
-func NewLightning(config *config.Config, cacheDirectoryPath string, getKeystore func() keystore.Keystore, httpClient *http.Client) *Lightning {
+func NewLightning(config *config.Config,
+	cacheDirectoryPath string,
+	getKeystore func() keystore.Keystore,
+	httpClient *http.Client,
+	ratesUpdater *rates.RateUpdater,
+	btcCoin coin.Coin) *Lightning {
 	return &Lightning{
-		config:             config,
+		backendConfig:      config,
 		cacheDirectoryPath: cacheDirectoryPath,
 		getKeystore:        getKeystore,
 		log:                logging.Get().WithGroup("lightning"),
 		synced:             false,
 		httpClient:         httpClient,
+		ratesUpdater:       ratesUpdater,
+		btcCoin:            btcCoin,
 	}
 }
 
 // Activate first creates a mnemonic from the keystore entropy then connects to instance.
 func (lightning *Lightning) Activate() error {
-	lightningConfig := lightning.config.LightningConfig()
+	lightningConfig := lightning.backendConfig.LightningConfig()
 
 	if len(lightningConfig.Accounts) > 0 {
 		return errp.New("Lightning accounts already configured")
@@ -137,7 +149,7 @@ func (lightning *Lightning) Disconnect() {
 
 // Deactivate disconnects the instance and changes the config to inactive.
 func (lightning *Lightning) Deactivate() error {
-	lightningConfig := lightning.config.LightningConfig()
+	lightningConfig := lightning.backendConfig.LightningConfig()
 
 	if len(lightningConfig.Accounts) == 0 {
 		return nil
@@ -154,13 +166,37 @@ func (lightning *Lightning) Deactivate() error {
 	return nil
 }
 
+// CheckActive returns an error if the lightning service not has been activated.
+func (lightning *Lightning) CheckActive() error {
+	lightningConfig := lightning.backendConfig.LightningConfig()
+	if len(lightningConfig.Accounts) == 0 || lightning.sdkService == nil {
+		return errp.New("Lightning not initialized")
+	}
+	return nil
+}
+
+// Balance returns the balance of the lightning account.
+func (lightning *Lightning) Balance() (*accounts.Balance, error) {
+	if err := lightning.CheckActive(); err != nil {
+		return nil, err
+	}
+
+	nodeInfo, err := lightning.sdkService.NodeInfo()
+	if err != nil {
+		return nil, err
+	}
+
+	amount := coin.NewAmountFromInt64(int64(nodeInfo.ChannelsBalanceMsat / 1000))
+	return accounts.NewBalance(amount, coin.Amount{}), nil
+}
+
 func accountBreezFolder(accountCode types.Code) string {
 	return strings.Join([]string{"breez-", string(accountCode)}, "")
 }
 
 // connect initializes the connection configuration and calls connect to create a Breez SDK instance.
 func (lightning *Lightning) connect(registerNode bool) error {
-	lightningConfig := lightning.config.LightningConfig()
+	lightningConfig := lightning.backendConfig.LightningConfig()
 
 	if len(lightningConfig.Accounts) > 0 && lightning.sdkService == nil {
 		initializeLogging(lightning.log)
@@ -232,7 +268,7 @@ func (lightning *Lightning) connect(registerNode bool) error {
 }
 
 func (lightning *Lightning) setLightningConfig(config config.LightningConfig) error {
-	if err := lightning.config.SetLightningConfig(config); err != nil {
+	if err := lightning.backendConfig.SetLightningConfig(config); err != nil {
 		lightning.log.WithError(err).Warn("Error updating lightning config")
 		return errp.New("Error updating lightning config")
 	}

frontends/web/src/api/lightning.ts
@@ -15,7 +15,7 @@
  */
 
 import { apiGet, apiPost } from '../utils/request';
-import { AccountCode } from './account';
+import { AccountCode, IBalance } from './account';
 import { TSubscriptionCallback, subscribeEndpoint } from './subscribe';
 import qs from 'query-string';
 
@@ -900,6 +900,10 @@ export const postActivateNode = async (): Promise<void> => {
 
 export const postDeactivateNode = async (): Promise<void> => {
   return postApiResponse<void, undefined>('lightning/deactivate-node', undefined, 'Error calling postDeactivateNode');
+};
+
+export const getLightningBalance = async (): Promise<IBalance> => {
+  return getApiResponse<IBalance>('lightning/balance', 'Error calling getLightningBalance');
 };
 
 /**

frontends/web/src/routes/account/summary/summarybalance.tsx
@@ -24,9 +24,10 @@ import { Skeleton } from '../../../components/skeleton/skeleton';
 import { Badge } from '../../../components/badge/badge';
 import { USBSuccess } from '../../../components/icon';
 import style from './accountssummary.module.css';
-import { NodeState } from '../../../api/lightning';
-import { useEffect, useState } from 'react';
-import { toSat } from '../../../utils/conversion';
+import { useLightning } from '../../../hooks/lightning';
+import { useCallback, useEffect, useState } from 'react';
+import { getLightningBalance, subscribeNodeState } from '../../../api/lightning';
+import { unsubscribe } from '../../../utils/subscriptions';
 
 function TotalBalance({ total, fiatUnit }: accountApi.TAccountTotalBalance) {
   return (
@@ -41,13 +42,11 @@ function TotalBalance({ total, fiatUnit }: accountApi.TAccountTotalBalance) {
 
 type TProps = {
   accounts: accountApi.IAccount[];
-  connected: boolean;
-  keystoreName: string;
+  accountsKeystore: accountApi.TKeystore;
   totalBalancePerCoin?: accountApi.TAccountsBalanceByCoin;
   totalBalance?: accountApi.TAccountTotalBalance;
   balances?: Balances;
   keystoreDisambiguatorName?: string;
-  lightningNodeState?: NodeState;
 };
 
 type TAccountCoinMap = {
@@ -56,16 +55,29 @@ type TAccountCoinMap = {
 
 export function SummaryBalance({
   accounts,
-  connected,
-  keystoreName,
+  accountsKeystore,
   totalBalancePerCoin,
   totalBalance,
   balances,
   keystoreDisambiguatorName,
-  lightningNodeState
 }: TProps) {
   const { t } = useTranslation();
+  const { lightningConfig } = useLightning();
   const [lightningBalance, setLightningBalance] = useState<accountApi.IBalance>();
+
+  const fetchLightningBalance = useCallback(async () => {
+    setLightningBalance(await getLightningBalance());
+  }, []);
+
+  // if there is an active lightning account derived from this keystore, subscribe to any node state changes.
+  useEffect(() => {
+    const lightningAccounts = lightningConfig.accounts;
+    if (lightningAccounts.length && lightningAccounts[0].rootFingerprint === accountsKeystore.rootFingerprint) {
+      fetchLightningBalance();
+      const subscriptions = [subscribeNodeState(fetchLightningBalance)];
+      return () => unsubscribe(subscriptions);
+    }
+  }, [fetchLightningBalance, accountsKeystore, lightningConfig]);
 
   const getAccountsPerCoin = () => {
     return accounts.reduce((accountPerCoin, account) => {
@@ -77,30 +89,13 @@ export function SummaryBalance({
   const accountsPerCoin = getAccountsPerCoin();
   const coins = Object.keys(accountsPerCoin) as accountApi.CoinCode[];
 
-  useEffect(() => {
-    if (lightningNodeState) {
-      setLightningBalance({
-        hasAvailable: lightningNodeState.channelsBalanceMsat > 0,
-        available: {
-          amount: `${toSat(lightningNodeState.channelsBalanceMsat)}`,
-          unit: 'sat'
-        },
-        hasIncoming: false,
-        incoming: {
-          amount: '0',
-          unit: 'sat'
-        }
-      });
-    }
-  }, [lightningNodeState, lightningNodeState?.channelsBalanceMsat]);
-
   return (
     <div>
       <div className={style.accountName}>
         <p>
-          {keystoreName} {keystoreDisambiguatorName && `(${keystoreDisambiguatorName})`}
+          {accountsKeystore.name} {keystoreDisambiguatorName && `(${keystoreDisambiguatorName})`}
         </p>
-        {connected ? (
+        {accountsKeystore.connected ? (
           <Badge icon={(props) => <USBSuccess {...props} />} type="success">
             {t('device.keystoreConnected')}
           </Badge>
@@ -121,7 +116,7 @@ export function SummaryBalance({
             </tr>
           </thead>
           <tbody>
-            {lightningNodeState && (
+            {lightningBalance && (
               <BalanceRow key="lightning" code="lightning" name="Lightning" coinCode="lightning" balance={lightningBalance} />
             )}
             {accounts.length > 0 ? (

frontends/web/src/routes/lightning/lightning.tsx
@@ -18,14 +18,13 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import * as accountApi from '../../api/account';
-import { getNodeInfo, getListPayments, subscribeListPayments, subscribeNodeState, NodeState, Payment } from '../../api/lightning';
+import { getListPayments, subscribeListPayments, subscribeNodeState, Payment, getLightningBalance } from '../../api/lightning';
 import { Balance } from '../../components/balance/balance';
 import { View, ViewHeader } from '../../components/view/view';
 import { GuideWrapper, GuidedContent, Header, Main } from '../../components/layout';
 import { Spinner } from '../../components/spinner/Spinner';
 import { ActionButtons } from './actionButtons';
 import { LightningGuide } from './guide';
-import { toSat } from '../../utils/conversion';
 import { Payments } from './components/payments';
 import { unsubscribe } from '../../utils/subscriptions';
 
@@ -33,18 +32,14 @@ export function Lightning() {
   const { t } = useTranslation();
   const [balance, setBalance] = useState<accountApi.IBalance>();
   const [syncedAddressesCount] = useState<number>();
-  const [nodeState, setNodeState] = useState<NodeState>();
   const [payments, setPayments] = useState<Payment[]>();
   const [error, setError] = useState<string>();
 
   const onStateChange = useCallback(async () => {
     try {
       setError(undefined);
-      const nodeState = await getNodeInfo();
-      const payments = await getListPayments({});
-
-      setNodeState(nodeState);
-      setPayments(payments);
+      setBalance(await getLightningBalance());
+      setPayments(await getListPayments({}));
     } catch (err: any) {
       const errorMessage = err?.errorMessage || err;
       setError(errorMessage);
@@ -58,23 +53,6 @@ export function Lightning() {
     return () => unsubscribe(subscriptions);
   }, [onStateChange]);
 
-  useEffect(() => {
-    if (nodeState) {
-      setBalance({
-        hasAvailable: nodeState.channelsBalanceMsat > 0,
-        available: {
-          amount: `${toSat(nodeState.channelsBalanceMsat)}`,
-          unit: 'sat'
-        },
-        hasIncoming: false,
-        incoming: {
-          amount: '0',
-          unit: 'sat'
-        }
-      });
-    }
-  }, [nodeState, nodeState?.channelsBalanceMsat]);
-
   const hasDataLoaded = balance !== undefined;
 
   if (error) {
@@ -85,7 +63,7 @@ export function Lightning() {
     );
   }
 
-  if (!nodeState) {
+  if (!balance) {
     // Wait for the nodeState to become available
     return <Spinner guideExists={false} />;
   }

commit d5b9d09065d8408cfef1b8cc8b390c5b64ab773e

frontend: add missing key prop
Small regression introduced in 40976c936c7e388bd4a361a04b674d94ae1b3c83

Fixes the error:

accountssummary.tsx:180 Warning: Each child in a list should have a
unique "key" prop.

Check the render method of `AccountsSummary`.

frontends/web/src/routes/account/summary/accountssummary.tsx
@@ -189,6 +189,7 @@ export function AccountsSummary({ accounts, devices }: TProps) {
             {accountsByKeystore &&
               (accountsByKeystore.map(({ keystore, accounts }) =>
                 <SummaryBalance
+                  key={keystore.rootFingerprint}
                   keystoreDisambiguatorName={isAmbiguiousName(keystore.name, accountsByKeystore) ? keystore.rootFingerprint : undefined}
                   accountsKeystore={keystore}
                   accounts={accounts}