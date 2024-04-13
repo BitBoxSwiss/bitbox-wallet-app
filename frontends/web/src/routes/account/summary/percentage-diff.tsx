f96b635154156c095f1d8c917142e445d9bbc2b1
thisconnect <this@shiftcrypto.ch>
2024-04-13 13:12:03 +0200

    frontend: move arrow icons to icon component
    The account-summary arrows are static and do not have to be in JSX.
    
    Moved to svg file and import as regular image.

--- frontends/web/src/routes/account/summary/percentage-diff.tsx
+++ frontends/web/src/routes/account/summary/percentage-diff.tsx
@@ -16,40 +16,8 @@
 
 import { i18n } from '../../../i18n/i18n';
 import { localizePercentage } from '../../../utils/localize';
+import { ArrowDownRed, ArrowUpGreen } from '../../../components/icon';
 import styles from './percentage-diff.module.css';
-
-export const ArrowUp = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round">
-    <line x1="12" y1="19" x2="12" y2="5"></line>
-    <polyline points="5 12 12 5 19 12"></polyline>
-  </svg>
-);
-
-export const ArrowDown = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round">
-    <line x1="12" y1="5" x2="12" y2="19"></line>
-    <polyline points="19 12 12 19 5 12"></polyline>
-  </svg>
-);
-
 
 type TPercentageDiff = {
   hasDifference: boolean;
@@ -75,9 +43,9 @@ export const PercentageDiff = ({
         <>
           <span className={styles.arrow}>
             {positive ? (
-              <ArrowUp />
+              <ArrowUpGreen />
             ) : (
-              <ArrowDown />
+              <ArrowDownRed />
             )}
           </span>
           <span className={styles.diffValue}>
