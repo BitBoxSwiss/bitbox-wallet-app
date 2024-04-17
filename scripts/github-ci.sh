801550c2753242f12b36ce5312d9bcd2600c7dc0
thisconnect <this@shiftcrypto.ch>
2024-04-17 11:08:29 +0200

    nodejs: update node to latest lts version 20.x

--- scripts/github-ci.sh
+++ scripts/github-ci.sh
@@ -35,7 +35,7 @@ if [ "$OS_NAME" == "osx" ]; then
     brew install qt@5
     brew install nvm
     source /usr/local/opt/nvm/nvm.sh
-    nvm install 18 # install this node version
+    nvm install 20 # install this node version
     export PATH="/usr/local/opt/qt@5/bin:$PATH"
     export LDFLAGS="-L/usr/local/opt/qt@5/lib"
     export CPPFLAGS="-I/usr/local/opt/qt@5/include"
