0049dec05c6f2cefd5a7370ab3cffff94641e45d
thisconnect <this@shiftcrypto.ch>
2024-05-06 14:39:17 +0200

    backend: allow links to app stores
    There are 2 links to the BitBox01 2FA app in the old BitBox01
    settings under 'Pair mobile app' - 'Show QR code'.

--- backend/backend.go
+++ backend/backend.go
@@ -97,6 +97,9 @@ var fixedURLWhitelist = []string{
 	"https://bitcoincore.org/en/2016/01/26/segwit-benefits/",
 	"https://en.bitcoin.it/wiki/Bech32_adoption",
 	"https://github.com/bitcoin/bips/",
+	// app stores
+	"https://itunes.apple.com/",
+	"https://play.google.com/store/",
 	// Others
 	"https://cointracking.info/import/bitbox/",
 }
