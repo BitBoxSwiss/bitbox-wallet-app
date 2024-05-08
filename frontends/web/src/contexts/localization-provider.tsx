616d3feba48f91860c97c86747bda96143b311bb
sl <seangeekpro@gmail.com>
2024-05-08 19:23:34 +0200

    frontend: expose useLocalizedPunctuation in Context API
    useLocalizedPunctuation may get noisy (log/warnings) if native
    locale is empty or invalid. This is due to the hook
    being used in many places and thus it runs indpendently
    of one another.
    
    Using context API, we can expose this globally and every
    consumer will get the same instance of useLocalizedPunctuation.
    This reduces the calls of useLocalizedPunctuation which results
    in fixing the warning noises.

---
+++ frontends/web/src/contexts/localization-provider.tsx
@@ -0,0 +1,19 @@
+import { ReactNode, useContext } from 'react';
+import { useLocalizedPunctuation } from '../hooks/localized';
+import { LocalizationContext } from './localization-context';
+import { AppContext } from './AppContext';
+
+type TProps = {
+    children: ReactNode;
+  }
+
+export const LocalizationProvider = ({ children }: TProps) => {
+  const { nativeLocale } = useContext(AppContext);
+  const { decimal, group } = useLocalizedPunctuation(nativeLocale);
+
+  return (
+    <LocalizationContext.Provider value={{ decimal, group }}>
+      {children}
+    </LocalizationContext.Provider>
+  );
+};
