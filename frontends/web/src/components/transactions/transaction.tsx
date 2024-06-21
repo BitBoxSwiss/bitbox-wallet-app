5630a8f85dd208f4a446828cdfac48c29265ba01
NicolaLS <nicolaleonardo.susca@gmail.com>
2024-06-21 14:29:18 -0600

    frontend: extract transaction direction arrow
    Extract the arrow used in transaction.tsx to indicate the transaction
    direction into its own component in transactions/components. This
    improves readability of the transaction component, avoids re-declaring
    the logic every render and makes the arrow reusable which we will use
    when extracting the transaction details dialog into its own component.

--- frontends/web/src/components/transactions/transaction.tsx
+++ frontends/web/src/components/transactions/transaction.tsx
@@ -21,13 +21,13 @@ import * as accountApi from '../../api/account';
 import { A } from '../anchor/anchor';
 import { Dialog } from '../dialog/dialog';
 import { CopyableInput } from '../copy/Copy';
-import { Warning, ExpandIcon } from '../icon/icon';
+import { ExpandIcon } from '../icon/icon';
 import { ProgressRing } from '../progressRing/progressRing';
 import { FiatConversion } from '../rates/rates';
 import { Amount } from '../../components/amount/amount';
-import { ArrowIn, ArrowOut, ArrowSelf } from './components/icons';
 import { Note } from './note';
 import { TxDetail } from './components/detail';
+import { Arrow } from './components/arrow';
 import parentStyle from './transactions.module.css';
 import style from './transaction.module.css';
 
@@ -77,15 +77,6 @@ export const Transaction = ({
       .catch(console.error);
   };
 
-  const arrow = status === 'failed' ? (
-    <Warning style={{ maxWidth: '18px' }} />
-  ) : type === 'receive' ? (
-    <ArrowIn />
-  ) : type === 'send' ? (
-    <ArrowOut />
-  ) : (
-    <ArrowSelf />
-  );
   const sign = ((type === 'send') && '−') || ((type === 'receive') && '+') || '';
   const typeClassName = (status === 'failed' && style.failed) || (type === 'send' && style.send) || (type === 'receive' && style.receive) || '';
   const shortDate = time ? parseTimeShort(time, i18n.language) : '---';
@@ -96,7 +87,12 @@ export const Transaction = ({
     <div className={`${style.container}${index === 0 ? ' ' + style.first : ''}`}>
       <div className={`${parentStyle.columns} ${style.row}`}>
         <div className={parentStyle.columnGroup}>
-          <div className={parentStyle.type}>{arrow}</div>
+          <div className={parentStyle.type}>
+            <Arrow
+              status={status}
+              type={type}
+            />
+          </div>
           <div className={parentStyle.date}>
             <span className={style.columnLabel}>
               {t('transaction.details.date')}:
@@ -181,7 +177,12 @@ export const Transaction = ({
               internalID={internalID}
               note={note}
             />
-            <TxDetail label={t('transaction.details.type')}>{arrow}</TxDetail>
+            <TxDetail label={t('transaction.details.type')}>
+              <Arrow
+                status={status}
+                type={type}
+              />
+            </TxDetail>
             <TxDetail label={t('transaction.confirmation')}>{numConfirmations}</TxDetail>
             <TxDetail label={t('transaction.details.status')}>
               <ProgressRing
