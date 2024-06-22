cbbc7944ff5a5d12179b44a9e7528c5fc8c9a937
NicolaLS <nicolaleonardo.susca@gmail.com>
2024-06-21 20:22:00 -0600

    frontend: create DetailsDialog component
    Extract the transactions details dialog in transactions.tsx into its own
    component to make the transaction component itself more concice and
    easier to understand.

--- frontends/web/src/components/transactions/transaction.tsx
+++ frontends/web/src/components/transactions/transaction.tsx
@@ -18,17 +18,14 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import * as accountApi from '@/api/account';
-import { A } from '@/components/anchor/anchor';
-import { Dialog } from '@/components/dialog/dialog';
 import { FiatConversion } from '@/components/rates/rates';
 import { Amount } from '@/components/amount/amount';
-import { Note } from './note';
-import { TxDetail } from './components/detail';
 import { Arrow } from './components/arrow';
-import { TxDate, TxDateDetail } from './components/date';
-import { TxStatus, TxStatusDetail } from './components/status';
+import { TxDate } from './components/date';
+import { TxStatus } from './components/status';
 import { ShowDetailsButton } from './components/show-details-button';
-import { TxAddress, TxDetailCopyableValues } from './components/address-or-txid';
+import { TxAddress } from './components/address-or-txid';
+import { TxDetailsDialog } from './components/details';
 import parentStyle from './transactions.module.css';
 import style from './transaction.module.css';
 
@@ -45,7 +42,6 @@ export const Transaction = ({
   explorerURL,
   type,
   amount,
-  feeRatePerKb,
   numConfirmations,
   numConfirmationsComplete,
   time,
@@ -55,19 +51,6 @@ export const Transaction = ({
 }: Props) => {
   const { t } = useTranslation();
   const [transactionDialog, setTransactionDialog] = useState<boolean>(false);
-  const [transactionInfo, setTransactionInfo] = useState<accountApi.ITransaction>();
-
-  const showDetails = () => {
-    accountApi.getTransaction(accountCode, internalID).then(transaction => {
-      if (!transaction) {
-        console.error('Unable to retrieve transaction ' + internalID);
-        return null;
-      }
-      setTransactionInfo(transaction);
-      setTransactionDialog(true);
-    })
-      .catch(console.error);
-  };
 
   const sign = ((type === 'send') && '−') || ((type === 'receive') && '+') || '';
   const typeClassName = (status === 'failed' && style.failed) || (type === 'send' && style.send) || (type === 'receive' && style.receive) || '';
@@ -96,7 +79,7 @@ export const Transaction = ({
             />
           )}
           <ShowDetailsButton
-            onClick={showDetails}
+            onClick={() => setTransactionDialog(true)}
             expand={!transactionDialog}
             hideOnMedium
           />
@@ -122,132 +105,27 @@ export const Transaction = ({
             </span>
           </div>
           <ShowDetailsButton
-            onClick={showDetails}
+            onClick={() => setTransactionDialog(true)}
             expand={!transactionDialog}
           />
         </div>
       </div>
-      {/*
-        Amount and Confirmations info are displayed using props data
-        instead of transactionInfo because they are live updated.
-      */}
-      <Dialog
+      <TxDetailsDialog
         open={transactionDialog}
-        title={t('transaction.details.title')}
         onClose={() => setTransactionDialog(false)}
-        slim
-        medium>
-        {transactionInfo && (
-          <>
-            <Note
-              accountCode={accountCode}
-              internalID={internalID}
-              note={note}
-            />
-            <TxDetail label={t('transaction.details.type')}>
-              <Arrow
-                status={status}
-                type={type}
-              />
-            </TxDetail>
-            <TxDetail label={t('transaction.confirmation')}>{numConfirmations}</TxDetail>
-            <TxStatusDetail
-              status={status}
-              numConfirmations={numConfirmations}
-              numConfirmationsComplete={numConfirmationsComplete}
-            />
-            <TxDateDetail time={time} />
-            <TxDetail label={t('transaction.details.fiat')}>
-              <span className={`${style.fiat} ${typeClassName}`}>
-                <FiatConversion amount={amount} sign={sign} noAction />
-              </span>
-            </TxDetail>
-            <TxDetail label={t('transaction.details.fiatAtTime')}>
-              <span className={`${style.fiat} ${typeClassName}`}>
-                {transactionInfo.amountAtTime ?
-                  <FiatConversion amount={transactionInfo.amountAtTime} sign={sign} noAction />
-                  :
-                  <FiatConversion noAction />
-                }
-              </span>
-            </TxDetail>
-            <TxDetail label={t('transaction.details.amount')}>
-              <span className={`${style.amount} ${typeClassName}`}>
-                {sign}
-                <Amount amount={amount.amount} unit={amount.unit} />
-              </span>
-              {' '}
-              <span className={`${style.currencyUnit} ${typeClassName}`}>{transactionInfo.amount.unit}</span>
-            </TxDetail>
-            {
-              transactionInfo.fee && transactionInfo.fee.amount ? (
-                <TxDetail
-                  label={t('transaction.fee')}
-                  title={feeRatePerKb.amount ? feeRatePerKb.amount + ' ' + feeRatePerKb.unit + '/Kb' : ''}
-                >
-                  <Amount amount={transactionInfo.fee.amount} unit={transactionInfo.fee.unit} />
-                  {' '}
-                  <span className={style.currencyUnit}>{transactionInfo.fee.unit}</span>
-                </TxDetail>
-              ) : (
-                <TxDetail label={t('transaction.fee')}>---</TxDetail>
-              )
-            }
-            <TxDetailCopyableValues
-              label={t('transaction.details.address')}
-              values={transactionInfo.addresses}
-            />
-            {
-              transactionInfo.gas ? (
-                <TxDetail label={t('transaction.gas')}>{transactionInfo.gas}</TxDetail>
-              ) : null
-            }
-            {
-              transactionInfo.nonce ? (
-                <TxDetail label="Nonce">{transactionInfo.nonce}</TxDetail>
-              ) : null
-            }
-            {
-              transactionInfo.weight ? (
-                <TxDetail label={t('transaction.weight')}>
-                  {transactionInfo.weight}
-                  {' '}
-                  <span className={style.currencyUnit}>WU</span>
-                </TxDetail>
-              ) : null
-            }
-            {
-              transactionInfo.vsize ? (
-                <TxDetail label={t('transaction.vsize')}>
-                  {transactionInfo.vsize}
-                  {' '}
-                  <span className={style.currencyUnit}>b</span>
-                </TxDetail>
-              ) : null
-            }
-            {
-              transactionInfo.size ? (
-                <TxDetail label={t('transaction.size')}>
-                  {transactionInfo.size}
-                  {' '}
-                  <span className={style.currencyUnit}>b</span>
-                </TxDetail>
-              ) : null
-            }
-            <TxDetailCopyableValues
-              label={t('transaction.explorer')}
-              values={[transactionInfo.txID]}
-            />
-            <div className={`${style.detail} flex-center`}>
-              <A
-                href={explorerURL + transactionInfo.txID}
-                title={`${t('transaction.explorerTitle')}\n${explorerURL}${transactionInfo.txID}`}>
-                {t('transaction.explorerTitle')}
-              </A>
-            </div>
-          </>
-        )}
-      </Dialog>
+        accountCode={accountCode}
+        internalID={internalID}
+        note={note}
+        status={status}
+        type={type}
+        numConfirmations={numConfirmations}
+        numConfirmationsComplete={numConfirmationsComplete}
+        time={time}
+        amount={amount}
+        sign={sign}
+        typeClassName={typeClassName}
+        explorerURL={explorerURL}
+      />
     </div>
   );
 };
