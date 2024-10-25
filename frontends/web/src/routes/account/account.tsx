BitBoxSwiss/bitbox-wallet-app

commit ead160208dd13f6c066470d297c3d32b0c5eade1

frontend: update layout so that the transaction items go the edge

frontends/web/src/routes/account/account.module.css
@@ -9,15 +9,10 @@
     justify-content: flex-start;
 }
 
-.header {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: flex-start;
-    flex: none;
-    margin: 0 auto;
-    padding: var(--spacing-large) var(--spacing-large) 0 var(--spacing-large);
-    width: 800px;
+@media (max-width: 768px) {
+    .accountHeader {
+        padding: 0 var(--space-half);
+    }
 }
 
 .actionsContainer {

frontends/web/src/routes/account/account.tsx
@@ -314,45 +314,47 @@ export const Account = ({
             </Link>
             <HideAmountsButton />
           </Header>
+          {status.synced && hasDataLoaded && isBitcoinBased(account.coinCode) && (
+            <HeadersSync coinCode={account.coinCode} />
+          )}
           <View>
-            <ViewContent>
-              {status.synced && hasDataLoaded && isBitcoinBased(account.coinCode) && (
-                <HeadersSync coinCode={account.coinCode} />
-              )}
-              <label className="labelXLarge">
-                {t('accountSummary.availableBalance')}
-              </label>
-              <div className="flex flex-row flex-between flex-item-center flex-column-mobile">
-                <Balance balance={balance} />
-                {!isAccountEmpty && <ActionButtons {...actionButtonsProps} />}
-              </div>
-              {isAccountEmpty && (
-                <BuyReceiveCTA
-                  account={account}
-                  code={code}
-                  exchangeSupported={exchangeSupported}
-                  unit={balance.available.unit}
-                  balanceList={[balance]}
-                />
-              )}
-
-              {!transactions?.success ? (
-                <div className={`flex flex-row flex-center ${style.empty}`}>
-                  <p>{t('transactions.errorLoadTransactions')}</p>
+            <ViewContent fullWidth>
+              <div className={style.accountHeader}>
+                <label className="labelXLarge">
+                  {t('accountSummary.availableBalance')}
+                </label>
+                <div className="flex flex-row flex-between flex-item-center flex-column-mobile">
+                  <Balance balance={balance} />
+                  {!isAccountEmpty && <ActionButtons {...actionButtonsProps} />}
                 </div>
-              ) : (
-                <SubTitle className={style.titleWithButton}>
-                  {t('accountSummary.transactionHistory')}
-                  {hasTransactions && (
-                    <Button
-                      transparent
-                      onClick={exportAccount}
-                      title={t('account.exportTransactions')}>
-                      {t('account.export')}
-                    </Button>
-                  )}
-                </SubTitle>
-              )}
+                {isAccountEmpty && (
+                  <BuyReceiveCTA
+                    account={account}
+                    code={code}
+                    exchangeSupported={exchangeSupported}
+                    unit={balance.available.unit}
+                    balanceList={[balance]}
+                  />
+                )}
+
+                {!transactions?.success ? (
+                  <div className={`flex flex-row flex-center ${style.empty}`}>
+                    <p>{t('transactions.errorLoadTransactions')}</p>
+                  </div>
+                ) : (
+                  <SubTitle className={style.titleWithButton}>
+                    {t('accountSummary.transactionHistory')}
+                    {hasTransactions && (
+                      <Button
+                        transparent
+                        onClick={exportAccount}
+                        title={t('account.exportTransactions')}>
+                        {t('account.export')}
+                      </Button>
+                    )}
+                  </SubTitle>
+                )}
+              </div>
 
               {hasTransactions ? (
                 transactions.list.map(tx => (

commit 9b3d6a3644d706f3aa7c5da984d59af3ebb49875

frontend: fix padding of empty wallet message on mobile
Empty transaction message did not have any padding on mobile.

Regression introduced in ead160208dd13f6c066470d297c3d32b0c5eade1

frontends/web/src/routes/account/account.module.css
@@ -54,7 +54,6 @@
     justify-content: flex-end;
 }
 
-
 .withWalletConnect .walletConnect {
     display: flex;
 }
@@ -85,6 +84,10 @@
     margin-right: var(--space-eight);
 }
 
+.errorLoadTransactions {
+    text-align: center;
+}
+
 .titleWithButton {
     align-items: baseline;
     color: var(--color-secondary);
@@ -96,6 +99,11 @@
     padding-left: var(--space-half);
     padding-right: 0;
     text-align: right;
+}
+
+.emptyTransactions {
+    color: var(--color-secondary);
+    text-align: center;
 }
 
 @media (max-width: 768px) {

frontends/web/src/routes/account/account.tsx
@@ -338,10 +338,10 @@ export const Account = ({
                 )}
 
                 {!transactions?.success ? (
-                  <div className={`flex flex-row flex-center ${style.empty}`}>
-                    <p>{t('transactions.errorLoadTransactions')}</p>
-                  </div>
-                ) : (
+                  <p className={style.errorLoadTransactions}>
+                    {t('transactions.errorLoadTransactions')}
+                  </p>
+                ) : !isAccountEmpty && (
                   <SubTitle className={style.titleWithButton}>
                     {t('accountSummary.transactionHistory')}
                     {hasTransactions && (
@@ -367,7 +367,9 @@ export const Account = ({
                   />
                 ))
               ) : transactions?.success && (
-                <p>{t('transactions.placeholder')}</p>
+                <p className={style.emptyTransactions}>
+                  {t('transactions.placeholder')}
+                </p>
               )}
 
               <TransactionDetails