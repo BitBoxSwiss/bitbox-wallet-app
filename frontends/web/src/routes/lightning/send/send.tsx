a86ec8c71c2201d3a9165bf045f6049cee99b232
Ross Savage <hello@satimoto.com>
2024-11-04 09:02:15 +0100

    frontend: remove useTrampoline state

--- frontends/web/src/routes/lightning/send/send.tsx
+++ frontends/web/src/routes/lightning/send/send.tsx
@@ -252,7 +252,6 @@ export const Send = () => {
   const [customAmount, setCustomAmount] = useState<number>();
   const [rawInputError, setRawInputError] = useState<string>();
   const [sendError, setSendError] = useState<string>();
-  const [useTrampoline] = useState<boolean>(true);
 
   const back = () => {
     switch (step) {
@@ -304,7 +303,7 @@ export const Send = () => {
       case InputTypeVariant.BOLT11:
         await postSendPayment({
           bolt11: paymentDetails.invoice.bolt11,
-          useTrampoline,
+          useTrampoline: true,
           // amountMsat is optional, if amount is missing the UI shows edit-invoice step for the user to enter a custom amountMsat which is passed here
           amountMsat: customAmount ? toMsat(customAmount) : undefined
         });
