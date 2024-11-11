f00344c8370160f1ec7da3c30878d17d434f129a
sl <seangeekpro@gmail.com>
2024-11-11 09:05:16 +0100

    frontend: update UI of add account dropdown to use react-select
    Identical UI with the select account dropdown in buy & sell page
    with icon and coin name on the option and selected value.

--- frontends/web/src/routes/account/add/components/coin-dropdown.tsx
+++ frontends/web/src/routes/account/add/components/coin-dropdown.tsx
@@ -1,5 +1,6 @@
+
 /**
- * Copyright 2021 Shift Crypto AG
+ * Copyright 2021-2024 Shift Crypto AG
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +17,9 @@
 
 import { useTranslation } from 'react-i18next';
 import * as backendAPI from '@/api/backend';
-import { Select } from '@/components/forms';
+import Select, { components, SingleValueProps, OptionProps, SingleValue, DropdownIndicatorProps } from 'react-select';
+import { Logo } from '@/components/icon/logo';
+import styles from './coin-dropdown.module.css';
 
 type TCoinDropDownProps = {
   onChange: (coin: backendAPI.ICoin) => void;
@@ -24,31 +27,90 @@ type TCoinDropDownProps = {
   value: string;
 };
 
+type TOption = {
+  label: string;
+  value: backendAPI.ICoin['coinCode'];
+  isDisabled: boolean;
+}
+
+
+const DropdownIndicator = (props: DropdownIndicatorProps<TOption>) => {
+  return (
+    <components.DropdownIndicator {...props}>
+      <div className={styles.dropdown} />
+    </components.DropdownIndicator>
+  );
+};
+
+const SelectSingleValue = (props: SingleValueProps<TOption>) => {
+  const { value, label } = props.data;
+  return (
+    <div className={styles.singleValueContainer}>
+      <components.SingleValue {...props}>
+        <div className={styles.valueContainer}>
+          <Logo coinCode={value} alt={label} />
+          <span className={styles.selectLabelText}>{label}</span>
+        </div>
+      </components.SingleValue>
+    </div>
+  );
+};
+
+
+const SelectOption = (props: OptionProps<TOption>) => {
+  const { label, value } = props.data;
+
+  return (
+    <components.Option {...props}>
+      <div className={styles.valueContainer}>
+        <Logo coinCode={value} alt={label} />
+        <span className={styles.selectLabelText}>{label}</span>
+      </div>
+    </components.Option>
+  );
+};
+
+
 export const CoinDropDown = ({
   onChange,
   supportedCoins,
   value,
 }: TCoinDropDownProps) => {
   const { t } = useTranslation();
+
+  const options = [
+    { label: t('buy.info.selectPlaceholder'), value: 'choose', isDisabled: true },
+    ...supportedCoins.map(({ coinCode, name, canAddAccount }) => ({
+      value: coinCode,
+      label: name,
+      isDisabled: !canAddAccount,
+    })),
+  ] as TOption[];
+
   return (
     <Select
+      className={styles.select}
+      classNamePrefix="react-select"
       autoFocus
-      options={[
-        {
-          text: t('buy.info.selectPlaceholder'),
-          disabled: true,
-          value: 'choose',
-        },
-        ...(supportedCoins).map(({ coinCode, name, canAddAccount }) => ({
-          value: coinCode,
-          text: name,
-          disabled: !canAddAccount,
-        }))
-      ]}
-      onInput={e => onChange(supportedCoins.find(c => {
-        return c.coinCode === (e.target as HTMLSelectElement).value;
-      }) as backendAPI.ICoin)}
-      value={value}
-      id="coinCodeDropDown" />
+      isSearchable={false}
+      options={options}
+      onChange={(e) => {
+        const selectedOption = e as SingleValue<TOption>;
+        if (selectedOption) {
+          const selectedCoin = supportedCoins.find(c => c.coinCode === selectedOption.value);
+          if (selectedCoin) {
+            onChange(selectedCoin);
+          }
+        }
+      }}
+      value={options.find(option => option.value === value)}
+      components={{
+        DropdownIndicator,
+        SingleValue: SelectSingleValue,
+        Option: SelectOption,
+        IndicatorSeparator: () => null,
+      }}
+      placeholder={t('buy.info.selectPlaceholder')}
+    />
   );
 };
