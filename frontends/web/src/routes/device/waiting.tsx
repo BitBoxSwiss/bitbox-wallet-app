de6f1c685578422e36bad263db64901f3160e03d
thisconnect <this@shiftcrypto.ch>
2025-04-29 15:42:12 +0200

    frontend: update welcome message
    The reason for the generic first line is because we had the BB01 when
    this text was made. Now that the BB01 has been EOL for a long time,
    we don't need to consider it and just focus on current products.

--- frontends/web/src/routes/device/waiting.tsx
+++ frontends/web/src/routes/device/waiting.tsx
@@ -34,7 +34,7 @@ import { AppLogo, AppLogoInverted, SwissMadeOpenSource, SwissMadeOpenSourceDark 
 import { Footer, GuidedContent, GuideWrapper, Header, Main } from '@/components/layout';
 import { View, ViewContent } from '@/components/view/view';
 import { OutlinedSettingsButton } from '@/components/settingsButton/outlined-settings-button';
-import style from './bitbox01/bitbox01.module.css';
+import style from './waiting.module.css';
 
 export const Waiting = () => {
   const { t } = useTranslation();
@@ -69,13 +69,12 @@ export const Waiting = () => {
             <OutlinedSettingsButton />
           </Header>
           <View verticallyCentered width="550px" fitContent>
-            <ViewContent>
+            <ViewContent textAlign="center">
               <div>
                 {isDarkMode ? (<AppLogoInverted />) : (<AppLogo />)}
-                <div className="box large">
-                  <h3 className={style.waitingText}>{t('welcome.insertDevice')}</h3>
-                  <p className={style.waitingDescription}>{t('welcome.insertBitBox02')}</p>
-                </div>
+                <p className={style.waitingText}>
+                  {t('welcome.message')}
+                </p>
                 <Bluetooth />
               </div>
             </ViewContent>
