7f35dcd117a700b82a930aa185255b70371eeb10
thisconnect <this@shiftcrypto.ch>
2025-05-12 18:36:13 +0200

    frontend: refactor equal and inline variables
    Simplifying so that TypeScript better understand the code and we
    can change from any to unknown in the future.

--- frontends/web/src/utils/equal.ts
+++ frontends/web/src/utils/equal.ts
@@ -18,20 +18,17 @@ const isArray = Array.isArray;
 const keyList = Object.keys;
 const hasProp = Object.prototype.hasOwnProperty;
 
-export function equal(a: any, b: any): boolean {
+export const equal = (a: any, b: any): boolean => {
   if (Object.is(a, b)) {
     return true;
   }
 
   if (a && b && typeof a === 'object' && typeof b === 'object') {
-    let arrA = isArray(a), arrB = isArray(b), i: number, length: number, key: string;
-
-    if (arrA && arrB) {
-      length = a.length;
-      if (length !== b.length) {
+    if (isArray(a) && isArray(b)) {
+      if (a.length !== b.length) {
         return false;
       }
-      for (i = 0; i < length; i++) {
+      for (let i = 0; i < a.length; i++) {
         if (!equal(a[i], b[i])) {
           return false;
         }
@@ -39,26 +36,23 @@ export function equal(a: any, b: any): boolean {
       return true;
     }
 
-    if (arrA !== arrB) {
+    if (isArray(a) !== isArray(b)) {
       return false;
     }
 
-    let keys = keyList(a);
-    length = keys.length;
-
+    const length = keyList(a).length;
     if (length !== keyList(b).length) {
       return false;
     }
 
-    for (i = 0; i < length; i++) {
-      if (!hasProp.call(b, keys[i])) {
+    for (let i = 0; i < length; i++) {
+      if (!hasProp.call(b, keyList(a)[i])) {
         return false;
       }
     }
 
-    for (i = 0; i < length; i++) {
-      key = keys[i];
-      if (!equal(a[key], b[key])) {
+    for (let i = 0; i < length; i++) {
+      if (!equal(a[keyList(a)[i]], b[keyList(a)[i]])) {
         return false;
       }
     }
