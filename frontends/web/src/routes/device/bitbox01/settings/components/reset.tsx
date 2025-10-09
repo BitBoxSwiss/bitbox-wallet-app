bd5ab716eec68ef83c2c5bb53c9c963d4355929e
thisconnect <this@shiftcrypto.ch>
2025-10-09 12:41:47 +0200

    frontend: convert bitbox01 reset to functional typescript

--- frontends/web/src/routes/device/bitbox01/settings/components/reset.tsx
+++ frontends/web/src/routes/device/bitbox01/settings/components/reset.tsx
@@ -1,6 +1,6 @@
 /**
  * Copyright 2018 Shift Devices AG
- * Copyright 2021 Shift Crypto AG
+ * Copyright 2021-2025 Shift Crypto AG
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,113 +14,109 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-import { Component } from 'react';
-import { route } from '../../../../../utils/route';
-import { withTranslation } from 'react-i18next';
-import { Button, Checkbox } from '../../../../../components/forms';
-import { DialogLegacy, DialogButtons } from '../../../../../components/dialog/dialog-legacy';
-import { WaitDialog } from '../../../../../components/wait-dialog/wait-dialog';
-import { PasswordInput } from '../../../../../components/password';
-import { apiPost } from '../../../../../utils/request';
-import { alertUser } from '../../../../../components/alert/Alert';
+import { useState, ChangeEvent } from 'react';
+import { useTranslation } from 'react-i18next';
+import { route } from '@/utils/route';
+import { Button, Checkbox } from '@/components/forms';
+import { DialogLegacy, DialogButtons } from '@/components/dialog/dialog-legacy';
+import { WaitDialog } from '@/components/wait-dialog/wait-dialog';
+import { PasswordInput } from '@/components/password';
+import { apiPost } from '@/utils/request';
+import { alertUser } from '@/components/alert/Alert';
+import { SettingsButton } from '@/components/settingsButton/settingsButton';
 import style from '../../bitbox01.module.css';
-import { SettingsButton } from '../../../../../components/settingsButton/settingsButton';
 
-class Reset extends Component {
-  state = {
-    pin: null,
-    isConfirming: false,
-    activeDialog: false,
-    understand: false,
+type Props = {
+  deviceID: string;
+}
+
+export const Reset = ({ deviceID }: Props) => {
+  const { t } = useTranslation();
+
+  const [pin, setPin] = useState<string | null>(null);
+  const [understand, setUnderstand] = useState(false);
+  const [activeDialog, setActiveDialog] = useState(false);
+  const [isConfirming, setIsConfirming] = useState(false);
+
+  const handleUnderstandChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUnderstand(e.target.checked);
   };
 
-  handleUnderstandChange = (e) => {
-    this.setState({ understand: e.target.checked });
+  const setValidPIN = (e: ChangeEvent<HTMLInputElement>) => {
+    setPin(e.target.value);
   };
 
-  resetDevice = () => {
-    this.setState({
-      activeDialog: false,
-      isConfirming: true,
-    });
-    apiPost('devices/' + this.props.deviceID + '/reset', { pin: this.state.pin }).then(data => {
-      this.abort();
+  const abort = () => {
+    setPin(null);
+    setUnderstand(false);
+    setActiveDialog(false);
+    setIsConfirming(false);
+  };
+
+  const resetDevice = async () => {
+    setActiveDialog(false);
+    setIsConfirming(true);
+
+    try {
+      const data = await apiPost(`devices/${deviceID}/reset`, { pin });
+
+      abort();
+
       if (data.success) {
         if (data.didReset) {
           route('/', true);
         }
       } else if (data.errorMessage) {
-        alertUser(this.props.t(`bitbox.error.e${data.code}`, {
-          defaultValue: data.errorMessage,
-        }));
+        alertUser(
+          t(`bitbox.error.e${data.code as string}`, {
+            defaultValue: data.errorMessage,
+          })
+        );
       }
-    });
+    } catch (err) {
+      abort();
+      alertUser(String(err));
+    }
   };
 
-  setValidPIN = e => {
-    this.setState({ pin: e.target.value });
-  };
+  return (
+    <div>
+      <SettingsButton danger onClick={() => setActiveDialog(true)}>
+        {t('reset.title')}
+      </SettingsButton>
 
-  abort = () => {
-    this.setState({
-      pin: null,
-      understand: false,
-      isConfirming: false,
-      activeDialog: false,
-    });
-  };
+      {activeDialog && (
+        <DialogLegacy title={t('reset.title')} onClose={abort}>
+          <p>{t('reset.description')}</p>
 
-  render() {
-    const { t } = this.props;
-    const {
-      isConfirming,
-      activeDialog,
-      understand,
-      pin,
-    } = this.state;
-    return (
-      <div>
-        <SettingsButton danger onClick={() => this.setState({ activeDialog: true })}>
-          {t('reset.title')}
-        </SettingsButton>
-        {
-          activeDialog && (
-            <DialogLegacy
-              title={t('reset.title')}
-              onClose={this.abort}>
-              <p>
-                {t('reset.description')}
-              </p>
-              <PasswordInput
-                id="pin"
-                label={t('initialize.input.label')}
-                value={pin}
-                onInput={this.setValidPIN} />
-              <div className={style.agreements}>
-                <Checkbox
-                  id="funds_access"
-                  label={t('reset.understand')}
-                  checked={understand}
-                  onChange={this.handleUnderstandChange} />
-              </div>
-              <DialogButtons>
-                <Button danger disabled={!pin || !understand} onClick={this.resetDevice}>
-                  {t('reset.title')}
-                </Button>
-                <Button secondary onClick={this.abort} disabled={isConfirming}>
-                  {t('button.back')}
-                </Button>
-              </DialogButtons>
-            </DialogLegacy>
-          )
-        }
-        { isConfirming ? (
-          <WaitDialog title={t('reset.title')} />
-        ) : null }
-      </div>
-    );
-  }
-}
+          <PasswordInput
+            id="pin"
+            label={t('initialize.input.label')}
+            value={pin || ''}
+            onInput={setValidPIN}
+          />
 
-export default withTranslation()(Reset);
+          <div className={style.agreements}>
+            <Checkbox
+              id="funds_access"
+              label={t('reset.understand')}
+              checked={understand}
+              onChange={handleUnderstandChange}
+            />
+          </div>
+
+          <DialogButtons>
+            <Button danger disabled={!pin || !understand} onClick={resetDevice}>
+              {t('reset.title')}
+            </Button>
+            <Button secondary onClick={abort} disabled={isConfirming}>
+              {t('button.back')}
+            </Button>
+          </DialogButtons>
+        </DialogLegacy>
+      )}
+
+      {isConfirming && <WaitDialog title={t('reset.title')} />}
+    </div>
+  );
+};
