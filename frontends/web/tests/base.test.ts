e27b6b48b2091c5d75a3525ed25a6398574ce4e7
Nikolas De Giorgis <nikolas.degiorgis@gmail.com>
2025-11-03 14:51:29 +0000

    chore: reduce tests' flakyness.

--- frontends/web/tests/base.test.ts
+++ frontends/web/tests/base.test.ts
@@ -17,6 +17,7 @@
 import { test } from './helpers/fixtures';
 import { ServeWallet } from './helpers/servewallet';
 import { expect } from '@playwright/test';
+import { deleteAccountsFile, deleteConfigFile } from './helpers/fs';
 
 let servewallet: ServeWallet;
 
@@ -31,10 +32,16 @@ test('App main page loads', async ({ page, host, frontendPort, servewalletPort }
   await test.step('Navigate to the app', async () => {
     await page.goto(`http://${host}:${frontendPort}`);
     const body = page.locator('body');
-    await expect(body).toContainText('Please connect your BitBox and tap the side to continue.');
+    await expect(body).toContainText('Please connect your BitBox and tap the side to continue.'),
+    { timeout: 15000 };
   });
 });
 
-test.afterAll(() => {
-  servewallet.stop();
+test.beforeAll(async () => {
+  deleteAccountsFile();
+  deleteConfigFile();
+});
+
+test.afterAll(async () => {
+  await servewallet.stop();
 });
