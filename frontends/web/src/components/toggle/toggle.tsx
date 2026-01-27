fb087bf4d429fdfaab536162d40796cc590bc963
sl <seangeekpro@gmail.com>
2026-01-27 09:02:06 +0100

    ios: add haptic feedback to toggle

--- frontends/web/src/components/toggle/toggle.tsx
+++ frontends/web/src/components/toggle/toggle.tsx
@@ -1,17 +1,24 @@
 // SPDX-License-Identifier: Apache-2.0
 
+import { triggerHapticFeedback } from '@/utils/transport-mobile';
 import style from './toggle.module.css';
 
 export type TToggleProps = JSX.IntrinsicElements['input'];
 
 export const Toggle = ({
   className = '',
+  onChange,
   ...props
 }: TToggleProps) => {
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    triggerHapticFeedback();
+    onChange?.(e);
+  };
   return (
     <label className={`${style.container || ''} ${className}`}>
       <input
         type="checkbox"
+        onChange={handleChange}
         {...props} />
       <span className={style.slider}></span>
     </label>
