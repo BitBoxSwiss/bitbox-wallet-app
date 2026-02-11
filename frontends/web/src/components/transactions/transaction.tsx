4bfaafc8e8e1bb88a6f340aeebcb3d3eb2dd51d7
sl <seangeekpro@gmail.com>
2026-02-11 06:30:50 +0100

    frontend: limit ETH display to 9 decimal places
    Show 9 decimals for ETH balances in portfolio page, account page,
    and transaction history.

--- frontends/web/src/components/transactions/transaction.tsx
+++ frontends/web/src/components/transactions/transaction.tsx
@@ -166,6 +166,7 @@ const Amounts = ({
         {displayAmount.amount !== '0' && getTxSign(type)}
         <AmountWithUnit
           amount={displayAmount}
+          maxDecimals={9}
           unitClassName={styles.txUnit}
         />
       </span>
@@ -253,6 +254,7 @@ const Addresses = ({
               amount: (
                 <AmountWithUnit
                   amount={amount}
+                  maxDecimals={9}
                   unitClassName={styles.txUnit}
                 />
               ),
