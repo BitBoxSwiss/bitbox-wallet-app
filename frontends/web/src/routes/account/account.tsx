BitBoxSwiss/bitbox-wallet-app

commit a45bb14ae82ff215eab15517b116706567e0fc12

frontend: refactor input component styling
Changed how internal Input component styling works, with this
it is simpler to add a custom child next to the input field.

The actual styling of the input field is now on the parent
element.

Required to refactor input-with-dropdown as it previously
depended in input component styles.

frontends/web/src/components/forms/input.module.css
@@ -1,7 +1,12 @@
-.input {
+.inputContainer {
     width: 100%;
     margin-bottom: var(--space-half);
     position: relative;
+}
+
+.inputContainer input[type=checkbox] {
+    width: auto;
+    height: auto;
 }
 
 .labelContainer {
@@ -30,15 +35,14 @@
     text-align: right;
 }
 
-.input input {
+.inputField {
     background-color: var(--background-secondary);
     border-color: var(--background-quaternary);
     border-radius: 2px;
     border-style: solid;
     border-width: var(--border-width);
-    font-family: var(--font-family);
-    font-size: var(--size-default);
-    font-weight: 400;
+    display: flex;
+    gap: var(--space-eight); /* in case there is a child */
     height: var(--input-height);
     padding: 0 var(--space-half);
     width: 100%;
@@ -46,18 +50,32 @@
     will-change: border-color;
 }
 
-.input input:focus {
+.inputField input {
+    background-color: transparent;
+    border: none;
+    font-family: var(--font-family);
+    font-size: var(--size-default);
+    font-weight: 400;
+    height: var(--input-height);
+    padding: 0;
+    width: 100%;
+}
+.inputField input:focus {
+    outline: none;
+}
+
+.inputField:focus-within {
     border-color: var(--color-blue);
     outline: none;
 }
 
-.input input::placeholder {
+.inputField input::placeholder {
     color: var(--color-secondary);
 }
 
-.input input[disabled] {
-    background-color: var(--background-secondary) !important;
-    border-color: var(--background) !important;
+.inputField input[disabled] {
+    background-color: transparent;
+    border-color: transparent;
     color: var(--color-secondary) !important;
     cursor: not-allowed;
 }
@@ -76,19 +94,21 @@
     margin-left: 5px;
 }
 
-.input input[type=checkbox] {
-    width: auto;
-    height: auto;
+.inputField input[type=number],
+.inputField input[type=password],
+.inputField input[type=text] {
+    background: transparent;
 }
 
 /* webkit */
-.input input[type=number]::-webkit-inner-spin-button,
-.input input[type=number]::-webkit-outer-spin-button {
+.inputField input[type=number]::-webkit-inner-spin-button,
+.inputField input[type=number]::-webkit-outer-spin-button {
     margin: 0;
     -webkit-appearance: none;
 }
 
 /* firefox */
-.input input[type=number] {
+.inputField input[type=number] {
   -moz-appearance: textfield;
 }
+

frontends/web/src/components/forms/input-with-dropdown.module.css
@@ -1,3 +1,49 @@
+.input {
+    width: 100%;
+    margin-bottom: var(--space-half);
+    position: relative;
+}
+
+.labelContainer {
+    align-items: baseline;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+
+.labelContainer label {
+    display: block;
+    margin: 0 0 var(--space-quarter) 0;
+}
+
+.align-left label {
+    justify-self: flex-start;
+}
+
+.align-right label {
+    flex-grow: 1;
+    justify-self: flex-end;
+    text-align: right;
+}
+
+.align-right input {
+    text-align: right;
+}
+
+.isTransparent input[disabled] {
+    border-color: var(--background);
+    color: var(--color-default) !important;
+    cursor: default;
+}
+
+.errorText {
+    color: var(--color-softred) !important;
+}
+
+.errorText > span > span {
+    margin-left: 5px;
+}
+
 .inputDropdownWrapper {
     display: grid;
     grid-template-columns: 1fr minmax(160px, 280px);

frontends/web/src/components/forms/input-with-dropdown.tsx
@@ -4,7 +4,6 @@ import { forwardRef } from 'react';
 import { TBaseInputProps } from './types';
 import { Dropdown, TOption } from '@/components/dropdown/dropdown';
 import { ChevronLeftDark } from '@/components/icon';
-import baseStyles from './input.module.css';
 import styles from './input-with-dropdown.module.css';
 
 export type TInputWithDropdownProps<T> = TBaseInputProps & {
@@ -38,13 +37,13 @@ export const InputWithDropdown = forwardRef<HTMLInputElement, TInputWithDropdown
 }: TInputWithDropdownProps<any>, ref) => {
   return (
     <div className={`
-      ${baseStyles.input || ''}
-      ${baseStyles[`align-${align}`] || ''}
+      ${styles.input || ''}
+      ${styles[`align-${align}`] || ''}
       ${className}
-      ${transparent ? baseStyles.isTransparent || '' : ''}
+      ${transparent ? styles.isTransparent || '' : ''}
       `}>
       {label ? (
-        <div className={baseStyles.labelContainer}>
+        <div className={styles.labelContainer}>
           <label htmlFor={id} className={error ? styles.errorText : ''}>
             {label}
             {error ? (

frontends/web/src/components/forms/input.tsx
@@ -20,7 +20,7 @@ export const Input = forwardRef<HTMLInputElement, TInputProps>(({
 }: TInputProps, ref) => {
   return (
     <div className={[
-      styles.input,
+      styles.inputContainer,
       styles[`align-${align}`],
       className,
       transparent ? styles.isTransparent : '',
@@ -36,16 +36,18 @@ export const Input = forwardRef<HTMLInputElement, TInputProps>(({
           {labelSection && labelSection}
         </div>
       ) : null }
-      <input
-        autoComplete="off"
-        autoCorrect="off"
-        spellCheck={false}
-        type={type}
-        id={id}
-        ref={ref}
-        {...props}
-      />
-      {children}
+      <div className={styles.inputField}>
+        <input
+          autoComplete="off"
+          autoCorrect="off"
+          spellCheck={false}
+          type={type}
+          id={id}
+          ref={ref}
+          {...props}
+        />
+        {children}
+      </div>
     </div>
   );
 });

frontends/web/src/routes/account/send/feetargets.module.css
@@ -15,7 +15,7 @@
     border-left: none;
     border-top-left-radius: 0;
     padding-left: 0;
-    padding-right: 58px;
+    padding-right: 0;
     border-color: var(--background-quaternary) !important;
 }
 
@@ -29,6 +29,18 @@
     padding-bottom: 0;
     padding-top: 0;
     z-index: 1;
+}
+
+.calculatingFeePlaceholder {
+    margin-bottom: var(--space-quarter);
+}
+
+.fee > div {
+    padding: 0 var(--space-quarter) 0 0;
+}
+
+.fee > div:not(:focus-within) {
+    border-left: none;
 }
 
 @media (min-width: 640px) {
@@ -62,15 +74,16 @@
     border-width: var(--border-width);
 }
 
+
+.fee input::placeholder {
+    font-size: var(--size-small);
+}
+
 .customFeeUnit {
-    bottom: var(--space-quarter);
     color: var(--color-secondary);
-    font-size: min(var(--size-default), 20px);
-    line-height: 1;
-    height: calc(var(--input-height) * 0.5);
-    padding-right: var(--space-half);
-    position: absolute;
-    right: 0;
+    font-size: var(--size-small);
+    line-height: var(--input-height);
+    white-space: nowrap;
     width: auto;
 }
 

frontends/web/src/routes/account/send/feetargets.tsx
@@ -153,6 +153,7 @@ export const FeeTargets = ({
           showCalculatingFeeLabel ? (
             <Input
               disabled
+              className={style.calculatingFeePlaceholder}
               label={t('send.priority')}
               placeholder={t('send.feeTarget.placeholder')}
               value=""

commit 7ff51e13cf97487805960e9bfd664e4d6d8c7f63

frontend: cleanup input classes
- Fixed a regression in name and device-name-setting components
  where error was misplaced, example error with german umlauts
  introduced in a45bb14ae82ff215eab15517b116706567e0fc12
- removed unused className (account searchinput and setup name
  wizardLabel)
- added comments to prepare for new classNameInputField prop

frontends/web/src/components/forms/input.module.css
@@ -41,6 +41,7 @@
     border-style: solid;
     border-width: var(--border-width);
     display: flex;
+    flex-direction: row;
     gap: var(--space-eight); /* in case there is a child */
     height: var(--input-height);
     padding: 0 var(--space-half);

frontends/web/src/components/transactions/components/tx-detail-dialog/tx-detail-dialog.module.css
@@ -121,7 +121,7 @@
 }
 
 .note {
-    margin: 0;
+    margin-bottom: 0;
     width: 80%;
 }
 

frontends/web/src/routes/account/account.tsx
@@ -302,7 +302,6 @@ const RemountAccount = ({
                       <Input
                         ref={searchInputRef}
                         type="text"
-                        className={style.searchInput}
                         placeholder="Search transactions..."
                         value={searchTerm}
                         onChange={(e) => setSearchTerm(e.currentTarget.value)}

frontends/web/src/routes/account/send/components/inputs/receiver-address-input.module.css
@@ -2,8 +2,9 @@
   position: relative;
 }
 
-.inputWithIcon input {
-  padding-left: calc(var(--space-half) * 3);
+/* TODO: used for ETH, should be its own class */
+.inputWithIcon > div:nth-child(2) {
+  padding-left: var(--input-qr-button);
 }
 
 .action {

frontends/web/src/routes/account/walletconnect/components/connect-form/connect-form.module.css
@@ -13,8 +13,9 @@
     position: relative;
 }
 
-.inputWithIcon input {
-    padding-right: calc(var(--space-half) * 2 + 18px);
+/* TODO: its own class */
+.inputWithIcon > div:nth-child(2) {
+    padding-left: var(--input-qr-button);
 }
 
 @media (max-width: 768px) {

frontends/web/src/routes/device/bitbox02/setup/name.module.css
@@ -1,6 +1,11 @@
+/* TODO: should be its own class also used in device-name-settings */
 .inputError > div,
 .inputError > div:focus-within {
   border-color: var(--color-danger);
+}
+
+.inputError {
+  margin-bottom: 0;
 }
 
 .errorMessage {

frontends/web/src/routes/device/bitbox02/setup/name.tsx
@@ -54,17 +54,14 @@ export const SetDeviceName = ({
         <ViewContent textAlign="left" minHeight="140px">
           <Input
             autoFocus
-            className={`
-              ${style.wizardLabel || ''}
-              ${error && !nameIsTooShort && style.inputError || ''}
-            `}
+            className={error && !nameIsTooShort && style.inputError || ''}
             label={t('bitbox02Wizard.stepCreate.nameLabel')}
             onInput={(e) => setDeviceName(e.target.value)}
             placeholder={t('bitbox02Wizard.stepCreate.namePlaceholder')}
             value={deviceName}
-            id="deviceName">
-            <DeviceNameErrorMessage error={error} invalidChars={invalidChars} />
-          </Input>
+            id="deviceName"
+          />
+          <DeviceNameErrorMessage error={error} invalidChars={invalidChars} />
         </ViewContent>
         <ViewButtons>
           <Button

frontends/web/src/routes/settings/components/device-settings/device-name-setting.tsx
@@ -108,7 +108,7 @@ const SetDeviceNameDialog = ({ open, onClose, currentName, onInputChange, name, 
       </p>
       <Input
         autoFocus
-        className={`m-none ${error && !nameIsTooShort ? nameStyle.inputError as string : ''}`}
+        className={error && !nameIsTooShort ? nameStyle.inputError : ''}
         label={t('bitbox02Settings.deviceName.input')}
         onInput={(e) => onInputChange(e.target.value)}
         placeholder={t('bitbox02Settings.deviceName.placeholder')}