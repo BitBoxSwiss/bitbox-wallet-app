533154ea2e59518286a1bac47d5a539df0c82b75
thisconnect <this@shiftcrypto.ch>
2026-02-19 10:52:54 +0100

    frontend: refactor send simplify passing data through components
    Changed to take defaultCurrency from rates context.
    
    Before defaultCurrency was passed via props with various different
    namings: activeCurrency, fiatUnit etc.
    
    Simplified also some other props that were unnecessary renamed.

--- frontends/web/src/routes/account/send/components/fiat-value.tsx
+++ frontends/web/src/routes/account/send/components/fiat-value.tsx
@@ -1,42 +1,30 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import { useContext } from 'react';
-import type { ConversionUnit } from '@/api/account';
-import { Amount } from '@/components/amount/amount';
-import { AmountUnit } from '@/components/amount/amount-with-unit';
-import { RatesContext } from '@/contexts/RatesContext';
+import type { TAmountWithConversions } from '@/api/account';
+import { AmountWithUnit } from '@/components/amount/amount-with-unit';
 import style from './fiat-value.module.css';
 
 type TFiatValueProps = {
-  amount: string;
-  baseCurrencyUnit: ConversionUnit;
+  amount: TAmountWithConversions;
   className?: string;
   enableRotateUnit?: boolean;
 };
 
 export const FiatValue = ({
   amount,
-  baseCurrencyUnit,
   className,
   enableRotateUnit = false,
 }: TFiatValueProps) => {
-  const { rotateDefaultCurrency } = useContext(RatesContext);
-  const rotateUnit = enableRotateUnit ? rotateDefaultCurrency : undefined;
 
   const classNames = `${style.fiatValue || ''} ${className && className || ''}`;
 
   return (
     <p className={classNames}>
-      <Amount
+      <AmountWithUnit
         alwaysShowAmounts
+        convertToFiat
         amount={amount}
-        unit={baseCurrencyUnit}
-        onMobileClick={rotateUnit} />
-      {' '}
-      <AmountUnit
-        unit={baseCurrencyUnit}
-        rotateUnit={rotateUnit}
-        className={style.unit} />
+        enableRotateUnit={enableRotateUnit} />
     </p>
   );
 };
