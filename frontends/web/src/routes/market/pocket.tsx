e07f8576c0a0ce3ec668f56167987c0f17f9ab90
thisconnect <this@shiftcrypto.ch>
2026-04-01 11:03:39 +0200

    frontend: add point-to-device graphic to pocket verify step
    To guide the user to focus on the device for address verification.

--- frontends/web/src/routes/market/pocket.tsx
+++ frontends/web/src/routes/market/pocket.tsx
@@ -11,6 +11,7 @@ import { verifyAddress, getPocketURL, TMarketAction } from '@/api/market';
 import { AccountCode, getInfo, getTransactionList, hasPaymentRequest, signAddress, proposeTx, sendTx, TTxInput } from '@/api/account';
 import { Header } from '@/components/layout';
 import { Spinner } from '@/components/spinner/Spinner';
+import { PointToBitBox02 } from '@/components/icon';
 import { PocketTerms } from '@/components/terms/pocket-terms';
 import { useLoad } from '@/hooks/api';
 import { UseDisableBackButton } from '@/hooks/backbutton';
@@ -362,7 +363,10 @@ export const Pocket = ({
             open={verifying}
             title={t('receive.verifyBitBox02')}
             medium centered>
-            <div className="text-center">{t('buy.pocket.verifyBitBox02')}</div>
+            <div className="text-center">
+              {t('buy.pocket.verifyBitBox02')}
+              <PointToBitBox02 />
+            </div>
           </Dialog>
           <FirmwareUpgradeRequiredDialog
             open={fwRequiredDialog}
