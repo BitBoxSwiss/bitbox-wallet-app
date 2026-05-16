e46b2efdddd53b046e99f66975c286799895291d
Marko Bencun <marko@shiftcrypto.ch>
2026-05-16 18:10:24 +0000

    backend/handlers: return open errors in response

--- frontends/web/src/api/system.ts
+++ frontends/web/src/api/system.ts
@@ -2,10 +2,17 @@
 
 import { apiPost } from '@/utils/request';
 
+type TOpenResponse = {
+  success: true;
+} | {
+  success: false;
+  errorMessage: string;
+};
+
 export const notifyUser = (text: string) => {
   return apiPost('notify-user', { text });
 };
 
-export const open = (href: string) => {
+export const open = (href: string): Promise<TOpenResponse> => {
   return apiPost('open', href);
 };
