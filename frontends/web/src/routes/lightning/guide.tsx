f8b783f573b3b06b342fb67700c81dcd4184c821
strmci <strmci@protonmail.com>
2026-07-20 16:06:17 +0200

    frontend: add lightning guide to send and receive flows

--- frontends/web/src/routes/lightning/guide.tsx
+++ frontends/web/src/routes/lightning/guide.tsx
@@ -17,3 +17,51 @@ export const LightningGuide = () => {
     </Guide>
   );
 };
+
+export const LightningSendGuide = () => {
+  const { t } = useTranslation();
+  return (
+    <Guide title={t('guide.guideTitle.lightningSend')}>
+      <Entry key="guide.lightning.send.payment" entry={{
+        text: t('guide.lightning.send.payment.text'),
+        title: t('guide.lightning.send.payment.title'),
+      }} />
+      <Entry key="guide.lightning.send.invoiceAndAddress" entry={{
+        text: t('guide.lightning.send.invoiceAndAddress.text'),
+        title: t('guide.lightning.send.invoiceAndAddress.title'),
+      }} />
+      <Entry key="guide.lightning.send.fee" entry={{
+        text: t('guide.lightning.send.fee.text'),
+        title: t('guide.lightning.send.fee.title'),
+      }} />
+      <Entry key="guide.lightning.send.cancel" entry={{
+        text: t('guide.lightning.send.cancel.text'),
+        title: t('guide.lightning.send.cancel.title'),
+      }} />
+    </Guide>
+  );
+};
+
+export const LightningReceiveGuide = () => {
+  const { t } = useTranslation();
+  return (
+    <Guide title={t('guide.guideTitle.lightningReceive')}>
+      <Entry key="guide.lightning.receive.payment" entry={{
+        text: t('guide.lightning.receive.payment.text'),
+        title: t('guide.lightning.receive.payment.title'),
+      }} />
+      <Entry key="guide.lightning.receive.invoiceOrAddress" entry={{
+        text: t('guide.lightning.receive.invoiceOrAddress.text'),
+        title: t('guide.lightning.receive.invoiceOrAddress.title'),
+      }} />
+      <Entry key="guide.lightning.receive.reuseAddress" entry={{
+        text: t('guide.lightning.receive.reuseAddress.text'),
+        title: t('guide.lightning.receive.reuseAddress.title'),
+      }} />
+      <Entry key="guide.lightning.receive.fee" entry={{
+        text: t('guide.lightning.receive.fee.text'),
+        title: t('guide.lightning.receive.fee.title'),
+      }} />
+    </Guide>
+  );
+};
