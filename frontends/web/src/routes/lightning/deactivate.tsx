f15022c9cc1a17b4bea429b76f4dac5215ba4a94
thisconnect <this@shiftcrypto.ch>
2026-09-17 13:33:47 +0200

    frontend: fix hardcoded text in deactivate lightning component
    Used the same key as used in the settings (Disable Lightning wallet)
    as "Shut down your wallet" is ambigous. With this the settings
    button, view title and primary button use the same string
    conistently.

--- frontends/web/src/routes/lightning/deactivate.tsx
+++ frontends/web/src/routes/lightning/deactivate.tsx
@@ -55,7 +55,7 @@ export const LightningDeactivate = () => {
           </ViewContent>
           <ViewButtons>
             <Button danger disabled={!agree} onClick={() => deactivateWallet()}>
-              Shut down lightning wallet
+              {t('lightning.settings.disableWallet')}
             </Button>
             <DesktopBackButton onClick={() => navigate(-1)}>
               {t('button.back')}
@@ -90,7 +90,7 @@ export const LightningDeactivate = () => {
         variant="navigation"
         mobileBackButton={step === 'intro'}
         onBack={() => navigate(-1)}
-        title="Shut down lightning wallet"
+        title={t('lightning.settings.disableWallet')}
       />
       {renderSteps()}
     </Main>
